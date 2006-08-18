441607fd2c86bf4670edefe9fe386f9b858e7e78
etaewdev <etaewdev@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2006-08-18 20:20:36 +0000

    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/1.x@181 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/effects/StaticEffect.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using DOL.GS.PacketHandler;
+
+namespace DOL.GS.Effects
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class StaticEffect : IGameEffect
+	{
+		private ushort m_id;
+		protected GameLiving m_owner = null;
+
+		/// <summary>
+		/// create static effect
+		/// </summary>
+		public StaticEffect()
+		{
+		}
+
+		/// <summary>
+		/// Cancel effect
+		/// </summary>
+		/// <param name="playerCanceled"></param>
+		public virtual void Cancel(bool playerCanceled) {
+			if (playerCanceled && HasNegativeEffect) {
+				if (Owner is GamePlayer)
+					((GamePlayer)Owner).Out.SendMessage("You can't remove this effect!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+				return;
+			}
+			Stop();
+		}
+
+		/// <summary>
+		/// Start the effect on target
+		/// </summary>
+		/// <param name="target">The effect target</param>
+		public virtual void Start(GameLiving target)
+		{
+			lock (this) {
+				m_owner = target;
+				target.EffectList.Add(this);
+			}
+		}
+
+		/// <summary>
+		/// Stop the effect on owner
+		/// </summary>
+		public virtual void Stop() 
+		{
+			lock (this) {
+				if(m_owner != null) {
+					m_owner.EffectList.Remove(this);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Name of the effect
+		/// </summary>
+		public virtual string Name { get { return "Noname"; } }
+
+		/// <summary>
+		/// Remaining Time of the effect in milliseconds
+		/// </summary>
+		public virtual int RemainingTime {
+			get {
+				return 0; // unlimited
+			}
+		}
+
+		public GameLiving Owner {
+			get { return m_owner; }
+		}
+
+		public virtual ushort Icon { 
+			get { return 0; } }
+
+		/// <summary>
+		/// unique id for identification in effect list
+		/// </summary>
+		public ushort InternalID
+		{
+			get { return m_id; }
+			set { m_id = value; }
+		}
+
+		public virtual bool HasNegativeEffect {
+			get { return false; }
+		}
+
+		public virtual IList DelveInfo {
+			get {
+				return new ArrayList(0);
+			}
+		}
+	}
+}
