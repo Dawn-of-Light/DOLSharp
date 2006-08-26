f45a4349c9abf01de2959715c6177659655fd576
malkavien <malkavien@users.sourceforge.net>
2006-08-26 19:46:12 +0000

    - Changed : Revert some damage formulas, modified by purga and merged by error. Affects PA/BS and Spells.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/1.x@196 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/spells/LifedrainSpellHandler.cs
+++ GameServer/spells/LifedrainSpellHandler.cs
@@ -22,75 +22,76 @@ using DOL.GS.PacketHandler;
 
 namespace DOL.GS.Spells
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	[SpellHandlerAttribute("Lifedrain")]
-	public class LifedrainSpellHandler : DirectDamageSpellHandler
-	{
-		/// <summary>
-		/// execute direct effect
-		/// </summary>
-		/// <param name="target">target that gets the damage</param>
-		/// <param name="effectiveness">factor from 0..1 (0%-100%)</param>
-		public override void OnDirectEffect(GameLiving target, double effectiveness)
-		{
-			if (target == null) return;
-			if (!target.Alive || target.ObjectState!=GameLiving.eObjectState.Active) return;
+    /// <summary>
+    /// 
+    /// </summary>
+    [SpellHandlerAttribute("Lifedrain")]
+    public class LifedrainSpellHandler : DirectDamageSpellHandler
+    {
+        /// <summary>
+        /// execute direct effect
+        /// </summary>
+        /// <param name="target">target that gets the damage</param>
+        /// <param name="effectiveness">factor from 0..1 (0%-100%)</param>
+        public override void OnDirectEffect(GameLiving target, double effectiveness)
+        {
+            if (target == null) return;
+            if (!target.Alive || target.ObjectState != GameLiving.eObjectState.Active) return;
 
-			// calc damage and healing
-			AttackData ad = CalculateDamageToTarget(target, effectiveness);
-			SendDamageMessages(ad);
-			DamageTarget(ad, true);
-			StealLife(ad);
-			target.StartInterruptTimer(SPELL_INTERRUPT_DURATION, ad.AttackType, Caster);
-		}
+            // calc damage and healing
+            AttackData ad = CalculateDamageToTarget(target, effectiveness);
+            SendDamageMessages(ad);
+            DamageTarget(ad, true);
+            StealLife(ad);
+            target.StartInterruptTimer(SPELL_INTERRUPT_DURATION, ad.AttackType, Caster);
+        }
 
-		/// <summary>
-		/// Uses percent of damage to heal the caster
-		/// </summary>
-		public virtual void StealLife(AttackData ad)
-		{
-			if(ad == null) return;
-			if(!m_caster.Alive) return;
+        /// <summary>
+        /// Uses percent of damage to heal the caster
+        /// </summary>
+        public virtual void StealLife(AttackData ad)
+        {
+            if (ad == null) return;
+            if (!m_caster.Alive) return;
 
-			int heal = (ad.Damage + ad.CriticalDamage) * m_spell.LifeDrainReturn/100;
-			if (m_caster.IsDiseased)
-			{
-				MessageToCaster("You are diseased!", eChatType.CT_SpellResisted);
-				heal >>= 1;
-			}
-			if(heal <= 0) return;
-			heal = m_caster.ChangeHealth(m_caster, GameLiving.eHealthChangeType.Spell, heal);
+            int heal = (ad.Damage + ad.CriticalDamage) * m_spell.LifeDrainReturn / 100;
+            if (m_caster.IsDiseased)
+            {
+                MessageToCaster("You are diseased!", eChatType.CT_SpellResisted);
+                heal >>= 1;
+            }
+            if (heal <= 0) return;
+            heal = m_caster.ChangeHealth(m_caster, GameLiving.eHealthChangeType.Spell, heal);
 
-			if(heal > 0) {
-				MessageToCaster("You steal " + heal + " hit point" + (heal==1?".":"s."), eChatType.CT_Spell);
-			}
-			else {
-				MessageToCaster("You cannot absorb any more life.", eChatType.CT_SpellResisted);
-			}
-		}
+            if (heal > 0)
+            {
+                MessageToCaster("You steal " + heal + " hit point" + (heal == 1 ? "." : "s."), eChatType.CT_Spell);
+            }
+            else
+            {
+                MessageToCaster("You cannot absorb any more life.", eChatType.CT_SpellResisted);
+            }
+        }
 
-		/// <summary>
-		/// Calculates the base 100% spell damage which is then modified by damage variance factors
-		/// </summary>
-		/// <returns></returns>
-		public override double CalculateDamageBase()
-		{
-			double spellDamage = Spell.Damage;
-			GamePlayer player = Caster as GamePlayer;
-			if (player != null && player.CharacterClass.ManaStat != eStat.UNDEFINED)
-			{
-				int manaStatValue = player.GetModified((eProperty) player.CharacterClass.ManaStat);
-				int speclevel = m_caster.GetModifiedSpecLevel(m_spellLine.Spec);
-				spellDamage *= (0.845 + (speclevel-50)*0.01 + manaStatValue/spellDamage);
-				if (spellDamage < 0)
-					spellDamage = 0;
-			}
-			return spellDamage;
-		}
+        /// <summary>
+        /// Calculates the base 100% spell damage which is then modified by damage variance factors
+        /// </summary>
+        /// <returns></returns>
+        public override double CalculateDamageBase()
+        {
+            double spellDamage = Spell.Damage;
+            GamePlayer player = Caster as GamePlayer;
+            if (player != null && player.CharacterClass.ManaStat != eStat.UNDEFINED)
+            {
+                int manaStatValue = player.GetModified((eProperty)player.CharacterClass.ManaStat);
+                spellDamage *= (manaStatValue + 180) / 250.0;
+                if (spellDamage < 0)
+                    spellDamage = 0;
+            }
+            return spellDamage;
+        }
 
-		// constructor
-		public LifedrainSpellHandler(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) {}		
-	}
+        // constructor
+        public LifedrainSpellHandler(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) { }
+    }
 }
