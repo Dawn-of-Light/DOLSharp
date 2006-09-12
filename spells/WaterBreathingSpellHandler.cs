f0e59a858770b497e4f6f3c0f08b078dd4973a46
etaewdev <etaewdev@users.sourceforge.net>
2006-09-12 20:58:00 +0000

    *) Code from Purga - Added: Spell handlers for EnduranceHeal,PowerHeal and WaterBreathing *) Code from Me - Added: ServerRules.OnLivingKilled here handling is added for gaining Experience, BountyPoints or RealmPoints for a non GameNPC or GamePlayer item. - Bugfix: You will now only received the "You have achieved level" message, when you reach your final level that your current experience allows. - Bugfix: If a keep npc was targeted in attack state by any realm member the whole keep was marked as under attack. - Added: The keep and tower lords are now worth RPs. - Bugfix: Hibernia portal keep stones now work - Changed: Keep doors are now worth RPs - Bugfix: When you upgrade a level your keeps pieces will now gain health. - Bugfix: When attempting to claim a tower, there is no longer a 500 guild bps requirement, it is not 50 guild bp. - Changed: The first component to be destroyed on a keep will now be worth RPs - Bugfix: Added a manual door state update triggered by targeting a door to fix an issue which some players encountered closed doors with 0 health. - Bugfix: Added a check for a portal stone when teleporting to border keeps. - Bugfix: You can now cast self and group target spells when targeting a keep component or door. - Added: NPCs can now be worth RPs and BPs - Added: When repairing a component or door, you will now be rewarded RPs
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/1.x/GameServer@222 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ spells/WaterBreathingSpellHandler.cs
@@ -0,0 +1,64 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *///made by DeMAN
+using System;
+using System.Reflection;
+using DOL.GS.PacketHandler;
+using DOL.GS.Effects;
+using DOL.Events;
+using log4net;
+ 
+ 
+namespace DOL.GS.Spells
+{
+	[SpellHandlerAttribute("WaterBreathing")]
+	public class WaterBreathingSpellHandler : SpellHandler
+	{
+		/// <summary>
+		/// Defines a logger for this class.
+		/// </summary>
+		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public WaterBreathingSpellHandler(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) {}
+
+		public override void OnEffectStart(GameSpellEffect effect)
+		{
+			GamePlayer player = effect.Owner as GamePlayer;
+			if (player != null)
+				player.CanBreathUnderWater = true;
+			eChatType toLiving = (Spell.Pulse == 0) ? eChatType.CT_Spell : eChatType.CT_SpellPulse;
+			eChatType toOther = (Spell.Pulse == 0) ? eChatType.CT_System : eChatType.CT_SpellPulse;
+			if (Spell.Message2 != "")
+				Message.SystemToArea(effect.Owner, Util.MakeSentence(Spell.Message2, effect.Owner.GetName(0, false)), toOther, effect.Owner);
+			MessageToLiving(effect.Owner, Spell.Message1 == "" ? "You find yourself able to move freely and breathe water like air!" : Spell.Message1, toLiving);
+			base.OnEffectStart(effect);
+		}
+
+		public override int OnEffectExpires(GameSpellEffect effect, bool noMessages)
+		{
+			GamePlayer player = effect.Owner as GamePlayer;
+			if (player != null)
+			{
+				player.CanBreathUnderWater = false;
+				if (player.IsDiving)
+					MessageToLiving(effect.Owner, "With a gulp and a gasp you realize that you are unable to breathe underwater any longer!", eChatType.CT_SpellExpires);
+			}
+			return base.OnEffectExpires(effect, noMessages);
+		}
+	}
+}
