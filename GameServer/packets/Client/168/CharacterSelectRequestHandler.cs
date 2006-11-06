fdde4bda693143e425cb202dcf9eb3f0f07be036
etaewdev <etaewdev@users.sourceforge.net>
2006-11-06 14:11:33 +0000

    - Bugfix: InventoryItem CopyFrom now uses IsTradable - Bugfix: Server can now accept connections again (Now I finally understand the PacketHandler system)
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/1.x@252 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/packets/Client/168/CharacterSelectRequestHandler.cs
@@ -0,0 +1,90 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using DOL.Database;
+
+namespace DOL.GS.PacketHandler.Client.v168
+{
+	[PacketHandlerAttribute(PacketHandlerType.TCP,0xB8^168,"Handles setting SessionID and the active character")]
+	public class CharacterSelectRequestHandler : IPacketHandler
+	{
+		public int HandlePacket(GameClient client, GSPacketIn packet)
+		{
+			int packetVersion;
+			switch (client.Version)
+			{
+				case GameClient.eClientVersion.Version168:
+				case GameClient.eClientVersion.Version169:
+				case GameClient.eClientVersion.Version170:
+				case GameClient.eClientVersion.Version171:
+				case GameClient.eClientVersion.Version172:
+				case GameClient.eClientVersion.Version173:
+					packetVersion = 168; break;
+				default:
+					packetVersion = 174; break;
+			}
+			packet.Skip(4); //Skip the first 4 bytes
+			if(packetVersion==174) packet.Skip(1);
+			string charName = packet.ReadString(28);
+
+			//TODO Character handling 
+			if(charName.Equals("noname")) 
+			{
+				client.Out.SendSessionID();
+			} 
+			else 
+			{
+				// SH: Also load the player if client player is NOT null but their charnames differ!!!
+				// only load player when on charscreen and player is not loaded yet
+				// packet is sent on every region change (and twice after "play" was pressed)
+				if(
+				   (
+					(client.Player == null && client.Account.Characters != null) 
+					|| (client.Player!=null && client.Player.Name.ToLower()!=charName.ToLower())
+				   ) && client.ClientState == GameClient.eClientState.CharScreen)
+				{
+					bool charFound=false;
+					for(int i=0;i<client.Account.Characters.Length;i++)
+					{
+						if(client.Account.Characters[i]!=null 
+							&& client.Account.Characters[i].Name==charName)
+						{
+							charFound=true;
+							client.LoadPlayer(i);
+							break;
+						}
+					}
+					if(charFound==false)
+					{
+						client.Player=null;
+						client.ActiveCharIndex=-1;
+					}
+				}
+				if(client.Player==null)
+				{
+					// client keeps sending the name of the deleted char even if new one was created, correct name only after "play" button pressed
+					//client.Server.Error(new Exception("ERROR, active character not found!!! name="+charName));
+				}
+				client.Out.SendSessionID();
+			}
+			return 1;
+		}
+	}
+}
+
---
+++ GameServer/packets/Client/168/HousingMenuRequestHandler.cs
@@ -0,0 +1,82 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using DOL.GS.Housing;
+using DOL.GS.PacketHandler;
+using System.Reflection;
+using log4net;
+
+namespace DOL.GS.PacketHandler.Client.v168
+{
+	[PacketHandlerAttribute(PacketHandlerType.TCP, 0x00, "Handles housing menu requests")]
+	public class HousingBuyRequestHandler : IPacketHandler
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		public int HandlePacket(GameClient client, GSPacketIn packet)
+		{
+			int housenumber = packet.ReadShort();
+			int menuid = packet.ReadByte();
+			int flag = packet.ReadByte();
+//			client.Out.SendDebugMessage("CtoS_0x00 (houseNumber:0x{0:X4} menuid:{1} flag:0x{2:X2})", housenumber, menuid, flag);
+           
+			House house = (House)HouseMgr.GetHouse(client.Player.CurrentRegionID,housenumber);
+			if(house == null) 
+			    return 1; 
+			if (client.Player == null) 
+			    return 1;
+		
+			client.Player.CurrentHouse = house;
+
+			switch(menuid)
+			{
+				case 0: // Exterior decoration (Garden)
+                    if (!house.IsOwner(client.Player) && !house.CanAddGarden(client.Player))
+                        return 1;
+					client.Player.Out.SendMerchantWindow(HouseTemplateMgr.OutdoorShopItems, eMerchantWindowType.HousingOutsideShop);
+					break;
+
+				case 1: // Interior decoration
+                    if (!house.IsOwner(client.Player) && !house.CanAddInterior(client.Player))
+                        return 1;
+					client.Player.Out.SendMerchantWindow(HouseTemplateMgr.IndoorShopItems, eMerchantWindowType.HousingInsideShop);
+					break;
+
+				case 2: // Exterior menu
+                    if (!house.IsOwner(client.Player) && !house.CanAddGarden(client.Player))
+                        return 1;
+					client.Player.Out.SendMerchantWindow(HouseTemplateMgr.OutdoorMenuItems, eMerchantWindowType.HousingOutsideMenu);
+					break;
+
+				case 7:
+					house.SendHouseInfo(client.Player);
+					break;
+
+				case 8: // Interior menu (flag = 0x00 - roof, 0xFF - floor or wall)
+                    if (!house.IsOwner(client.Player) && !house.CanAddInterior(client.Player))
+                        return 1;
+					client.Player.Out.SendMerchantWindow(HouseTemplateMgr.IndoorMenuItems, eMerchantWindowType.HousingInsideMenu);
+					break;
+
+				default:
+					client.Out.SendMessage("Invalid menu id "+menuid+" (hookpoint?).",eChatType.CT_System,eChatLoc.CL_SystemWindow);
+					break;
+			}
+			return 1;
+		}
+	}
+}
