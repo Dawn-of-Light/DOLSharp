3359da707466156e230f80baf66087e43c4a7a9e
Urzaria <Urzaria@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-04-25 12:11:42 +0000

    - Add: some entries for language manager support

--- GameServerScripts/quests/hibernia/ImportantDelivery.cs
+++ GameServerScripts/quests/hibernia/ImportantDelivery.cs
@@ -876,7 +876,7 @@ namespace DOL.GS.Quests.Hibernia
 			GiveItem(freagus, m_questPlayer, recruitsCloak);
 
 			m_questPlayer.GainExperience(12);
-			m_questPlayer.AddMoney(Money.GetMoney(0, 0, 0, 1, Util.Random(50)), "You recieve {0} as a reward.");
+			m_questPlayer.AddMoney(Money.GetMoney(0, 0, 0, 1, Util.Random(50)), LanguageMgr.GetTranslation(m_questPlayer.Client, "Hib.ImportantDelivery.FinishQuest.RecieveReward"));
 		}
 	}
 }
