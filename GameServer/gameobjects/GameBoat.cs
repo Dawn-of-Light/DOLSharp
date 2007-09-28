db5465bc1faba456b1b323ec01e609f6670a47e6
ichijin <ichijin@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-09-28 04:33:49 +0000

    - Added missing guilddues from looting section. - Added Player Boats -- clicking will move your boat to that location -- Player boats CANNOT be boarded by right clicking type /boat board -- /boat summon/unsummon - to remove/add boat to world -- /boat delete - removes boat permanantly -- /boat follow - follows another boat - Added /viewreports command - Allows GM/players to view bug reports. -- /viewreports delete # - removes report -- /viewreprots close # - closes a report

--- GameServer/gameobjects/GameBoat.cs
+++ GameServer/gameobjects/GameBoat.cs
@@ -1,106 +1,144 @@
-using DOL.AI.Brain;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using DOL.GS;
 using DOL.Database;
+using DOL.Language;
+using DOL.GS.Movement;
+using DOL.GS.PacketHandler;
+using log4net;
+using DOL.AI.Brain;
 
 namespace DOL.GS
 {
 	public class GameBoat : GameMovingObject
 	{
-		/*
-		 * TODO:
-		 * 
-		 * Boat Commands
-		 * Command	Description
-		 * /setdest	Commands the vehicle you're driving to go to your ground target. (Also works with Seige Equipment)
-		 * /vboard	Board targetted boat
-		 * /vboot	Boot someone from your boat
-		 * /vdestination [x] [y]	Will sail your boat to a specified location (x, y) within your current zone
-		 * /vfollow	Allows the controller of a movable vehicle to follow their currently targeted object
-		 * /vforward	Move your boat forward
-		 * /vinvite	Invite someone onto your boat
-		 * /vset [public/private]	Sets your boat to either Public or Private
-		 * 
-		 * different speeds for different populations on boats (personal / toa will always move full speed)
-		 * - For each person under the total capacity that is not on the boat, movement speed is decreased approximately 3%. If a boat has less than the minimum requirement, the boat cannot be moved.
-		 * - Boats can only be built away from enemy controlled keeps,
-		 * 
-		 **/
-		private byte m_type = 0;
+        private byte m_type = 0;
+        protected DBBoat m_DBboat;
+        private string boat_id;
+        private string boat_owner;
+        private string boat_name;
+        private ushort boat_model;
+        private int boat_maxspeedbase;
 
 		private RegionTimer m_removeTimer = null;
 
-		public GameBoat(byte type)
+        /// <summary>
+        /// Defines a logger for this class.
+        /// </summary>
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public GameBoat(byte type)
+            : base()
+        {
+            m_type = type;
+            base.BoatOwnerID = OwnerID;
+        }
+
+        public GameBoat()
 			: base()
-		{
-			m_type = type;
-		}
-
-		public GameBoat()
-			: base()
-		{ }
-
-		public override void LoadFromDatabase(DataObject obj)
-		{
-			switch ((obj as Mob).Model)
-			{
-				case 2648: m_type = 0; break;
-				case 2646: m_type = 1; break;
-				case 2647: m_type = 2; break;
-			}
-
-			base.LoadFromDatabase(obj);
-		}
+        {
+            base.BoatOwnerID = OwnerID;
+        }
 
 		public override bool AddToWorld()
 		{
-			Name = "Boat";
-			Model = 2648;
-			BlankBrain brain = new BlankBrain();
-			SetOwnBrain(brain);
-			MaxSpeedBase = 316;
-			return base.AddToWorld();
-		}
+            if (!base.AddToWorld())
+            {
+                return false;
+            }
+            return true;            
+        }
 
-		public override ushort Model
-		{
-			get
-			{
-				switch (m_type)
-				{
-					case 0: return 2648;
-					case 1: return 2646;
-					case 2: return 2647;
-					default: return 2648;
-				}
-			}
-		}
+        /// <summary>
+        /// Gets or sets the boats db
+        /// </summary>
+        public DBBoat theBoatDB
+        {
+            get { return m_DBboat; }
+            set { m_DBboat = value; }
+        }
 
-		public override string Name
-		{
-			get
-			{
-				switch (m_type)
-				{
-					case 0: return "scout boat";
-					case 1: return "galleon";
-					case 2: return "warship";
-					default: return "boat";
-				}
-			}
-		}
+        public string BoatID
+        {
+            get
+            {
+                return boat_id;
+            }
+            set
+            {
+                boat_id = value;
+            }
+        }
+        
+        public override string Name
+        {
+            get
+            {
+                return boat_name;
+            }
+            set
+            {
+                boat_name = value;
+            }
+        }
 
-		public override int MAX_PASSENGERS
-		{
-			get
-			{
-				switch (m_type)
-				{
-					case 0: return 8;
-					case 1: return 16;
-					case 2: return 32;
-					default: return 2;
-				}
-			}
-		}
+        public override ushort Model
+        {
+            get
+            {
+                return boat_model;
+            }
+            set
+            {
+                boat_model = value;
+            }
+        }
+
+        public override int MaxSpeedBase
+        {
+            get
+            {
+                return boat_maxspeedbase;
+            }
+            set
+            {
+                boat_maxspeedbase = value;
+            }
+        }
+
+        public string OwnerID
+        {
+            get
+            {
+                return boat_owner;
+            }
+            set
+            {
+                boat_owner = value;
+            }
+        }
+
+        public override int MAX_PASSENGERS
+        {
+            get
+            {
+                switch (m_type)
+                {
+                    case 0: return 8;
+                    case 1: return 8;
+                    case 2: return 16;
+                    case 3: return 32;
+                    case 4: return 32;
+                    case 5: return 31;
+                    case 6: return 24;
+                    case 7: return 64;
+                    case 8: return 33;
+                    default: return 2;
+                }
+            }
+        }
 
 		public override int REQUIRED_PASSENGERS
 		{
@@ -108,10 +146,16 @@ namespace DOL.GS
 			{
 				switch (m_type)
 				{
-					case 0: return 1;
-					case 1: return 2;
-					case 2: return 4;
-					default: return 1;
+                    case 0: return 1;
+                    case 1: return 1;
+                    case 2: return 1;
+                    case 3: return 1;
+                    case 4: return 1;
+                    case 5: return 1;
+                    case 6: return 1;
+                    case 7: return 1;
+                    case 8: return 1;
+                    default: return 1;
 				}
 			}
 		}
@@ -148,26 +192,78 @@ namespace DOL.GS
 					m_removeTimer.Stop();
 				m_removeTimer.Start(15 * 60 * 1000);
 			}
-
+      
 			return true;
 		}
 
-		protected int RemoveCallback(RegionTimer timer)
+        protected int RemoveCallback(RegionTimer timer)
 		{
 			m_removeTimer.Stop();
 			m_removeTimer = null;
 			Delete();
 			return 0;
-		}
+        }
 
-		public override bool Interact(GamePlayer player)
-		{
-			if (player.InCombat)
-			{
-				player.Out.SendMessage("You cannot board a boat when in combat!", DOL.GS.PacketHandler.eChatType.CT_System, DOL.GS.PacketHandler.eChatLoc.CL_SystemWindow);
-				return false;
-			}
-			return base.Interact(player);
-		}
-	}
+        /// <summary>
+        /// Checks if a player can see the boat.
+        /// </summary>
+        public bool CanSeeBoat(GamePlayer player, GameBoat boat)
+        {
+            foreach (GamePlayer plr in boat.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+            {
+                if (player.Name == plr.Name)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Must overide interact for Game Boat - Cannot allow boarders on player boat
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        public override bool Interact(GamePlayer player)
+        {
+            if (this.BoatOwnerID != "")            
+                return false;
+              
+ 	        return base.Interact(player);
+        }       
+       
+        /// <summary>
+        /// Loads this boat from a boat table
+        /// </summary>
+        /// <param name="obj"></param>
+        public override void LoadFromDatabase(DataObject obj)
+        {
+            if (!(obj is DBBoat))
+                return;
+
+            m_DBboat = (DBBoat)obj;
+            boat_id = m_DBboat.ObjectId;
+            boat_name = m_DBboat.BoatName;
+            boat_maxspeedbase = m_DBboat.BoatMaxSpeedBase;
+            boat_model = m_DBboat.BoatModel;
+            boat_owner = m_DBboat.BoatOwner;
+            switch (boat_model)
+            {
+                case 1616: m_type = 0; break;
+                case 2648: m_type = 1; break;
+                case 2646: m_type = 2; break;
+                case 2647: m_type = 3; break;
+                case 1615: m_type = 4; break;
+                case 1595: m_type = 5; break;
+                case 1612: m_type = 6; break;
+                case 1613: m_type = 7; break;
+                case 1614: m_type = 8; break;
+            }
+            theBoatDB = m_DBboat;
+            base.LoadFromDatabase(obj);
+        }
+
+        public override void SaveIntoDatabase()
+        {
+            GameServer.Database.SaveObject(theBoatDB);
+        }
+    }
 }
