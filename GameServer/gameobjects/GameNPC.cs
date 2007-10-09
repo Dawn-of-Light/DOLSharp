0f80ec9be8f1868d7b6a102faba246f0c4fe4963
ZioRed <ZioRed@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-10-09 13:09:29 +0000

    - Bugfix: attempt to solve a null-reference error in IsFriend on source/target Faction

--- GameServer/gameobjects/GameNPC.cs
+++ GameServer/gameobjects/GameNPC.cs
@@ -3810,7 +3810,10 @@ namespace DOL.GS
 		/// <returns></returns>
 		public bool IsFriend(GameNPC npc)
 		{
-            return (npc.Faction == Faction || npc.Faction.FriendFactions.Contains(Faction));
+            if (Faction == null || npc.Faction == null)
+                return false;
+            else
+                return (npc.Faction == Faction || Faction.FriendFactions.Contains(npc.Faction));
 		}
 
         private string m_boatowner_id;
