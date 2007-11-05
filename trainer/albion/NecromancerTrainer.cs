36c55ee57f6f3fc21196bf1d1ea8f486c4c598a3
aredhel77 <aredhel77@users.sourceforge.net>
2007-11-05 19:30:03 +0000

    - Changed: Made the necromancer trainer a bit chattier... err more live like
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@840 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- trainer/albion/NecromancerTrainer.cs
+++ trainer/albion/NecromancerTrainer.cs
@@ -47,37 +47,39 @@ namespace DOL.GS.Trainer
  		{		
  			if (!base.Interact(player)) return false;
 								
-			// check if class matches.				
-			if (player.CharacterClass.ID == (int) eCharacterClass.Necromancer) {
+			// If the player is a necromancer, offer training, if it is a disciple,
+			// offer a promotion. Otherwise send them somewhere else.
 
-				// popup the training window
-				player.Out.SendTrainerWindow();
-				//player.Out.SendMessage(this.Name + " says, \"Select what you like to train.\"", eChatType.CT_Say, eChatLoc.CL_PopupWindow);												
-
-			} else {
-				// perhaps player can be promoted
-				if (CanPromotePlayer(player)) {
-					player.Out.SendMessage(this.Name + " says, \"Do you desire to [join the Temple of Arawn] and defend our realm as a Necromancer?\"",eChatType.CT_Say,eChatLoc.CL_PopupWindow);
-				} else {
-					player.Out.SendMessage(this.Name + " says, \"You must seek elsewhere for your training.\"", eChatType.CT_Say, eChatLoc.CL_ChatWindow);							
-				}
+			if (player.CharacterClass.ID == (int) eCharacterClass.Necromancer) 
+				OfferTraining(player);
+            else 
+            {
+				if (CanPromotePlayer(player)) 
+                {
+                    String message = "Hail, young Disciple. Have you come seeking to imbue yourself with the power of ";
+                    message += "Arawn and serve as one of his [Necromancers]?";
+                    WhisperPlayer(player, message, eChatLoc.CL_PopupWindow);
+				} 
+                else 
+                    DismissPlayer(player);					
 			}
 			return true;
  		}
 
 		/// <summary>
-		/// checks wether a player can be promoted or not
+		/// Checks wether a player can be promoted or not.
 		/// </summary>
 		/// <param name="player"></param>
 		/// <returns></returns>
 		public override bool CanPromotePlayer(GamePlayer player)
 		{
-			return (player.Level>=5 && player.CharacterClass.ID == (int) eCharacterClass.Disciple && (player.Race == (int) eRace.Briton
-   				|| player.Race == (int) eRace.Saracen || player.Race == (int) eRace.Inconnu));
+			return (player.Level >= 5 && player.CharacterClass.ID == (int) eCharacterClass.Disciple 
+                && (player.Race == (int) eRace.Briton || player.Race == (int) eRace.Saracen 
+                || player.Race == (int) eRace.Inconnu));
 		}
 
 		/// <summary>
-		/// Talk to trainer
+		/// Talk to the trainer.
 		/// </summary>
 		/// <param name="source"></param>
 		/// <param name="text"></param>
@@ -87,12 +89,29 @@ namespace DOL.GS.Trainer
 			if (!base.WhisperReceive(source, text)) return false;			
 			GamePlayer player = source as GamePlayer;			
 	
-			switch (text) {
-			case "join the Temple of Arawn":
-				// promote player to other class
-				if (CanPromotePlayer(player)) {
-					PromotePlayer(player, (int)eCharacterClass.Necromancer, "Lord Arawn has accepted you into his Temple. Here is his gift to you. Use it well, Disciple.", null);
-					player.ReceiveItem(this,WEAPON_ID);
+			switch (text.ToLower()) 
+            {
+                case "necromancers":
+                    String message = "The necromancer is a cloth wearing priest of Arawn, lord of the underworld. ";
+                    message += "Due to their allegiance to the dark master, they are granted spellcasting and combat ";
+                    message += "prowess that is unique in Albion. Unlike other casters, a Necromancer is powerless ";
+                    message += "until it calls upon the dark magic of Arawn to transform itself into a fearsome ";
+                    message += "servant, controlled by an ethereal \"shade\". As a shade, the Necro appears as a ";
+                    message += "translucent shadowy, floating ghost that cannot be wounded by melee or spells while ";
+                    message += "the servant carries out their dark commands. When the servant is slain or released ";
+                    message += "in combat, the Necromancer returns to mortal form, alive, but with a very small ";
+                    message += "amount of health. Necromancers have the power to wound their enemies and the pain ";
+                    message += "they inflict brings them a surge of power or restores life to their servant. Do you ";
+                    message += "wish to dedicate yourself to Lord Arawn and [join the Temple of Arawn]?";
+                    WhisperPlayer(player, message, eChatLoc.CL_PopupWindow);
+                    break;
+
+			case "join the temple of arawn":
+				if (CanPromotePlayer(player)) 
+                {
+					PromotePlayer(player, (int)eCharacterClass.Necromancer, 
+                        "Lord Arawn has accepted you into his Temple. Here is his gift to you. Use it well, Disciple.", null);
+					player.ReceiveItem(this, WEAPON_ID);
 				}
 				break;
 			}
