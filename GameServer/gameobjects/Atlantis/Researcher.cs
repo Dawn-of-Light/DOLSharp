8210e173a46de93501e28b0ea73e09d87d00d4fe
aredhel77 <aredhel77@users.sourceforge.net>
2007-11-15 22:04:36 +0000

    - Bug Fix: Combine should now work properly again
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@860 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/gameobjects/Atlantis/Researcher.cs
+++ GameServer/gameobjects/Atlantis/Researcher.cs
@@ -39,15 +39,19 @@ namespace DOL.GS
             GameNpcInventoryTemplate template = new GameNpcInventoryTemplate();
 			switch (Realm)
 			{
-				case 1: template.AddNPCEquipment(eInventorySlot.TorsoArmor, 2230); break;
-				case 2: template.AddNPCEquipment(eInventorySlot.TorsoArmor, 2232);
-						template.AddNPCEquipment(eInventorySlot.ArmsArmor, 2233);
-						template.AddNPCEquipment(eInventorySlot.LegsArmor, 2234);
-						template.AddNPCEquipment(eInventorySlot.HandsArmor, 2235);
-						template.AddNPCEquipment(eInventorySlot.FeetArmor, 2236);
+				case 1: 
+					template.AddNPCEquipment(eInventorySlot.TorsoArmor, 2230); break;
+				case 2: 
+					template.AddNPCEquipment(eInventorySlot.TorsoArmor, 2232);
+					template.AddNPCEquipment(eInventorySlot.ArmsArmor, 2233);
+					template.AddNPCEquipment(eInventorySlot.LegsArmor, 2234);
+					template.AddNPCEquipment(eInventorySlot.HandsArmor, 2235);
+					template.AddNPCEquipment(eInventorySlot.FeetArmor, 2236);
 					break;
-				case 3: template.AddNPCEquipment(eInventorySlot.TorsoArmor, 2231); ; break;
+				case 3: 
+					template.AddNPCEquipment(eInventorySlot.TorsoArmor, 2231); ; break;
 			}
+
             Inventory = template.CloseTemplate();
             Flags = 16;	// Peace flag.
             return base.AddToWorld();
