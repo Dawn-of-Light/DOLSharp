d470886dd84a510e719294cd016b2e8894248e39
Urzaria <Urzaria@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-11-16 18:23:17 +0000

    - Bugfix: name of gravestone

--- GameServer/gameobjects/GameGravestone.cs
+++ GameServer/gameobjects/GameGravestone.cs
@@ -43,7 +43,7 @@ namespace DOL.GS
 			//as standard! We want our mobs/items etc. at
 			//the same startingspots when we restart!
 			m_saveInDB = false;
-			m_Name = LanguageMgr.GetTranslation(player.Client, "GameLiving.ReceiveItem", player.Name);
+			m_Name = LanguageMgr.GetTranslation(player.Client, "GameGravestone.GameGravestone.Grave", player.Name);
 			m_Heading = player.Heading;
 			m_X=player.X;
 			m_Y=player.Y;
