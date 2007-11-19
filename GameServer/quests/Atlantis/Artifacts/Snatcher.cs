2657ab0c2c3f887799d9882724086d5d4cc4cdd9
aredhel77 <aredhel77@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-11-19 18:37:40 +0000

    - Bug Fix: Files/folders for artifact quests and encounters resored
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@867 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/quests/Atlantis/Artifacts/Snatcher.cs
@@ -0,0 +1,73 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DOL.Events;
+using DOL.GS.Quests;
+using DOL.Database;
+
+namespace DOL.GS.Quests.Atlantis.Artifacts
+{
+	/// <summary>
+	/// Quest for the Snatcher artifact.
+	/// </summary>
+	/// <author>Aredhel</author>
+	class Snatcher : ArtifactQuest
+	{
+		public Snatcher()
+			: base() { }
+
+		public Snatcher(GamePlayer questingPlayer)
+			: base(questingPlayer) { }
+
+		/// <summary>
+		/// This constructor is needed to load quests from the DB.
+		/// </summary>
+		/// <param name="questingPlayer"></param>
+		/// <param name="dbQuest"></param>
+		public Snatcher(GamePlayer questingPlayer, DBQuest dbQuest)
+			: base(questingPlayer, dbQuest) { }
+
+		/// <summary>
+		/// Quest initialisation.
+		/// </summary>
+		public static void Init()
+		{
+			ArtifactQuest.Init("Snatcher", typeof(Snatcher));
+		}
+
+		/// <summary>
+		/// The name of the quest (not necessarily the same as
+		/// the name of the reward).
+		/// </summary>
+		public override string Name
+		{
+			get { return "Snatcher"; }
+		}
+
+		/// <summary>
+		/// The reward for this quest.
+		/// </summary>
+		public override String ArtifactID
+		{
+			get { return "Snatcher"; }
+		}
+	}
+}
