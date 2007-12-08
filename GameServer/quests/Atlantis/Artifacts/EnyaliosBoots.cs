e5ae18aab4ccec0ca6775b8a0daba154a06612ef
aredhel77 <aredhel77@users.sourceforge.net>
2007-12-08 14:52:57 +0000

    - Added: Alvarus Leggings, Enyalios Boots
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@901 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/quests/Atlantis/Artifacts/EnyaliosBoots.cs
@@ -0,0 +1,175 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DOL.Events;
+using DOL.GS.Quests;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using System.Collections;
+
+namespace DOL.GS.Quests.Atlantis.Artifacts
+{
+	/// <summary>
+	/// Quest for the Enyalios' Boots artifact.
+	/// </summary>
+	/// <author>Aredhel</author>
+	class EnyaliosBoots : ArtifactQuest
+	{
+		public EnyaliosBoots()
+			: base() { }
+
+		public EnyaliosBoots(GamePlayer questingPlayer)
+			: base(questingPlayer) { }
+
+		/// <summary>
+		/// This constructor is needed to load quests from the DB.
+		/// </summary>
+		/// <param name="questingPlayer"></param>
+		/// <param name="dbQuest"></param>
+		public EnyaliosBoots(GamePlayer questingPlayer, DBQuest dbQuest)
+			: base(questingPlayer, dbQuest) { }
+
+		/// <summary>
+		/// Quest initialisation.
+		/// </summary>
+		public static void Init()
+		{
+			ArtifactQuest.Init("Enyalio's Boots", typeof(EnyaliosBoots));
+		}
+
+		/// <summary>
+		/// Handle an item given to the scholar.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="item"></param>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		public override bool ReceiveItem(GameLiving source, GameLiving target, InventoryItem item)
+		{
+			if (base.ReceiveItem(source, target, item))
+				return true;
+
+			GamePlayer player = source as GamePlayer;
+			Scholar scholar = target as Scholar;
+			if (player == null || scholar == null)
+				return false;
+
+			if (Step == 2 && ArtifactMgr.GetArtifactID(item.Name) == ArtifactID)
+			{
+				String armorType = GlobalConstants.ArmorLevelToName(player.BestArmorLevel,
+					(eRealm)player.Realm);
+				ItemTemplate template = null;
+				Dictionary<String, ItemTemplate> versions = ArtifactMgr.GetArtifactVersions(ArtifactID,
+					(eCharacterClass)player.CharacterClass.ID, (eRealm)player.Realm);
+
+				foreach (String versionKey in versions.Keys)
+				{
+					String[] serializedKey = versionKey.Split(';');
+					if (serializedKey[0] == armorType)
+					{
+						template = versions[versionKey];
+						break;
+					}
+				}
+
+				if (template != null && RemoveItem(player, item))
+				{
+					GiveItem(scholar, player, ArtifactID, template);
+					String reply = String.Format("Here, take these boots! I hope you find {0}",
+						"them useful. Don't lose them, I can only unlock their power for you once!");
+					scholar.TurnTo(player);
+					scholar.SayTo(player, eChatLoc.CL_PopupWindow, reply);
+					FinishQuest();
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Handle whispers to the scholar.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="target"></param>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public override bool WhisperReceive(GameLiving source, GameLiving target, string text)
+		{
+			if (base.WhisperReceive(source, target, text))
+				return true;
+
+			GamePlayer player = source as GamePlayer;
+			Scholar scholar = target as Scholar;
+			if (player == null || scholar == null)
+				return false;
+
+			if (Step == 1 && text.ToLower() == ArtifactID.ToLower())
+			{
+				String reply = String.Format("Do you have the story that goes along with that? {0} {1}",
+					"If you do, hand it in to me, and I will unlock the power that these boots had",
+					"been vested with. If not, go get the story!");
+				scholar.TurnTo(player);
+				scholar.SayTo(player, eChatLoc.CL_PopupWindow, reply);
+				Step = 2;
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Description for the current step.
+		/// </summary>
+		public override string Description
+		{
+			get
+			{
+				switch (Step)
+				{
+					case 1:
+						return "Defeat Behrooz.";
+					case 2:
+						return "Turn in the completed book.";
+					default:
+						return base.Description;
+				}
+			}
+		}
+
+		/// <summary>
+		/// The name of the quest (not necessarily the same as
+		/// the name of the reward).
+		/// </summary>
+		public override string Name
+		{
+			get { return "Enyalios' Boots"; }
+		}
+
+		/// <summary>
+		/// The reward for this quest.
+		/// </summary>
+		public override String ArtifactID
+		{
+			get { return "Enyalio's Boots"; }
+		}
+	}
+}
