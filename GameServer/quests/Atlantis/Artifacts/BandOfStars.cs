a6809ce74a0b4ee9960c2b1e97cc201fece901c5
aredhel77 <aredhel77@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-12-15 13:49:03 +0000

    - Changed: GameNPC will automatically turn to the person they have something to say to - Added: Encounters and Quests for the following artifacts: Arms of the Winds, Aten's Shield, Band of Stars, Bruiser, Shield of Khaos
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@913 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/quests/Atlantis/Artifacts/BandOfStars.cs
@@ -0,0 +1,170 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DOL.Events;
+using DOL.GS.Quests;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using System.Collections;
+
+namespace DOL.GS.Quests.Atlantis.Artifacts
+{
+	/// <summary>
+	/// Quest for the Band of Stars artifact.
+	/// </summary>
+	/// <author>Aredhel</author>
+	class BandOfStars : ArtifactQuest
+	{
+		public BandOfStars()
+			: base() { }
+
+		public BandOfStars(GamePlayer questingPlayer)
+			: base(questingPlayer) { }
+
+		/// <summary>
+		/// This constructor is needed to load quests from the DB.
+		/// </summary>
+		/// <param name="questingPlayer"></param>
+		/// <param name="dbQuest"></param>
+		public BandOfStars(GamePlayer questingPlayer, DBQuest dbQuest)
+			: base(questingPlayer, dbQuest) { }
+
+		/// <summary>
+		/// Quest initialisation.
+		/// </summary>
+		public static void Init()
+		{
+			ArtifactQuest.Init("Band of Stars", typeof(BandOfStars));
+		}
+
+		/// <summary>
+		/// Handle an item given to the scholar.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="item"></param>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		public override bool ReceiveItem(GameLiving source, GameLiving target, InventoryItem item)
+		{
+			if (base.ReceiveItem(source, target, item))
+				return true;
+
+			GamePlayer player = source as GamePlayer;
+			Scholar scholar = target as Scholar;
+			if (player == null || scholar == null)
+				return false;
+
+			if (Step == 2 && ArtifactMgr.GetArtifactID(item.Name) == ArtifactID)
+			{
+				Dictionary<String, ItemTemplate> versions = ArtifactMgr.GetArtifactVersions(ArtifactID,
+					(eCharacterClass)player.CharacterClass.ID, (eRealm)player.Realm);
+
+				if (versions.Count > 0 && RemoveItem(player, item))
+				{
+					GiveItem(scholar, player, ArtifactID, versions[";;"]);
+					String reply = String.Format("Do you see how the stars in the {0} {1}? {2} {3} {4} {5} {6} {7}.",
+						"band have begun to glow, ",
+						player.CharacterClass.Name,
+						"That is because the magic of the bracelet is active again. It shall flow through",
+						"the Band and into you, then back into the Band. Because of this flow, you may not",
+						"give the bracelet to anyone else. Destroying the Bracelet will end the flow of",
+						"magic and once stopped, it cannot be restarted again. May the Band of Stars guide",
+						"you to success in the trials, ",
+						player.CharacterClass.Name);
+					scholar.TurnTo(player);
+					scholar.SayTo(player, eChatLoc.CL_PopupWindow, reply);
+					FinishQuest();
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Handle whispers to the scholar.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="target"></param>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public override bool WhisperReceive(GameLiving source, GameLiving target, string text)
+		{
+			if (base.WhisperReceive(source, target, text))
+				return true;
+
+			GamePlayer player = source as GamePlayer;
+			Scholar scholar = target as Scholar;
+			if (player == null || scholar == null)
+				return false;
+
+			if (Step == 1 && text.ToLower() == ArtifactID.ToLower())
+			{
+				String reply = String.Format("This bracelet was quite beautiful and powerful. {0} {1} {2}",
+					"Let's get that power flowing again! Give me the King's Vase and I shall examine it.",
+					"Once I have uncovered the secrets in the Vase, I shall make the Band of Stars",
+					"available to you.");
+				scholar.TurnTo(player);
+				scholar.SayTo(player, eChatLoc.CL_PopupWindow, reply);
+				Step = 2;
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Description for the current step.
+		/// </summary>
+		public override string Description
+		{
+			get
+			{
+				switch (Step)
+				{
+					case 1:
+						return "Defeat Chisisi.";
+					case 2:
+						return "Turn in the King's Vase in the Stygia Haven or in the Hall of Heroes.";
+					default:
+						return base.Description;
+				}
+			}
+		}
+
+		/// <summary>
+		/// The name of the quest (not necessarily the same as
+		/// the name of the reward).
+		/// </summary>
+		public override string Name
+		{
+			get { return "Band of Stars"; }
+		}
+
+		/// <summary>
+		/// The reward for this quest.
+		/// </summary>
+		public override String ArtifactID
+		{
+			get { return "Band of Stars"; }
+		}
+	}
+}
