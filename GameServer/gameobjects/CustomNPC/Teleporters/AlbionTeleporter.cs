c5f60265a00d8ead1fd2de3fbc0614f8d19c1d87
aredhel77 <aredhel77@users.sourceforge.net>
2007-12-16 16:37:36 +0000

    - Added: Basic support for teleporter NPCs - Added: Albion teleporter
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@915 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/gameobjects/CustomNPC/Teleporters/AlbionTeleporter.cs
@@ -0,0 +1,177 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DOL.GS;
+using DOL.Database;
+using System.Collections;
+using DOL.GS.Spells;
+using log4net;
+using System.Reflection;
+
+namespace DOL.GS
+{
+	/// <summary>
+	/// Albion teleporter.
+	/// </summary>
+	/// <author>Aredhel</author>
+	public class AlbionTeleporter : GameTeleporter
+	{
+		/// <summary>
+		/// Defines a logger for this class.
+		/// </summary>
+		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		/// <summary>
+		/// Add equipment to the teleporter.
+		/// </summary>
+		/// <returns></returns>
+		public override bool AddToWorld()
+		{
+			GameNpcInventoryTemplate template = new GameNpcInventoryTemplate();
+			template.AddNPCEquipment(eInventorySlot.Cloak, 57, 66);
+			template.AddNPCEquipment(eInventorySlot.TorsoArmor, 1005, 86);
+			template.AddNPCEquipment(eInventorySlot.LegsArmor, 140, 6);
+			template.AddNPCEquipment(eInventorySlot.ArmsArmor, 141, 6);
+			template.AddNPCEquipment(eInventorySlot.HandsArmor, 142, 6);
+			template.AddNPCEquipment(eInventorySlot.FeetArmor, 143, 6);
+			template.AddNPCEquipment(eInventorySlot.TwoHandWeapon, 1166);
+			Inventory = template.CloseTemplate();
+
+			SwitchWeapon(GameLiving.eActiveWeaponSlot.TwoHanded);
+			return base.AddToWorld();
+		}
+
+		/// <summary>
+		/// Player right-clicked the teleporter.
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns></returns>
+		public override bool Interact(GamePlayer player)
+		{
+			if (!base.Interact(player))
+				return false;
+
+			String intro = String.Format("Greetings. I can channel the energies of this place to send you {0} {1} {2} {3} {4} {5}",
+				"to far away lands. If you wish to fight in the Frontiers I can send you to [Forest Sauvage] or to the",
+				"border keeps [Castle Sauvage] and [Snowdonia Fortress]. Maybe you wish to undertake the Trials of",
+				"Atlantis in [Oceanus] haven or wish to visit the harbor of [Gothwaite] and the [Shrouded Isles]?",
+				"You could explore the [Avalon Marsh] or perhaps you would prefer the comforts of the [housing] regions.",
+				"Perhaps the fierce [Battlegrounds] are more to your liking or do you wish to meet the citizens inside",
+				"the great city of [Camelot] or the [Inconnu Crypt]?");
+			SayTo(player, intro);
+			return true;
+		}
+
+		/// <summary>
+		/// Player has picked a subselection.
+		/// </summary>
+		/// <param name="player"></param>
+		/// <param name="subSelection"></param>
+		protected override void OnSubSelectionPicked(GamePlayer player, Teleport subSelection)
+		{
+			switch (subSelection.TeleportID.ToLower())
+			{
+				case "shrouded isles":
+					{
+						String reply = String.Format("The isles of Avalon are an excellent choice. {0} {1}",
+							"Would you prefer the harbor of [Gothwaite] or perhaps one of the outlying towns",
+							"like [Wearyall] Village, Fort [Gwyntell], or Caer [Diogel]?");
+						SayTo(player, reply);
+						return;
+					}
+				case "housing":
+					{
+						String reply = String.Format("I can send you to your [personal] house. If you do {0} {1} {2} {3}",
+							"not have a personal house or wish to be sent to the housing [entrance] then you will",
+							"arrive just inside the housing area. I can also send you to your [guild] house. If your",
+							"guild does not own a house then you will not be transported. You may go to your [Hearth] bind",
+							"as well if you are bound inside a house.");
+						SayTo(player, reply);
+						return;
+					}
+			}
+			base.OnSubSelectionPicked(player, subSelection);
+		}
+
+		/// <summary>
+		/// Player has picked a destination.
+		/// </summary>
+		/// <param name="player"></param>
+		/// <param name="destination"></param>
+		protected override void OnDestinationPicked(GamePlayer player, Teleport destination)
+		{
+			switch (destination.TeleportID.ToLower())
+			{
+				case "avalon marsh":
+					SayTo(player, "You shall soon arrive in the Avalon Marsh.");
+					break;
+				case "camelot":
+					SayTo(player, "The great city awaits!");
+					break;
+				case "castle sauvage":
+					SayTo(player, "Castle Sauvage is what you seek, and Castle Sauvage is what you shall find.");
+					break;
+				case "forest sauvage":
+					SayTo(player, "Now to the Frontiers for the glory of the realm!");
+					break;
+				case "gothwaite":
+					SayTo(player, "The Shrouded Isles await you.");
+					break;
+				case "oceanus":
+					SayTo(player, "You will soon arrive in the Haven of Oceanus.");
+					break;
+				case "snowdonia fortress":
+					SayTo(player, "Snowdonia Fortress is what you seek, and Snowdonia Fortress is what you shall find.");
+					break;
+				default:
+					SayTo(player, "This destination is not yet supported.");
+					return;
+			}
+			base.OnDestinationPicked(player, destination);
+		}
+
+		/// <summary>
+		/// Teleport the player to the designated coordinates.
+		/// </summary>
+		/// <param name="player"></param>
+		/// <param name="destination"></param>
+		protected override void OnTeleport(GamePlayer player, Teleport destination)
+		{
+			SpellLine spellLine = SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells);
+			IList spellList = SkillBase.GetSpellList(GlobalSpellsLines.Mob_Spells);
+			foreach (Spell spell in spellList)
+			{
+				if (spell.SpellType == "UniPortal")
+				{
+					TargetObject = player;
+					UniPortal portalHandler = new UniPortal(this, spell, spellLine, destination);
+					portalHandler.CastSpell();
+					return;
+				}
+			}
+
+			// Spell not found in the database, fall back on default procedure.
+
+			log.Warn("UniPortal spell not found");
+			base.OnTeleport(player, destination);
+		}
+	}
+}
---
+++ GameServer/spells/UniPortal.cs
@@ -0,0 +1,72 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DOL.GS.Effects;
+using DOL.Database;
+
+namespace DOL.GS.Spells
+{
+	/// <summary>
+	/// The spell used by classic teleporters.
+	/// </summary>
+	/// <author>Aredhel</author>
+	[SpellHandlerAttribute("UniPortal")]
+	public class UniPortal : SpellHandler
+	{
+		private Teleport m_destination;
+
+		public UniPortal(GameLiving caster, Spell spell, SpellLine spellLine, Teleport destination)
+			: base(caster, spell, spellLine) 
+		{
+			m_destination = destination;
+		}
+
+		/// <summary>
+		/// Whether this spell can be cast on the selected target at all.
+		/// </summary>
+		/// <param name="selectedTarget"></param>
+		/// <returns></returns>
+		public override bool CheckBeginCast(GameLiving selectedTarget)
+		{
+			if (!base.CheckBeginCast(selectedTarget))
+				return false;
+			return (selectedTarget is GamePlayer);
+		}
+
+		/// <summary>
+		/// Apply the effect.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="effectiveness"></param>
+		public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
+		{
+			GamePlayer player = target as GamePlayer;
+			if (player == null)
+				return;
+
+			UniPortalEffect effect = new UniPortalEffect(1000);
+			effect.Start(player);
+			player.MoveTo((ushort)m_destination.RegionID,
+				m_destination.X, m_destination.Y, m_destination.Z,
+				(ushort)m_destination.Heading);
+		}
+	}
+}
