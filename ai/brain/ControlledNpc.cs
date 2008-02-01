3e32e8eedbc476ea9e82090b0dce32d8bb8f7477
istandalonetoo <istandalonetoo@users.sourceforge.net>
2008-02-01 07:07:20 +0000

    - Bugfix: The Charge effect should be properly disabled - Added: Mauler chance to have a left hand swing chance - Removed: Maulers should no longer keep slash, crush, etc - Added: all GameLivings will now have realm ability recast times - Changed: The way AbstractServerRules handles npc deaths.  The changes should fix many problems regarding with damage percent and the inability to get full experience.  Some editting may be in store to fine tune the code, but now it should be much cleaner and work much better. - Changed: Buffs now check if they're the types that a vamp can't have instead of the buffs that they can have - Bugfix: FW styles now require two FW weapons
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@1047 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- ai/brain/ControlledNpc.cs
+++ ai/brain/ControlledNpc.cs
@@ -376,7 +376,7 @@ namespace DOL.AI.Brain
 								if (WorldMgr.GetDistance(Body.TargetObject, Body) >= 500)
 								{
 									ChargeAbility charge = Body.GetAbility(typeof(ChargeAbility)) as ChargeAbility;
-									if (charge != null && Body.GetSkillDisabledDuration(charge) == 0)
+									if (charge != null && Body.GetSkillDisabledDuration(charge) <= 0)
 									{
 										charge.Execute(Body);
 									}
