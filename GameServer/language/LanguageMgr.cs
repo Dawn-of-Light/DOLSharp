04a042bf700eec05b55f7c2cfa6806c3baf83706
graveen <graveen@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2008-03-13 19:14:02 +0000

    Contribution from Andriy: - Added: Language manager has now multiples files for better & more efficient localization; look in language/EN to have an idea
    Contribution from me:
    - Changed: /item can now handle platinum price part of an item
    - Added: Epics quests are now checking if mauler's parts are existing in database

--- GameServer/language/LanguageMgr.cs
+++ GameServer/language/LanguageMgr.cs
@@ -1,167 +1,247 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
+using System.Collections;
+using System.Reflection;
 using System.Text;
 using System.IO;
-using System.Collections;
+
 using DOL.GS;
-// Log
-using System.Reflection;
 using log4net;
 
 namespace DOL.Language
 {
 	public class LanguageMgr
 	{
+		/// <summary>
+		/// Defines a logger for this class.
+		/// </summary>
 		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-		// All the sentences, by language
+		
+
+		/// <summary>
+		/// All the sentences, by language
+		/// </summary>
 		private static Hashtable[] LangsSentences;
 		
-		// Optimisation, instead of read all the hastable to find the language, we have a table with ShortName->Langs
-		private static Hashtable m_NameToLangs;
+
+		/// <summary>
+		/// Optimisation, instead of read all the hastable to find the language, we have a table with ShortName->Langs
+		/// </summary>
+		private static Hashtable m_NameToLangs = new Hashtable();
 		
+
+		/// <summary>
+		/// All language enumerator
+		/// </summary>
 		public enum Langs : byte 
 		{
-			EN = 0,
-			FR = 1,
-			DE = 2,
-			ES = 3,
-			CZ = 4,
-			IT = 5,
+			EN,
+			IT,
+			DE,
+			FR,
+			CZ,
+			ES,
 			Langs_size
 		};
 
-		private static bool LoadLanguage(Langs l, string abrev, string longname)
+
+		/// <summary>
+		/// Count files in a language directory
+		/// </summary>
+		/// <param name="abrev"></param>
+		/// <returns></returns>
+		private static int CountLanguageFiles(string abrev)
 		{
-			string[] lines;
-			string FilePath = "." + Path.DirectorySeparatorChar + "languages" + Path.DirectorySeparatorChar + "Language-";
-			FilePath += abrev;
-			FilePath += ".txt";
-			LangsSentences[(int)l] = new Hashtable();
-			if (!File.Exists(FilePath))
+			int count = 0;
+			string LangPath = "." + Path.DirectorySeparatorChar + "languages" + Path.DirectorySeparatorChar + abrev + Path.DirectorySeparatorChar;
+			
+			if (!Directory.Exists(LangPath))
+				return count;
+
+			foreach (string file in Directory.GetFiles(LangPath, "*", SearchOption.AllDirectories))
 			{
-				log.Error("Language file : " + FilePath + " not found !");
+				if (!file.EndsWith(".txt"))
+					continue;
+				count++;
+			}
+
+			return count;
+		}
+
+
+		/// <summary>
+		/// Load language files
+		/// </summary>
+		/// <param name="lng"></param>
+		/// <param name="abrev"></param>
+		/// <param name="longname"></param>
+		/// <returns></returns>
+		private static bool LoadLanguage(Langs lng, string abrev, string longname)
+		{
+			LangsSentences[(int)lng] = new Hashtable();
+
+			if (CountLanguageFiles(abrev) == 0)
+			{
+				log.Error(longname + " language not found!");
 				if (DOL.GS.ServerProperties.Properties.SERV_LANGUAGE == abrev)
 				{
-					log.Error("Default Language file : " + FilePath + " missing !! Server can't start without !");
-					return false;
+					log.Error("Default " + longname + " language files missing!! Server can't start without!");
+					if (GameServer.Instance != null)
+						GameServer.Instance.Stop();
 				}
 				return false;
 			}
-			lines = File.ReadAllLines(FilePath, Encoding.GetEncoding(1252));
 
-			IList textList = new ArrayList(lines);
-			foreach (string line in textList)
+			string LangPath = "." + Path.DirectorySeparatorChar + "languages" + Path.DirectorySeparatorChar + abrev + Path.DirectorySeparatorChar;
+			foreach (string FilePath in Directory.GetFiles(LangPath, "*", SearchOption.AllDirectories))
 			{
-				if (line.StartsWith("#"))
-					continue;
-				if (line.IndexOf(':') == -1)
+				if (!FilePath.EndsWith(".txt"))
 					continue;
 
-                // Maybe use Regex here ?
-                string[] splitted = new string[2];
-				splitted[0] = line.Substring(0, line.IndexOf(':'));
-				splitted[1] = line.Substring(line.IndexOf(':') + 1);
+				string[] lines = File.ReadAllLines(FilePath, Encoding.GetEncoding(1252));
+				IList textList = new ArrayList(lines);
+				
+				foreach (string line in textList)
+				{
+					if (line.StartsWith("#"))
+						continue;
 
-				splitted[1] = splitted[1].Replace("\t", "");
-				//log.Debug("Index : " + splitted[0] + " sentence : '" + splitted[1] + "'");
-				LangsSentences[(int)l][splitted[0]] = splitted[1];
+					if (line.IndexOf(':') == -1)
+						continue;
+
+					string[] splitted = new string[2];
+
+					splitted[0] = line.Substring(0, line.IndexOf(':'));
+					splitted[1] = line.Substring(line.IndexOf(':') + 1);
+					
+					splitted[1] = splitted[1].Replace("\t", " ");
+					splitted[1] = splitted[1].Trim();
+
+					LangsSentences[(int)lng][splitted[0]] = splitted[1];
+				}
 			}
-			// Now we can set the short/long name
-			LangsSentences[(int) l]["SHORT_NAME"] = abrev;
-			// long name must be in english !
-			LangsSentences[(int)l]["LONG_NAME"] = longname;
-			m_NameToLangs[abrev] = l;
+			
+			LangsSentences[(int)lng]["SHORT_NAME"] = abrev;
+			LangsSentences[(int)lng]["LONG_NAME"] = longname;
+			m_NameToLangs[abrev] = lng;
+			
 			return true;
 		}
-		
+
+
+		/// <summary>
+		/// Initial function
+		/// </summary>
+		/// <returns></returns>
 		public static bool Init()
 		{
 			LangsSentences = new Hashtable[(int)Langs.Langs_size];
-			m_NameToLangs = new Hashtable();
 			
-			// Here you can add new languages, just add the Langs too.
-			// Full name must be in english !
 			LoadLanguage(Langs.EN, "EN", "English");
-			LoadLanguage(Langs.FR, "FR", "French");
+			LoadLanguage(Langs.IT, "IT", "Italian");
+			LoadLanguage(Langs.FR, "FR", "French");	
 			LoadLanguage(Langs.DE, "DE", "German");
 			LoadLanguage(Langs.ES, "ES", "Spanish");
 			LoadLanguage(Langs.CZ, "CZ", "Czech");
-			LoadLanguage(Langs.IT, "IT", "Italian");
-			
+					
 			return true;
 		}
 
+
+		/// <summary>
+		/// Convert the shortname into enumerator
+		/// </summary>
+		/// <param name="abrev"></param>
+		/// <returns></returns>
 		public static Langs NameToLangs(string abrev)
 		{
 			if (m_NameToLangs[abrev] == null)
-				return (Langs) m_NameToLangs["EN"]; // English default
+				return (Langs)m_NameToLangs["EN"];
+
 			return (Langs)m_NameToLangs[abrev];
 		}
 
-		public static string LangsToName(Langs l)
+
+		/// <summary>
+		/// Convert the enumerator into shortname
+		/// </summary>
+		/// <param name="l"></param>
+		/// <returns></returns>
+		public static string LangsToName(Langs lng)
 		{
-			return (string)LangsSentences[(int)l]["SHORT_NAME"];
+			return (string)LangsSentences[(int)lng]["SHORT_NAME"];
 		}
 
-		public static string LangsToCompleteName(GameClient c, Langs l)
+
+		/// <summary>
+		/// Convert the enumerator into longname
+		/// </summary>
+		/// <param name="c"></param>
+		/// <param name="l"></param>
+		/// <returns></returns>
+		public static string LangsToCompleteName(GameClient client, Langs lng)
 		{
-			string ID = (string)LangsSentences[(int) l]["LONG_NAME"];
-			return GetTranslation(c, "System.LanguagesName." + ID);
+			string ID = (string)LangsSentences[(int)lng]["LONG_NAME"];
+			return GetTranslation(client, "System.LanguagesName." + ID);
 		}
 		
+
+		/// <summary>
+		/// Translate the sentence
+		/// </summary>
+		/// <param name="lang"></param>
+		/// <param name="TranslationID"></param>
+		/// <param name="args"></param>
+		/// <returns></returns>
 		public static string GetTranslation(string lang, string TranslationID, params object [] args)
 		{
 			Langs Language = NameToLangs(lang);
-			//log.Debug("Translation requested: " + TranslationID);
 			string translated = (string)LangsSentences[(int)Language][TranslationID];
+			
 			if (translated == null && lang != DOL.GS.ServerProperties.Properties.SERV_LANGUAGE)
 			{
 				translated = (string)LangsSentences[(int)NameToLangs(DOL.GS.ServerProperties.Properties.SERV_LANGUAGE)][TranslationID];
-				log.Warn("Warning Tanslation ID : " + TranslationID + " doesn't exists in language : " + lang);
+				log.Warn("Tanslation ID: \"" + TranslationID + "\" doesn't exists in " + lang + " language.");
 			}
+
 			if (translated == null && lang != "EN")
 			{
 				translated = (string)LangsSentences[(int)NameToLangs("EN")][TranslationID];
-				log.Warn("Warning Tanslation ID : " + TranslationID + " doesn't exists in language : English");
+				log.Warn("Tanslation ID: \"" + TranslationID + "\" doesn't exists in English language.");
 			}
+
 			if (translated == null)
 			{
-				translated = "Error during translation, contact your shard admin with error name : '" + TranslationID + "'";
-				log.Error("Error during translation with  ID : " + TranslationID);
+				translated = "Error during translation, contact your shard admin with error name: \"" + TranslationID + "\". Thanks!";
+				log.Error("Error during translation with ID: " + TranslationID);
 			}
 			else
 			{
                 try
                 {
-					if (args.Length > 0) // only do this if args are passed in; this avoid errors on translated strings with {0} args used post translation
-	                    translated = string.Format(translated, args);
+					if (args.Length > 0)
+						translated = string.Format(translated, args);
                 }
                 catch
                 {
-                    log.Error("Parameters number error, ID: "+TranslationID+" (Arg count="+args.Length+")");
+                    log.Error("Parameters number error, ID: " + TranslationID + " (Arg count=" + args.Length + ").");
                 }
             }
+
 			return translated;
 		}
 
+
+		/// <summary>
+		/// Translate the sentence
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="TranslationID"></param>
+		/// <param name="args"></param>
+		/// <returns></returns>
 		public static string GetTranslation(GameClient client, string TranslationID, params object[] args)
 		{
+            if (client == null)
+                return GetTranslation(DOL.GS.ServerProperties.Properties.SERV_LANGUAGE, TranslationID, args);
+			
 			return GetTranslation(client.Account.Language, TranslationID, args);
 		}
 	}
