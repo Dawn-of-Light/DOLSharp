86ad350d57e1f1b1e1150af948058516d1f83853
supgee3 <supgee3@users.sourceforge.net>
2008-03-15 19:17:47 +0000

    Bugfix: Muzzle now works correctly. Added: Updated some guild information in the English language file.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@1123 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- commands/gmcommands/muzzle.cs
+++ commands/gmcommands/muzzle.cs
@@ -1,43 +1,33 @@
 /*
-* DAWN OF LIGHT - The first free open source DAoC server emulator
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*
-* Written by Supgee.
-* August 25, 2007.
-* changed by Ultra2k.
-* March 05, 2008.
-*/
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 using System;
-using System.IO;
-using System.Reflection;
-using System.Collections;
 using DOL.GS.PacketHandler;
-using log4net;
 
 namespace DOL.GS.Commands
 {
-    [CmdAttribute(
-    "&muzzle", //command to handle
-    ePrivLevel.GM, //minimum privelege level
-    "A command to mute players.", //command description
-    "/muzzle <on> / <off>")] //usage
-    public class MuzzleCommandHandler : ICommandHandler
+	[Cmd(
+		"&muzzle",
+		ePrivLevel.GM,
+		"Command to mute annoying players.",
+        "/muzzle <on> <off>")]
+    public class MuzzleCommandHandler : AbstractCommandHandler, ICommandHandler
     {
-        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
         public void OnCommand(GameClient client, string[] args)
         {
             if (args.Length < 2)
@@ -63,7 +53,6 @@ namespace DOL.GS.Commands
                         player.IsMuzzled = true;
                         player.Out.SendMessage("You've been muzzled by " + client.Player.Name + ".", eChatType.CT_System, eChatLoc.CL_SystemWindow);
                         client.Out.SendMessage("You've muzzled " + player.Name + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                        log.Info(client.Player.Name + " muzzled " + player.Name + ".");
                     }
                     break;
                 case "off":
@@ -77,7 +66,6 @@ namespace DOL.GS.Commands
                         player.IsMuzzled = false;
                         player.Out.SendMessage("You've been unmuzzled by " + client.Player.Name + ".", eChatType.CT_System, eChatLoc.CL_SystemWindow);
                         client.Out.SendMessage("You've unmuzzled " + player.Name + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                        log.Info(client.Player.Name + " unmuzzled " + player.Name + ".");
                     }
                     break;
                 default: break;
