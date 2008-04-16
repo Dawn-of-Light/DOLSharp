4a1dd0c65dfdf5cf8e76b3d6e88791a1334dabe9
andriy88 <andriy88@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2008-04-16 18:08:36 +0000

    - Changed: Moved CmdAttribute from GameUtilis to Commands folder. - Added: Some entries for language manager support [GM Commands]
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@1169 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/commands/gmcommands/kick.cs
+++ GameServer/commands/gmcommands/kick.cs
@@ -4,32 +4,37 @@
 
 using DOL.GS;
 using DOL.GS.PacketHandler;
+using DOL.Language;
 
 namespace DOL.GS.Commands
 {
 	[CmdAttribute(
-	  "&kick",
-	  new string[] { "&k" },
-	  ePrivLevel.GM,
-		"Kicks the player offline of whom you select. USE: KICK <NAME>",
-		 "Use: KICK <NAME>")]
+		"&kick",
+		new string[] { "&k" },
+		ePrivLevel.GM,
+		"GMCommands.Kick.Description",
+		"GMCommands.Kick.Usage")]
 	public class KickCommandHandler : AbstractCommandHandler, ICommandHandler
 	{
 		public void OnCommand(GameClient client, string[] args)
 		{
 			GameClient clientc = WorldMgr.GetClientByPlayerName(args[1], true, false);
+			
 			if (clientc == null)
 			{
-				DisplayMessage(client, "No one with that name is online to Kick!");
+				DisplayMessage(client, LanguageMgr.GetTranslation(client, "GMCommands.Kick.NoPlayerOnLine"));
 				return;
 			}
+
 			for (int i = 0; i < 8; i++)
 			{
-				string message = "You have been removed from the server";
+				string message;
 				if (client != null && client.Player != null)
-					message += " by GM " + client.Player.Name;
-				clientc.Out.SendMessage(message + "!", eChatType.CT_Help, eChatLoc.CL_SystemWindow);
-				clientc.Out.SendMessage(message + "!", eChatType.CT_Help, eChatLoc.CL_ChatWindow);
+					message = LanguageMgr.GetTranslation(clientc, "GMCommands.Kick.RemovedFromServerByGM", client.Player.Name);
+				else
+					message = LanguageMgr.GetTranslation(clientc, "GMCommands.Kick.RemovedFromServer");
+				clientc.Out.SendMessage(message, eChatType.CT_Help, eChatLoc.CL_SystemWindow);
+				clientc.Out.SendMessage(message, eChatType.CT_Help, eChatLoc.CL_ChatWindow);
 			}
 
 			clientc.Out.SendPlayerQuit(true);
