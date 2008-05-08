30ee0c2afe979b3ae3eeb633490d9aacb1386b15
bluesi <bluesi@users.sourceforge.net>
2008-05-08 23:53:05 +0000

    Changed: Cleric and Berzerker RR5 according to the 1.90 Changes. (1.90 Patch Notes: - The Berserker's RR5 ability, Ferocious Will, is now a flat 25% Absorb buff for 30 seconds. - The proc rate of the cleric's RR5, Retribution of the Faithful, has been increased to 50%.)
    Changed: The Calculation of Power Vampiirs gain from attacks. Though the calculation still isn't quite accurate, the result comes closer to live than the old one.
    
    Changed: Housing Place /Pickup ItemHandler now allows to place / pickup several weapons and shields from a players inventory to house walls.
    
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@1229 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/realmabilities/effects/rr5/FerociousWillEffect.cs
+++ GameServer/realmabilities/effects/rr5/FerociousWillEffect.cs
@@ -12,8 +12,7 @@ namespace DOL.GS.Effects
 	{
 
 		private RegionTimer ticktimer;
-		//private int absorb = 0;
-		private int m_currentBonus = 0;
+		private int m_currentBonus = 25;
 
 		public FerociousWillEffect()
 			: base(30000)
@@ -24,37 +23,19 @@ namespace DOL.GS.Effects
 		private GameLiving owner;
 
 		public override void Start(GameLiving target)
-		{
-			base.Start(target);
-			owner = target;
-			GamePlayer player = target as GamePlayer;
-			if (player != null)
-			{
-				foreach (GamePlayer p in player.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-				{
-					p.Out.SendSpellEffectAnimation(player, player, Icon, 0, false, 1);
-				}
-			}
-			m_currentBonus = 0;
-			ticktimer = new RegionTimer(target);
-			ticktimer.Callback = new RegionTimerCallback(OnTick);
-			ticktimer.Start(5000);
-
-
-		}
-
-		private int OnTick(RegionTimer timer)
-		{
-			if (m_currentBonus >= 25)
-			{
-				owner.BuffBonusCategory1[(int)eProperty.ArmorAbsorbtion] -= m_currentBonus;
-				m_currentBonus += 5;
-				owner.BuffBonusCategory1[(int)eProperty.ArmorAbsorbtion] += m_currentBonus;
-				return 5000;
-			}
-			return 0;
-
-		}
+        {
+            base.Start(target);
+            owner = target;
+            GamePlayer player = target as GamePlayer;
+            if (player != null)
+            {
+                foreach (GamePlayer p in player.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                {
+                    p.Out.SendSpellEffectAnimation(player, player, Icon, 0, false, 1);
+                }
+            }
+            owner.BuffBonusCategory1[(int)eProperty.ArmorAbsorbtion] += m_currentBonus;
+        }
 
 		public override string Name { get { return "Ferocious Will"; } }
 
@@ -76,7 +57,7 @@ namespace DOL.GS.Effects
 			get
 			{
 				ArrayList list = new ArrayList();
-				list.Add("Gives the zerker an ABS buff that ticks up by 5% every 5 seconds for a max of 25% at 25 seconds. Lasts 30 seconds total.");
+				list.Add("Gives the Berzerker an ABS buff of 25%. Lasts 30 seconds total.");
 				return list;
 			}
 		}
