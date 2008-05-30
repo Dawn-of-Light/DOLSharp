fe7535cf0ab3a389456c1c563eb83e48f8cd563f
Darwin_vico <Darwin_vico@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2008-05-30 10:55:37 +0000

    /stats complete command
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@1256 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/gameutils/PlayerStatistics.cs
+++ GameServer/gameutils/PlayerStatistics.cs
@@ -1,5 +1,22 @@
-//save bytes int -> ushort
-
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+ 
 using System;
 using System.Collections;
 using System.Reflection;
@@ -133,7 +150,8 @@ namespace DOL.GS
 			if (onlineTime.Days < 1)
 				stringOnlineTime = "Online time: " + onlineTime.Hours + " hours, " + onlineTime.Minutes + " minutes, " + onlineTime.Seconds + " seconds\n";
 
-			string message = "Statistics for " + player.Name + " this Session:\n" +
+			string message = 		"Options: /stats [ top | rp | kills | deathblows | irs | heal | rez | <name> ]\n"+
+									"Statistics for " + player.Name + " this Session:\n" +
 									"Total RP: " + stats.TotalRP + "\n" +
 									"RP earned from kills: " + stats.RealmPointsEarnedFromKills + "\n" +
 									"Kills that have earned RP: " + stats.KillsThatHaveEarnedRPs + "\n" +
