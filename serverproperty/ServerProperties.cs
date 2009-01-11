3c5d16c8d364bfea67838c5791eed261d61d918c
Graveen <graveen@users.sourceforge.net>
2009-01-11 09:10:12 +0000

    - Added: SP for allow/refuse entrance to BG
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@1490 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- serverproperty/ServerProperties.cs
+++ serverproperty/ServerProperties.cs
@@ -457,7 +457,13 @@ namespace DOL.GS.ServerProperties
         /// </summary>
         [ServerProperty("battlegroup_max_member", "Max number of members allowed in a battlegroup.", 64)]
         public static readonly int BATTLEGROUP_MAX_MEMBER;
-
+        
+        /// <summary>
+        /// This if the server battleground zones are open to players
+        /// </summary>
+        [ServerProperty("bg_zones_open", "Can the players teleport to battleground", true)]
+        public static readonly bool BG_ZONES_OPENED;
+        
         /// <summary>
         /// This enables or disables new guild dues. Live standard is 2% dues
         /// </summary>
