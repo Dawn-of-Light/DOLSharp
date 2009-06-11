365745bac3ad52263076798edd54c99464f147ac
etaewdev <etaewdev@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2009-06-11 09:15:40 +0000

    - Changed: Housing hookpoints are no longer hardcoded server side, they are now in the house_hookpointoffset table. You can find this SQL in the user releases section. Benefits of this include automatic database adding when a player tries to add a item to a hookpoint that the server doesnt know about yet.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@1698 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ DOLDatabase/DOLTables/HouseHookpointOffset.cs
@@ -0,0 +1,122 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+ 
+using System;
+
+using DOL.Database;
+using DOL.Database.Attributes;
+
+namespace DOL.Database
+{
+	[DataTable(TableName = "house_hookpointoffset")]
+	public class HouseHookpointOffset : DataObject
+	{
+		private int m_model;
+		private int m_hookpoint;
+		private int m_offX;
+		private int m_offY;
+		private int m_offZ;
+		private int m_offH;
+
+		static bool m_autoSave;
+
+		public HouseHookpointOffset()
+		{
+			m_autoSave = false;
+		}
+
+		override public bool AutoSave
+		{
+			get
+			{
+				return m_autoSave;
+			}
+			set
+			{
+				m_autoSave = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public int Model
+		{
+			get { return m_model; }
+			set
+			{
+				Dirty = true;
+				m_model = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public int Hookpoint
+		{
+			get { return m_hookpoint; }
+			set
+			{
+				Dirty = true;
+				m_hookpoint = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = true)]
+		public int OffX
+		{
+			get { return m_offX; }
+			set
+			{
+				Dirty = true;
+				m_offX = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = true)]
+		public int OffY
+		{
+			get { return m_offY; }
+			set
+			{
+				Dirty = true;
+				m_offY = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = true)]
+		public int OffZ
+		{
+			get { return m_offZ; }
+			set 
+			{ 
+				Dirty = true; 
+				m_offZ = value; 
+			}
+		}
+
+		[DataElement(AllowDbNull = true)]
+		public int OffH
+		{
+			get { return m_offH; }
+			set
+			{
+				Dirty = true;
+				m_offH = value;
+			}
+		}
+	}
+}
