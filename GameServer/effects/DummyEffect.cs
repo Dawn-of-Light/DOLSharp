104252663c5c39266a0aad33a889eebe97bafdab
aredhel77 <aredhel77@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2009-06-21 11:50:23 +0000

    - Added: Shades of Mist spell handler ("ShadesOfMist", shade morph and defensive proc) - Added: ShadeModel and CreationModel properties for GamePlayer to ease morphing in spell Handlers - Added: DummyEffect for testing purposes, constructor takes the effect id; unfortunately effect id and icon id appear to be treated differently, needs further investigation - Added: /cast effect <effect id> will now add a dummy effect to the player, the idea here is to find out which effect icon relates to which id

--- GameServer/effects/DummyEffect.cs
+++ GameServer/effects/DummyEffect.cs
@@ -1,16 +1,16 @@
-/*
+﻿/*
  * DAWN OF LIGHT - The first free open source DAoC server emulator
- *
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -18,22 +18,36 @@
  */
 using System;
 using System.Collections;
-using DOL.Language;
 
 namespace DOL.GS.Effects
 {
-	/// <summary>
-	/// Player shade effect.
-	/// </summary>
-	public class ShadeEffect : StaticEffect, IGameEffect
-	{
+    /// <summary>
+    /// Dummy effect for testing purposes (identifying buff icons and such).
+    /// </summary>
+    /// <author>Aredhel</author>
+    public class DummyEffect : TimedEffect, IGameEffect
+    {
+        /// <summary>
+        /// Create a new DummyEffect for the given effect ID.
+        /// </summary>
+        /// <param name="effectId"></param>
+        public DummyEffect(ushort effectId) : base(60000)
+        {
+            EffectId = effectId;
+        }
+
+        /// <summary>
+        /// The ID associated with this effect.
+        /// </summary>
+        public ushort EffectId { get; protected set; }
+
 		/// <summary>
-		/// The effect owner
+		/// The effect owner.
 		/// </summary>
 		GamePlayer m_player;
 
 		/// <summary>
-		/// Start the shade effect on a player.
+		/// Start the dummy effect on a player.
 		/// </summary>
 		/// <param name="living">The effect target</param>
 		public override void Start(GameLiving living)
@@ -62,36 +76,27 @@ namespace DOL.GS.Effects
 		}
 
 		/// <summary>
-		/// Cancel the effect.
-		/// </summary>
-		public override void Cancel(bool playerCancel) 
-		{
-            if (m_player != null)
-			    m_player.Shade(false);
-		}
-
-		/// <summary>
 		/// Name of the effect.
 		/// </summary>
 		public override string Name 
-        { 
-            get { return LanguageMgr.GetTranslation(m_player.Client, "Effects.ShadeEffect.Name"); } 
+        {
+            get { return String.Format("Effect #{0}", EffectId); } 
         }	
 
 		/// <summary>
-		/// Icon to show on players, can be id
+		/// Icon to show on players.
 		/// </summary>
 		public override ushort Icon 
         { 
-            get { return 0x193; } 
+            get { return EffectId; } 
         }
 		
 		/// <summary>
-		/// Delve Info
+		/// Delve information.
 		/// </summary>
 		public override IList DelveInfo 
-        { 
+        {
             get { return new ArrayList(0); } 
         }
-	}
+    }
 }
