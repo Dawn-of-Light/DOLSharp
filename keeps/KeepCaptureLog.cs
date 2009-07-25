48495065bc3302aabcc5f659be3e34b55d6986c1
tolakram <tolakram@users.sourceforge.net>
2009-07-25 10:29:04 +0000

    - Added: KeepCaptureLog left out of previous commit
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@1766 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ keeps/KeepCaptureLog.cs
@@ -0,0 +1,110 @@
+using System;
+using DOL.Database;
+using DOL.Database.Attributes;
+using DOL.Events;
+using DOL.GS;
+
+namespace DOL.Database
+{
+	[DataTable(TableName = "KeepCaptureLog")]
+	public class KeepCaptureLog : DataObject
+	{
+		long m_ID;
+		DateTime m_dateTaken = DateTime.Now;
+		string m_keepName;
+		string m_keepType;
+		int m_numEnemies;
+		int m_rpReward;
+		int m_combatTime;
+		private bool m_autoSave;
+
+		public KeepCaptureLog()
+			: base()
+		{
+		}
+
+		public override bool AutoSave
+		{
+			get { return m_autoSave; }
+			set { m_autoSave = value; }
+		}
+
+
+		[PrimaryKey(AutoIncrement = true)]
+		public long ID
+		{
+			get { return m_ID; }
+			set
+			{
+				Dirty = true;
+				m_ID = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public DateTime DateTaken
+		{
+			get { return m_dateTaken; }
+			set
+			{
+				Dirty = true;
+				m_dateTaken = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public string KeepName
+		{
+			get { return m_keepName; }
+			set
+			{
+				Dirty = true;
+				m_keepName = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public string KeepType
+		{
+			get { return m_keepType; }
+			set
+			{
+				Dirty = true;
+				m_keepType = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public int NumEnemies
+		{
+			get { return m_numEnemies; }
+			set
+			{
+				Dirty = true;
+				m_numEnemies = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public int CombatTime
+		{
+			get { return m_combatTime; }
+			set
+			{
+				Dirty = true;
+				m_combatTime = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public int RPReward
+		{
+			get { return m_rpReward; }
+			set
+			{
+				Dirty = true;
+				m_rpReward = value;
+			}
+		}
+	}
+}
