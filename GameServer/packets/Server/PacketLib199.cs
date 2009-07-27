3cf835b21262a349b54ad7d258f0301136e09b56
dunnerholl <dunnerholl@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2009-07-27 19:00:53 +0000

    Added: Version 1.99 Support
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@1770 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/packets/Server/PacketLib199.cs
+++ GameServer/packets/Server/PacketLib199.cs
@@ -24,19 +24,19 @@ using log4net;
 
 namespace DOL.GS.PacketHandler
 {
-	[PacketLib(198, GameClient.eClientVersion.Version198)]
-	public class PacketLib198 : PacketLib197
+	[PacketLib(199, GameClient.eClientVersion.Version199)]
+	public class PacketLib199 : PacketLib198
 	{
 		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
 		/// <summary>
-		/// Constructs a new PacketLib for Version 1.98 clients
+		/// Constructs a new PacketLib for Version 1.99 clients
 		/// </summary>
 		/// <param name="client">the gameclient this lib is associated with</param>
-		public PacketLib198(GameClient client)
+		public PacketLib199(GameClient client)
 			: base(client)
 		{
-			//Dunnerholl 2009-07-28: TODO CtoS_0x11 player market search got some new search parameters, they are simply appended as a few more bytes (12) need to do analysis and update handler
+			//Dunnerholl 2009-07-28: no changes found in a quick standard analysis
 		}
 	}
 }
