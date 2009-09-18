d00995451008081efeca3176595430a886bfea14
luhz <luhz@users.sourceforge.net>
2009-09-18 18:57:26 +0000

    - Bugfix: Multiple players can no longer protect the same target.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@1869 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- skillhandler/ProtectAbilityHandler.cs
+++ skillhandler/ProtectAbilityHandler.cs
@@ -77,8 +77,22 @@ namespace DOL.GS.SkillHandler
 				return;
 			}
 
-			// cancel all protect effects by this player before adding a new one
-			foreach (ProtectEffect protect in player.EffectList.GetAllOfType(typeof(ProtectEffect)))
+			// check if someone is protecting the target
+			foreach (ProtectEffect protect in protectTarget.EffectList.GetAllOfType(typeof (ProtectEffect)))
+			{
+				if (protect.ProtectTarget != protectTarget)
+					continue;
+				if (protect.ProtectSource == player)
+				{
+					protect.Cancel(false);
+					return;
+				}
+				player.Out.SendMessage(string.Format("{0} is already protecting {1}!", protect.ProtectSource.GetName(0, true), protect.ProtectTarget.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+				return;
+			}
+
+			// cancel all guard effects by this player before adding a new one
+			foreach (ProtectEffect protect in player.EffectList.GetAllOfType(typeof (ProtectEffect)))
 			{
 				if (protect.ProtectSource == player)
 					protect.Cancel(false);
