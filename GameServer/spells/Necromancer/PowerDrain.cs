39156dec47cd1be8d45e970571628f5dec82482f
Graveen <graveen@users.sourceforge.net>
2009-10-10 09:53:33 +0000

    - Changed: PowerTransfer and PowerDrain spells are now generic spells. There is no longer prereqs of spell casted by a pet. - Added: PowerTransferPet and PowerDrainPet,that must be casted by a pet - Removed: PowerTransferDisabled spellhandler
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@1933 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/spells/Necromancer/PowerDrain.cs
+++ GameServer/spells/Necromancer/PowerDrain.cs
@@ -1,16 +1,16 @@
-/*
+﻿/*
  * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -23,65 +23,30 @@ using DOL.AI.Brain;
 
 namespace DOL.GS.Spells
 {
-	/// <summary>
-	/// Handles power drain (conversion of target health to caster
-	/// power).
-	/// </summary>
-	/// <author>Aredhel</author>	
-	[SpellHandlerAttribute("PowerDrain")]
-	public class PowerDrain : DirectDamageSpellHandler
+	[SpellHandlerAttribute("PowerDrainPet")]
+	public class PowerDrainPet : PowerDrain
 	{
-		/// <summary>
-		/// Execute direct effect.
-		/// </summary>
-		/// <param name="target">Target that takes the damage.</param>
-		/// <param name="effectiveness">Effectiveness of the spell (0..1, equalling 0-100%).</param>
-		public override void OnDirectEffect(GameLiving target, double effectiveness)
+		public override void DrainPower(AttackData ad)
 		{
-			if (target == null) return;
-			if (!target.IsAlive || target.ObjectState != GameLiving.eObjectState.Active) return;
-
-			// Calculate damage to the target.
-
-			AttackData ad = CalculateDamageToTarget(target, effectiveness);
-			SendDamageMessages(ad);
-			DamageTarget(ad, true);
-		    DrainPower(ad);
-			target.StartInterruptTimer(SPELL_INTERRUPT_DURATION, ad.AttackType, Caster);
+			if ( !(m_caster is NecromancerPet))
+				return;
+			
+			base.DrainPower(ad);
 		}
-
-		/// <summary>
-		/// Use a percentage of the damage to refill caster's power.
-		/// </summary>
-		/// <param name="ad">Attack data.</param>
-		public virtual void DrainPower(AttackData ad)
+		
+		/// The power channelled through this spell goes to the owner, not the pet
+		protected override GameLiving Owner()
 		{
-			if (ad == null || !m_caster.IsAlive || !(m_caster is NecromancerPet)) 
-				return;
-
-			// The power channelled through this spell goes to the owner, not
-			// the pet.
-
-			GameLiving owner = ((Caster as NecromancerPet).Brain as IControlledBrain).Owner;
-			if (owner == null)
-				return;
-
-			int powerGain = (ad.Damage + ad.CriticalDamage) * m_spell.LifeDrainReturn / 100;
-			powerGain = owner.ChangeMana(m_caster, GameLiving.eManaChangeType.Spell, powerGain);
-
-			if (powerGain > 0)
-				MessageToOwner(String.Format("Your summon channels {0} power to you!", powerGain), eChatType.CT_Spell);
-			else
-				MessageToOwner("You cannot absorb any more power.", eChatType.CT_SpellResisted);
+			return ((Caster as NecromancerPet).Brain as IControlledBrain).Owner;
 		}
-
-        /// <summary>
-        /// Send message to owner.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="chatType"></param>
-        private void MessageToOwner(String message, eChatType chatType)
-        {
+		
+		/// <summary>
+		/// Message the pet's owner, not the pet
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="chatType"></param>
+		protected override void MessageToOwner(String message, eChatType chatType)
+		{
 			GameNPC npc = Caster as GameNPC;
 			if (npc != null)
 			{
@@ -93,15 +58,15 @@ namespace DOL.GS.Spells
 						owner.Out.SendMessage(message, chatType, eChatLoc.CL_SystemWindow);
 				}
 			}
-        }
-
+		}
+		
 		/// <summary>
-        /// Create a new handler for the power drain spell.
+		/// Create a new handler for the necro petpower drain spell.
 		/// </summary>
 		/// <param name="caster"></param>
 		/// <param name="spell"></param>
 		/// <param name="line"></param>
-		public PowerDrain(GameLiving caster, Spell spell, SpellLine line) 
-            : base(caster, spell, line) { }
+		public PowerDrainPet(GameLiving caster, Spell spell, SpellLine line)
+			: base(caster, spell, line) { }
 	}
 }
