e852a3e0abfc4a807d8ab9f3f4865351101d53d0
tolakram <tolakram@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2009-12-07 13:41:02 +0000

    - Bugfix: Do not count realm 0 guards for kill missions

--- GameServer/quests/Missions/KillMission.cs
+++ GameServer/quests/Missions/KillMission.cs
@@ -27,7 +27,8 @@ namespace DOL.GS.Quests
 			EnemyKilledEventArgs eargs = args as EnemyKilledEventArgs;
 
 			//we don't want mission masters to be considered realm guards because they are insta respawn
-			if (eargs.Target is Keeps.MissionMaster)
+			//in addition do not count realm 0 guards
+			if (eargs.Target is Keeps.MissionMaster || eargs.Target.Realm == eRealm.None)
 				return;
 
 			if (m_targetType.IsInstanceOfType(eargs.Target) == false)
