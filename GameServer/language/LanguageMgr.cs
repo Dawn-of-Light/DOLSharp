2d5ebd2d96af438937407651bc055ceeaacd77d2
Graveen <graveen@users.sourceforge.net>
2010-01-23 15:26:11 +0000

    - Fixed: language files got duplicate crlf after each text line
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2121 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/language/LanguageMgr.cs
+++ GameServer/language/LanguageMgr.cs
@@ -1,3 +1,21 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 using System.Collections;
 using System.Reflection;
 using System.Text;
@@ -206,7 +224,7 @@ namespace DOL.Language
 			if ((translated == null) && (lang != "EN") && (DOL.GS.ServerProperties.Properties.SERV_LANGUAGE != "EN"))
 			{
 				translated = (string)LangsSentences[(int)NameToLangs("EN")][TranslationID];
-				//log.Warn("Tanslation ID: \"" + TranslationID + "\" doesn't exists in English language.");
+				log.Warn("Tanslation ID: \"" + TranslationID + "\" doesn't exists in English language.");
 			}
 
 			if (translated == null)
