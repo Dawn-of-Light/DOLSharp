b68296f6de989a520e25abeec8aae1ec95d9dfb1
bluraven <bluraven@users.sourceforge.net>
2010-03-08 08:06:38 +0000

    Changes from BluRaven: - Added: ArtifactEncounters folder with BasicEncounterMob and TetheredEncounterMob scripts. - Added: MasterLevelEncounters folder with EncounterMgr and ML1FireIsland scripts.
    ML1FireIsland script is included for reference as an example of how to build an artifact or master level encounter.
    
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2186 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServerScripts/ArtifactEncounters/BasicEncounterMob.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+using DOL.AI.Brain;
+using DOL.Events;
+using DOL.GS.Effects;
+using log4net;
+using System.Reflection;
+using DOL.GS.Atlantis;
+using DOL.Database;
+using DOL.Language;
+using DOL.GS.Spells;
+
+namespace DOL.GS.Atlantis
+{
+    /// <summary>
+    /// The base class that most or all ArtifactEncounter mob's should inherit from.
+    /// </summary>
+    public class BasicEncounterMob : GameNPC
+    {
+        public static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public override void SaveIntoDatabase()
+        {
+        }
+
+        public virtual void CastSpellnoLOSchecks(Spell spell, SpellLine line)
+        {
+            if ((m_runningSpellHandler != null && spell.CastTime > 0))
+            {
+                Notify(GameLivingEvent.CastFailed, this, new CastFailedEventArgs(null, CastFailedEventArgs.Reasons.AlreadyCasting));
+                return;
+            }
+            ISpellHandler spellhandler = ScriptMgr.CreateSpellHandler(this, spell, line);
+            if (spellhandler != null)
+            {
+                m_runningSpellHandler = spellhandler;
+                spellhandler.CastingCompleteEvent += new CastingCompleteCallback(OnAfterSpellCastSequence);
+                spellhandler.CastSpell();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
+
