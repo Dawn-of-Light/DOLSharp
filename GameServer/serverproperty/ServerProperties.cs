b719037eda6b173f82f7c1bd9f19cbc3e3d3d3d0
tolakram <tolakram@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2010-03-25 22:39:36 +0000

    - Added: How about that archery server property from the previous commit

--- GameServer/serverproperty/ServerProperties.cs
+++ GameServer/serverproperty/ServerProperties.cs
@@ -995,6 +995,13 @@ namespace DOL.GS.ServerProperties
         [ServerProperty("classes", "start_as_base_class", "Should we start all players as their base class? true if yes (e.g. Armsmen become Fighters on Creation)", false)]
         public static bool START_AS_BASE_CLASS;
 
+		/// <summary>
+		/// Should the server start characters as Base Class?
+		/// </summary>
+		[ServerProperty("classes", "allow_old_archery", "Should we allow archers to be able to use arrows from their quiver?", false)]
+		public static bool ALLOW_OLD_ARCHERY;
+
+
 		#endregion
 
 		#region SPELLS
