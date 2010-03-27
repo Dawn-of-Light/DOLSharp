87d938b8d6d0cf9902d8b0558ff1a42d36a3c571
tolakram <tolakram@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2010-03-27 19:42:36 +0000

    - Bugfix: Always check for nulls when using abilities in CriticalMeleeHitChanceCalculator

--- GameServer/propertycalc/CriticalMeleeHitChanceCalculator.cs
+++ GameServer/propertycalc/CriticalMeleeHitChanceCalculator.cs
@@ -19,53 +19,59 @@
     using System;
     using DOL.GS.Effects;
 
-    namespace DOL.GS.PropertyCalc
-    {
-       /// <summary>
-       /// The critical hit chance calculator. Returns 0 .. 100 chance.
-       ///
-       /// BuffBonusCategory1 unused
-       /// BuffBonusCategory2 unused
-       /// BuffBonusCategory3 unused
-       /// BuffBonusCategory4 for uncapped realm ability bonus
-       /// BuffBonusMultCategory1 unused
-       ///
-       /// Crit propability is capped to 50% except for berserk
-       /// </summary>
-       [PropertyCalculator(eProperty.CriticalMeleeHitChance)]
-       public class CriticalMeleeHitChanceCalculator : PropertyCalculator
-       {
-          public CriticalMeleeHitChanceCalculator() { }
+namespace DOL.GS.PropertyCalc
+{
+	/// <summary>
+	/// The critical hit chance calculator. Returns 0 .. 100 chance.
+	///
+	/// BuffBonusCategory1 unused
+	/// BuffBonusCategory2 unused
+	/// BuffBonusCategory3 unused
+	/// BuffBonusCategory4 for uncapped realm ability bonus
+	/// BuffBonusMultCategory1 unused
+	///
+	/// Crit propability is capped to 50% except for berserk
+	/// </summary>
+	[PropertyCalculator(eProperty.CriticalMeleeHitChance)]
+	public class CriticalMeleeHitChanceCalculator : PropertyCalculator
+	{
+		public CriticalMeleeHitChanceCalculator() { }
 
-          public override int CalcValue(GameLiving living, eProperty property)
-          {
-             // no berserk for ranged weapons
-             IGameEffect berserk = living.EffectList.GetOfType(typeof(BerserkEffect));
-             if (berserk != null)
-             {
-                return 100;
-             }
+		public override int CalcValue(GameLiving living, eProperty property)
+		{
+			// no berserk for ranged weapons
+			IGameEffect berserk = living.EffectList.GetOfType(typeof(BerserkEffect));
+			if (berserk != null)
+			{
+				return 100;
+			}
 
-             // base 10% chance of critical for all with melee weapons plus ra bonus
-             int chance = 10 + living.BuffBonusCategory4[(int)property] + living.AbilityBonus[(int)property];
+			// base 10% chance of critical for all with melee weapons plus ra bonus
+			int chance = 10 + living.BuffBonusCategory4[(int)property] + living.AbilityBonus[(int)property];
 
-             if (living is GameNPC && (living as GameNPC).Brain is AI.Brain.IControlledBrain)
-             {
-                GamePlayer player = ((living as GameNPC).Brain as AI.Brain.IControlledBrain).GetPlayerOwner();
-                if (player != null)
-                {
-                   RealmAbilities.WildMinionAbility ab = player.GetAbility(typeof(RealmAbilities.WildMinionAbility)) as RealmAbilities.WildMinionAbility;
-                   chance += ab.Amount;
-                   if (living is NecromancerPet)
-                   {
-                      RealmAbilities.MasteryOfPain mop = player.GetAbility(typeof(RealmAbilities.MasteryOfPain)) as RealmAbilities.MasteryOfPain;
-                      chance += mop.Amount;
-                   }
-                }
-             }
+			if (living is GameNPC && (living as GameNPC).Brain is AI.Brain.IControlledBrain)
+			{
+				GamePlayer player = ((living as GameNPC).Brain as AI.Brain.IControlledBrain).GetPlayerOwner();
+				if (player != null)
+				{
+					RealmAbilities.WildMinionAbility ab = player.GetAbility(typeof(RealmAbilities.WildMinionAbility)) as RealmAbilities.WildMinionAbility;
+					if (ab != null)
+					{
+						chance += ab.Amount;
+					}
+					if (living is NecromancerPet)
+					{
+						RealmAbilities.MasteryOfPain mop = player.GetAbility(typeof(RealmAbilities.MasteryOfPain)) as RealmAbilities.MasteryOfPain;
+						if (mop != null)
+						{
+							chance += mop.Amount;
+						}
+					}
+				}
+			}
 
-             //50% hardcap
-             return Math.Min(chance, 50);
-          }
-       }
-    }
+			//50% hardcap
+			return Math.Min(chance, 50);
+		}
+	}
+}
