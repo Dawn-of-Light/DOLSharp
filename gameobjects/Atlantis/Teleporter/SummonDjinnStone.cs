02d878008894f2e2d01e6090e2e1c8dd23f4ebcf
Graveen <graveen@users.sourceforge.net>
2010-04-25 17:05:54 +0000

    - Changed: Renamed IsloationLevel to IsolationLevel - Fixed: RewardQuest: Summary length must not be greater than 255 characters.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@2333 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- gameobjects/Atlantis/Teleporter/SummonDjinnStone.cs
+++ gameobjects/Atlantis/Teleporter/SummonDjinnStone.cs
@@ -22,44 +22,39 @@ using System.Reflection;
 
 namespace DOL.GS
 {
-    /// <summary>
-    /// Djinn stone (summons ancient bound djinn).
-    /// </summary>
-    /// <author>Aredhel</author>
-    public class SummonDjinnStone : DjinnStone
-    {
-        /// <summary>
-        /// Defines a logger for this class.
-        /// </summary>
-        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+	/// <summary>
+	/// Djinn stone (summons ancient bound djinn).
+	/// </summary>
+	/// <author>Aredhel</author>
+	public class SummonDjinnStone : DjinnStone
+	{
+		/// <summary>
+		/// Creates and summons the djinn if it isn't already up.
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns></returns>
+		public override bool Interact(GamePlayer player)
+		{
+			if (!base.Interact(player))
+				return false;
 
-        /// <summary>
-        /// Creates and summons the djinn if it isn't already up.
-        /// </summary>
-        /// <param name="player"></param>
-        /// <returns></returns>
-        public override bool Interact(GamePlayer player)
-        {
-            if (!base.Interact(player))
-                return false;
+			if (Djinn == null)
+			{
+				try
+				{
+					Djinn = new SummonedDjinn(this);
+				}
+				catch (Exception e)
+				{
+					log.Warn(String.Format("Unable to create ancient bound djinn: {0}", e.Message));
+					return false;
+				}
+			}
 
-            if (Djinn == null)
-            {
-                try
-                {
-                    Djinn = new SummonedDjinn(this);
-                }
-                catch (Exception e)
-                {
-                    log.Warn(String.Format("Unable to create ancient bound djinn: {0}", e.Message));
-                    return false;
-                }
-            }
+			if (!Djinn.IsSummoned)
+				Djinn.Summon();
 
-            if (!Djinn.IsSummoned)
-                Djinn.Summon();
-
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
