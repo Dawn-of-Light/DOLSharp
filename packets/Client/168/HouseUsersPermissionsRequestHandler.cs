041b7bb48371cce11c504cf1d2f4008eaeb0b043
tobz <tobz@users.sourceforge.net>
2010-05-07 23:37:10 +0000

    Code from me: - bugfix: housing permissions will no longer shower the account name directly when permissions are given account-wide to a player. (this commit actually compiles this time. :D)
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@2354 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- packets/Client/168/HouseUsersPermissionsRequestHandler.cs
+++ packets/Client/168/HouseUsersPermissionsRequestHandler.cs
@@ -57,7 +57,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 				pak.WriteByte((byte)0x00);			// ?
 				pak.WriteByte((byte)perm.PermissionType);		// Type (Guild, Class, Race ...)
 				pak.WriteByte((byte)perm.PermissionLevel);	// Level (Friend, Visitor ...)
-				pak.WritePascalString(perm.TargetName);
+				pak.WritePascalString(perm.DisplayName);
 			}
 			client.Out.SendTCP(pak);
 			return 1;
