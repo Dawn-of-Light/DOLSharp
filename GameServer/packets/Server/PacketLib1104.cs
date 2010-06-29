07ece8face8abab6fca50a8e4f9ac0d35d35cfd7
bluraven <bluraven@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2010-06-29 09:13:27 +0000

    Changes from BluRaven: -added: "Dummy" packet support for 1.104.  This will allow clients with 1.104 to connect but they will be unable to log in to existing characters or create new ones.  They will also have incorrect information on the character list.  This will need further development.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2425 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/packets/Server/PacketLib1104.cs
@@ -0,0 +1,42 @@
+﻿/*
+* DAWN OF LIGHT - The first free open source DAoC server emulator
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
+#define NOENCRYPTION
+using System;
+using System.Reflection;
+using log4net;
+
+
+namespace DOL.GS.PacketHandler
+{
+    [PacketLib(1104, GameClient.eClientVersion.Version1104)]
+    public class PacketLib1104 : PacketLib1103
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Constructs a new PacketLib for Client Version 1.104
+        /// </summary>
+        /// <param name="client">the gameclient this lib is associated with</param>
+        public PacketLib1104(GameClient client)
+            : base(client)
+        {
+            // BluRaven: dumb support, untested. Report if buggy.
+        }
+    }
+}
