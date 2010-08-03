f9fc25062a91372010bac2e8e8021e80b277b2a0
graveen <graveen@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2010-08-03 21:55:07 +0000

    - Added: use default classtype when loading npc on hookpoints if bad classtype - Added: missing files in project GSS/quests
    Ding rev 2500 ! \o/

--- DOLConfig/AssemblyInfo.cs
+++ DOLConfig/AssemblyInfo.cs
@@ -17,38 +17,14 @@
  *
  */
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("Dawn of Light - DOLConfig")]
-[assembly: AssemblyDescription("Configuration GUI for Dawn of Light")]
+[assembly: AssemblyDescription("Configuration software")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dawn of Light Development Team")]
 [assembly: AssemblyProduct("DAoC Server Side Emulation Package - Dawn of Light")]
 [assembly: AssemblyCopyright("Dawn of Light Development Team")]
 [assembly: AssemblyTrademark("Dawn of Light Development Team")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("")]		
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("1fec40c2-6312-48ba-9aea-7c22c3a001d7")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.3.2000")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.9.4.2500")]
