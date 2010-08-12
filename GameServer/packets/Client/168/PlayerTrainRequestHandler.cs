c0844ad021f8c430edd46f60451a4a73a714205b
graveen <graveen@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2010-08-12 17:02:25 +0000

    - Changed: /train command is now migrated to the core. It is used with 1.105 trainer window - Changed: packetlib for 1.105 CtoS ans StoC. - Added: CtoS opcode for 1.105 /!\ CLIENT 1.105 IS NOT SUPPORTED /!\

--- GameServer/packets/Client/168/PlayerTrainRequestHandler.cs
+++ GameServer/packets/Client/168/PlayerTrainRequestHandler.cs
@@ -25,6 +25,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 {
 	/// <summary>
 	/// handles Train clicks from Trainer Window
+	/// D4 is up to 1.104
 	/// </summary>
 	[PacketHandlerAttribute(PacketHandlerType.TCP, 0xD4 ^ 168, "Handles Player Train Requests")]
 	public class PlayerTrainRequestHandler : IPacketHandler
@@ -44,49 +45,49 @@ namespace DOL.GS.PacketHandler.Client.v168
 			int skillindex = packet.ReadByte();
 
 			// idline not null so this is a Champion level training window
-            if (idline > 0)
-            {
-                ChampSpec spec = ChampSpecMgr.GetAbilityFromIndex(idline, row, skillindex);
-                if (spec != null)
-                {
-                    if (client.Player.HaveChampionSpell(spec.SpellID))
-                    {
-                        client.Out.SendMessage("You already have that ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                        return 1;
-                    }
-                    if (!client.Player.IsCSAvailable(idline, row, skillindex))
-                    {
-                        client.Out.SendMessage("You do not meet the requirements for that ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                        return 1;
-                    }
-                    if ((client.Player.ChampionSpecialtyPoints - spec.Cost) < 0)
-                    {
-                        client.Out.SendMessage("You do not have enough champion specialty points for that ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                        return 1;
-                    }
-                    client.Player.ChampionSpecialtyPoints -= spec.Cost;
-                    SpellLine sl = SkillBase.GetSpellLine(GlobalSpellsLines.Champion_Spells + client.Player.Name);
-                    if (sl.Spec.StartsWith("?"))
-                    {
-                        SpellLine line = new SpellLine(GlobalSpellsLines.Champion_Spells + client.Player.Name, GlobalSpellsLines.Champion_Spells, GlobalSpellsLines.Champion_Spells, true);
-                        SkillBase.RegisterSpellLine(line);
-                    }
-                    SkillBase.AddSpellToList(GlobalSpellsLines.Champion_Spells + client.Player.Name, spec.SpellID);
-                    client.Player.ChampionSpells += spec.SpellID.ToString() + "|1;";
+			if (idline > 0)
+			{
+				ChampSpec spec = ChampSpecMgr.GetAbilityFromIndex(idline, row, skillindex);
+				if (spec != null)
+				{
+					if (client.Player.HaveChampionSpell(spec.SpellID))
+					{
+						client.Out.SendMessage("You already have that ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+						return 1;
+					}
+					if (!client.Player.IsCSAvailable(idline, row, skillindex))
+					{
+						client.Out.SendMessage("You do not meet the requirements for that ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+						return 1;
+					}
+					if ((client.Player.ChampionSpecialtyPoints - spec.Cost) < 0)
+					{
+						client.Out.SendMessage("You do not have enough champion specialty points for that ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+						return 1;
+					}
+					client.Player.ChampionSpecialtyPoints -= spec.Cost;
+					SpellLine sl = SkillBase.GetSpellLine(GlobalSpellsLines.Champion_Spells + client.Player.Name);
+					if (sl.Spec.StartsWith("?"))
+					{
+						SpellLine line = new SpellLine(GlobalSpellsLines.Champion_Spells + client.Player.Name, GlobalSpellsLines.Champion_Spells, GlobalSpellsLines.Champion_Spells, true);
+						SkillBase.RegisterSpellLine(line);
+					}
+					SkillBase.AddSpellToList(GlobalSpellsLines.Champion_Spells + client.Player.Name, spec.SpellID);
+					client.Player.ChampionSpells += spec.SpellID.ToString() + "|1;";
 
-                    client.Player.AddSpellLine(sl);
-                    client.Player.UpdateSpellLineLevels(false);
-                    client.Player.RefreshSpecDependantSkills(true);
-                    //client.Out.SendUpdatePoints();
-                    //client.Out.SendUpdatePlayer();
-                    client.Out.SendMessage("You gain an ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                    client.Out.SendChampionTrainerWindow(idline);
-                    client.Out.SendUpdatePlayerSkills();
-                    return 1;
-                }
-                else { client.Out.SendMessage("Didn't find spec!", eChatType.CT_System, eChatLoc.CL_SystemWindow); }
-                return 1;
-            }
+					client.Player.AddSpellLine(sl);
+					client.Player.UpdateSpellLineLevels(false);
+					client.Player.RefreshSpecDependantSkills(true);
+					//client.Out.SendUpdatePoints();
+					//client.Out.SendUpdatePlayer();
+					client.Out.SendMessage("You gain an ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+					client.Out.SendChampionTrainerWindow(idline);
+					client.Out.SendUpdatePlayerSkills();
+					return 1;
+				}
+				else { client.Out.SendMessage("Didn't find spec!", eChatType.CT_System, eChatLoc.CL_SystemWindow); }
+				return 1;
+			}
 
 			IList speclist = client.Player.GetSpecList();
 			if (skillindex < speclist.Count)
@@ -98,12 +99,12 @@ namespace DOL.GS.PacketHandler.Client.v168
 					return 1;
 				}
 
-                // Graveen - autotrain 1.87 - allow players to train their AT specs even if no pts left
-                client.Player.SkillSpecialtyPoints += client.Player.GetAutoTrainPoints(spec, 2);
+				// Graveen - autotrain 1.87 - allow players to train their AT specs even if no pts left
+				client.Player.SkillSpecialtyPoints += client.Player.GetAutoTrainPoints(spec, 2);
 
-                if (client.Player.SkillSpecialtyPoints >= spec.Level + 1)
+				if (client.Player.SkillSpecialtyPoints >= spec.Level + 1)
 				{
-                    client.Player.SkillSpecialtyPoints -= (ushort)(spec.Level + 1);
+					client.Player.SkillSpecialtyPoints -= (ushort)(spec.Level + 1);
 					spec.Level++;
 					client.Player.OnSkillTrained(spec);
 
@@ -160,6 +161,34 @@ namespace DOL.GS.PacketHandler.Client.v168
 				log.Error("Player <" + client.Player.Name + "> requested to train incorrect skill index");
 
 			return 1;
+		}
+	}
+	
+	/// <summary>
+	/// handles Train clicks from Trainer Window
+	/// 53 is since 1.105
+	/// </summary>
+	[PacketHandlerAttribute(PacketHandlerType.TCP, 0xFB ^ 168, "Handles Player Train")]
+	public class PlayerTrainHandler : IPacketHandler
+	{
+		public int HandlePacket(GameClient client, GSPacketIn packet)
+		{
+			client.Out.SendMessage("This command is not implemented", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+			return 0;
+		}
+	}
+	
+	/// <summary>
+	/// handles Train clicks from Trainer Window
+	/// 53 is since 1.105
+	/// </summary>
+	[PacketHandlerAttribute(PacketHandlerType.TCP, 0xD3 ^ 168, "Call Player Train Window")]
+	public class PlayerTrainWindowHandler : IPacketHandler
+	{
+		public int HandlePacket(GameClient client, GSPacketIn packet)
+		{
+			client.Out.SendMessage("This command is not implemented", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+			return 0;
 		}
 	}
 }
