6d96e6925bebe7af8bc24f56651a201d3cdf1b04
tolakram <tolakram@users.sourceforge.net>
2010-08-17 23:32:44 +0000

    - Changed: Added aliases to /train command for use by 1.105 clients - Added: Combine to GameInventoryItem and added support in GamePlayerInventory
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@2524 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- gameobjects/IGameInventoryItem.cs
+++ gameobjects/IGameInventoryItem.cs
@@ -29,16 +29,72 @@ namespace DOL.GS
 	/// </summary>		
 	public interface IGameInventoryItem
 	{
+		/// <summary>
+		/// Is this item valid, checked when loading inventory
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns></returns>
 		bool CheckValid(GamePlayer player);
+		/// <summary>
+		/// This item is received by the player
+		/// </summary>
+		/// <param name="player"></param>
 		void OnReceive(GamePlayer player);
+		/// <summary>
+		/// The player loses this item
+		/// </summary>
+		/// <param name="player"></param>
 		void OnLose(GamePlayer player);
+		/// <summary>
+		/// The player is dropping this item into the world
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns></returns>
 		WorldInventoryItem Drop(GamePlayer player);
+		/// <summary>
+		/// This item is being removed from the world
+		/// </summary>
 		void OnRemoveFromWorld();
+		/// <summary>
+		/// This item is being equipped
+		/// </summary>
+		/// <param name="player"></param>
 		void OnEquipped(GamePlayer player);
+		/// <summary>
+		/// This item is being un-equipped
+		/// </summary>
+		/// <param name="player"></param>
 		void OnUnEquipped(GamePlayer player);
+		/// <summary>
+		/// This item has struck a target
+		/// </summary>
+		/// <param name="owner"></param>
+		/// <param name="target"></param>
 		void OnStrikeTarget(GameLiving owner, GameObject target);
+		/// <summary>
+		/// This item has been struck by an enemy
+		/// </summary>
+		/// <param name="owner"></param>
+		/// <param name="enemy"></param>
 		void OnStruckByEnemy(GameLiving owner, GameLiving enemy);
+		/// <summary>
+		/// The player is attempting to use this item
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns>true if use is handled here</returns>
 		bool Use(GamePlayer player);
+		/// <summary>
+		/// The player is attempting to combine this item with the target item
+		/// </summary>
+		/// <param name="player"></param>
+		/// <param name="targetItem"></param>
+		/// <returns>true if combine is handled here</returns>
+		bool Combine(GamePlayer player, InventoryItem targetItem);
+		/// <summary>
+		/// Player is delving this item
+		/// </summary>
+		/// <param name="delve"></param>
+		/// <param name="player"></param>
 		void Delve(List<String> delve, GamePlayer player);
 	}
 }
