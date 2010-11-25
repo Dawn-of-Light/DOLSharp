053c8f721ac8e60df81d543f946c54268ed8f61f
tolakram <tolakram@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2010-11-25 11:37:52 +0000

    - Added: Missing Chatutil modifications needed for previous commit - added SendDebugMessage
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2680 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/gameutils/ChatUtil.cs
+++ GameServer/gameutils/ChatUtil.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -80,6 +99,17 @@ namespace DOL.GS
 			var translatedMsg = LanguageMgr.GetTranslation(target, translationID, args);
 
 			target.Out.SendMessage(translatedMsg, eChatType.CT_Help, eChatLoc.CL_SystemWindow);
+		}
+
+		public static void SendDebugMessage(GamePlayer target, string message)
+		{
+			SendDebugMessage(target.Client, message);
+		}
+
+		public static void SendDebugMessage(GameClient target, string message)
+		{
+			if (target.Account.PrivLevel > (int)ePrivLevel.Player)
+				target.Out.SendMessage(message, eChatType.CT_Staff, eChatLoc.CL_SystemWindow);
 		}
 	}
 }
