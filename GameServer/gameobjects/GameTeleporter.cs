47f2b35523fb67b2f85e8c0463cdfc2c544df899
tolakram <tolakram@users.sourceforge.net>
2010-11-28 20:22:55 +0000

    - Added: Size support for indoor objects, stored in DPS_AF - Bugfix: another fix for teleport out of house issues
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2690 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/gameobjects/GameTeleporter.cs
+++ GameServer/gameobjects/GameTeleporter.cs
@@ -243,9 +243,10 @@ namespace DOL.GS
 		{
 			if (player.InCombat == false && GameRelic.IsPlayerCarryingRelic(player) == false)
 			{
+				if (player.InHouse && player.CurrentHouse != null)
+					player.CurrentHouse.Exit(player, true);
+
 				player.MoveTo((ushort)destination.RegionID, destination.X, destination.Y, destination.Z, (ushort)destination.Heading);
-				player.CurrentHouse = null;
-				player.InHouse = false;
 			}
 		}
 	}
