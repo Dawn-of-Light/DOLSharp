8002d9c71cece4bc0ab9e64de022f5dba6176517
graveen <graveen@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2011-01-31 00:27:57 +0000

    - Added: Climb Spikes Ability for pure tanks. This ability is gained at level 35, and allows pure tanks to climb keep walls for 30s. Re-use timer: 60s.
    /!\ Insert the following SQL into your database:
    
    REPLACE INTO `spell` (`Spell_ID`, `SpellID`, `ClientEffect`, `Icon`, `Name`, `Description`, `Target`, `Range`, `Power`, `CastTime`, `Damage`, `DamageType`, `Type`, `Duration`, `Frequency`, `Pulse`, `PulsePower`, `Radius`, `RecastDelay`, `ResurrectHealth`, `ResurrectMana`, `Value`, `Concentration`, `LifeDrainReturn`, `AmnesiaChance`, `Message1`, `Message2`, `Message3`, `Message4`, `InstrumentRequirement`, `SpellGroup`, `EffectGroup`, `SubSpellID`, `MoveCast`, `Uninterruptible`, `IsPrimary`, `IsSecondary`, `AllowBolt`, `SharedTimerGroup`, `PackageID`, `IsFocus`) VALUES ('tank_ability_X', 14841, 14841, 4135, 'Climbing Spikes', 'Self only allows climbing', 'Self', 0, 0, 0, 0, 0, 'Climbing', 30, 0, 0, 0, 0, 60, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, 0);
    REPLACE INTO `ability` (`Ability_ID`, `KeyName`, `Name`, `Description`, `IconID`, `Implementation`) VALUES ('Climbing', 'Climbing Spikes', 'Climbing Spikes', 'Climbing points are available on keeps in certain locations. Having the Climbing ability means that player is capable of using the climb points to reach the inner courtyard of the keep.', 4135, NULL);
    
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2807 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/skillhandler/Climbing.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using DOL.GS.PacketHandler;
+using DOL.GS;
+using DOL.Database;
+
+namespace DOL.GS.SkillHandler
+{
+	/// <summary>
+	/// Handler for Fury shout
+	/// </summary>
+	[SkillHandlerAttribute(Abilities.ClimbSpikes)]
+	public class ClimbingAbilityHandler : SpellCastingAbilityHandler
+	{
+		private static int spellid = -1;
+		
+		public override long Preconditions
+		{
+			get
+			{
+				return DEAD | SITTING | MEZZED | STUNNED;
+			}
+		}
+		public override int SpellID
+		{
+			get
+			{
+				return spellid;
+			}
+		}
+
+		public ClimbingAbilityHandler()
+		{
+			// Graveen: crappy, but not hardcoded. if we except by the ability name ofc...
+			// problems are: 
+			// 		- matching vs ability name / spell name needed
+			//		- spell name is not indexed
+			// perhaps a basis to think about, but definitively not the design we want.
+			if (spellid == -1)
+			{
+				spellid=0;
+				DBSpell climbSpell = GameServer.Database.SelectObject<DBSpell>("Name = '" + Abilities.ClimbSpikes.ToString() + "'");
+				if (climbSpell != null)
+					spellid = climbSpell.SpellID;
+			}
+		}
+	}
+}
