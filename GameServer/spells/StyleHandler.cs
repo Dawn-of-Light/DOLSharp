ad3320193c8b7f535549abbac985cb7ca55c519f
tolakram <tolakram@users.sourceforge.net>
2011-03-20 05:32:03 +0000

    - Added: Unique StyleHandler for CL spell styles. - Changed: MasterLevel styles should now use the MLStyleHandler instead of StyleHandler.  Removed support for Stylehandler2 through StyleHandler6.  Yell if they are used and present a good reason for having them in the core. - Bugfix: CL styles will now delve style information
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@2838 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/spells/StyleHandler.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DOL.GS.Effects;
+using DOL.GS.PacketHandler;
+using DOL.GS.PacketHandler.Client.v168;
+using DOL.GS.Styles;
+
+namespace DOL.GS.Spells
+{
+	[SpellHandlerAttribute("StyleHandler")]
+	public class StyleHandler : SpellHandler
+	{
+		public StyleHandler(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) { }
+
+		public override IList<string> DelveInfo
+		{
+			get
+			{
+				var list = new List<string>();
+				list.Add(Spell.Description);
+
+				GamePlayer player = Caster as GamePlayer;
+
+				if (player != null)
+				{
+					list.Add(" ");
+
+					Style style = SkillBase.GetStyleByID((int)Spell.Value, 0);
+					if (style == null)
+					{
+						style = SkillBase.GetStyleByID((int)Spell.Value, player.CharacterClass.ID);
+					}
+
+					if (style != null)
+					{
+						DetailDisplayHandler.WriteStyleInfo(list, style, player.Client);
+					}
+					else
+					{
+						list.Add("Style not found.");
+					}
+				}
+
+				return list;
+			}
+		}
+
+	}
+
+
+}
+
