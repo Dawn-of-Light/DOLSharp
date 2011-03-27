92faaa74358bc8a777edbdb8be3d4f5a76c294ec
tolakram <tolakram@users.sourceforge.net>
2011-03-27 00:44:21 +0000

    - Bugfix: Fixed an issue where quickbars would no longer update due to a bad send of all the spell lines.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@2847 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- packets/Server/PacketLib168.cs
+++ packets/Server/PacketLib168.cs
@@ -2186,7 +2186,7 @@ namespace DOL.GS.PacketHandler
 						SendTCP(pak);
 					}
 
-					SendListCasterSpellList();
+					SendNonHybridSpellLines();
 				}
 			}
 
@@ -2197,15 +2197,12 @@ namespace DOL.GS.PacketHandler
 		}
 
 		/// <summary>
-		/// Send list caster spells and advanced spell lines
+		/// Send non hybrid and advanced spell lines
 		/// </summary>
-		public virtual void SendListCasterSpellList()
+		public virtual void SendNonHybridSpellLines()
 		{
 			GamePlayer player = m_gameClient.Player;
 			if (player == null)
-				return;
-
-			if (player.CharacterClass.ClassType != eClassType.ListCaster)
 				return;
 
 			IList spellLines = m_gameClient.Player.GetSpellLines();
@@ -2216,38 +2213,43 @@ namespace DOL.GS.PacketHandler
 				{
 					int lineIndex = player.GetSpellLines().IndexOf(line);
 
-					var spells = new List<Spell>(SkillBase.GetSpellList(line.KeyName)); // copy
-					int spellCount = 0;
-					for (int i = 0; i < spells.Count; i++)
+					// We only handle list caster spells or advanced lines
+					if (player.CharacterClass.ClassType == eClassType.ListCaster || player.IsAdvancedSpellLine(line))
 					{
-						if ((spells[i]).Level <= line.Level && spells[i].SpellType != "StyleHandler")
+						// make a copy
+						var spells = new List<Spell>(SkillBase.GetSpellList(line.KeyName)); // copy
+						int spellCount = 0;
+						for (int i = 0; i < spells.Count; i++)
 						{
-							spellCount++;
-						}
-					}
-
-					using (var pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.VariousUpdate)))
-					{
-						pak.WriteByte(0x02); //subcode
-						pak.WriteByte((byte)(spellCount + 1)); //number of entry
-						pak.WriteByte(0x02); //subtype
-						pak.WriteByte((byte)lineIndex); //number of line
-						pak.WriteByte(0); // level, not used when spell line
-						pak.WriteShort(0); // icon, not used when spell line
-						pak.WritePascalString(line.Name);
-
-						foreach (Spell spell in spells)
-						{
-							if (spell.Level <= line.Level && spell.SpellType != "StyleHandler")
+							if ((spells[i]).Level <= line.Level && spells[i].SpellType != "StyleHandler")
 							{
-								// log.DebugFormat("{0} - icon {1}, {2}, level {3}", lineIndex, spell.Icon, spell.Name, spell.Level);
-								pak.WriteByte((byte)spell.Level);
-								pak.WriteShort(spell.Icon);
-								pak.WritePascalString(spell.Name);
+								spellCount++;
 							}
 						}
 
-						SendTCP(pak);
+						using (var pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.VariousUpdate)))
+						{
+							pak.WriteByte(0x02); //subcode
+							pak.WriteByte((byte)(spellCount + 1)); //number of entry
+							pak.WriteByte(0x02); //subtype
+							pak.WriteByte((byte)lineIndex); //number of line
+							pak.WriteByte(0); // level, not used when spell line
+							pak.WriteShort(0); // icon, not used when spell line
+							pak.WritePascalString(line.Name);
+
+							foreach (Spell spell in spells)
+							{
+								if (spell.Level <= line.Level && spell.SpellType != "StyleHandler")
+								{
+									// log.DebugFormat("{0} - icon {1}, {2}, level {3}", lineIndex, spell.Icon, spell.Name, spell.Level);
+									pak.WriteByte((byte)spell.Level);
+									pak.WriteShort(spell.Icon);
+									pak.WritePascalString(spell.Name);
+								}
+							}
+
+							SendTCP(pak);
+						}
 					}
 				}
 			}
