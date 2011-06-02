420d4437ef5aee7e8d479d799f03eacb8d3eea87
shursan <shursan@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2011-06-02 10:07:18 +0000

    BugFix: Hunter can now have their Pets in combat without loose their stealth if stealthed. Only the Hunter can do this.

--- GameServer/gameobjects/GameNPC.cs
+++ GameServer/gameobjects/GameNPC.cs
@@ -3516,8 +3516,11 @@ namespace DOL.GS
 
 				GamePlayer owner = null;
 
-				if ((owner = ((IControlledBrain)Brain).GetPlayerOwner()) != null)
-					owner.Stealth(false);
+                if ((owner = ((IControlledBrain)Brain).GetPlayerOwner()) != null)
+                {
+                    if (owner.CharacterClass.ID != (int)eCharacterClass.Hunter) // the Hunter is the only class capable of this.
+                        owner.Stealth(false);
+                }
 			}
 
 			SetLastMeleeAttackTick();
