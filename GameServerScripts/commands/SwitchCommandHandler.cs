3e8f5c2e950d58093ed54a4c18ce6a71bac44b1d
stephenxdol <stephenxdol@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2012-03-06 00:12:14 +0000

    - Added: /Switch command. use /switch <1h,2h,ranged,offhand> <backpack slot #>. The slot #s have been modified so that the first backpack slot is 1, second 2, and so forth.

---
+++ GameServerScripts/commands/SwitchCommandHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Reflection;
+using System.Threading;
+using DOL.Events;
+using DOL.GS.PacketHandler;
+using log4net;
+using DOL.Database;
+
+namespace DOL.GS.Commands
+{
+    [Cmd("&switch", ePrivLevel.Player,
+        "Equip Weapons from bag. (/switch 1h 1, will replace ur mainhand weapon with the first slot in ur backpack)",
+        "/switch 1h <slot>",
+        "/switch offhand <slot>",
+        "/switch 2h <slot>",
+        "/switch range <slot>")]
+    public class SwitchCommandHandler : AbstractCommandHandler, ICommandHandler
+    {
+        public void OnCommand(GameClient client, string[] args)
+        {
+            if (args.Length < 2)
+            {
+                DisplaySyntax(client);
+                return;
+            }
+
+            eInventorySlot ToSlot = eInventorySlot.FirstBackpack;
+
+            switch (args[1])
+            {
+                case "1h":
+                    ToSlot = eInventorySlot.RightHandWeapon;
+                    break;
+                case "2h":
+                    ToSlot = eInventorySlot.TwoHandWeapon;
+                    break;
+                case "offhand":
+                    ToSlot = eInventorySlot.LeftHandWeapon;
+                    break;
+                case "range":
+                    ToSlot = eInventorySlot.DistanceWeapon;
+                    break;
+            }
+
+            //The first backpack.
+            int FromSlot = 40;
+
+            if (int.TryParse(args[2], out FromSlot))
+            {
+                FromSlot = int.Parse(args[2]);
+                SwitchItem(client.Player, ToSlot, (eInventorySlot)FromSlot + 39);
+            }
+            else
+            {
+                DisplayMessage(client, "There seems to have been a problem. Please try again.");
+                DisplaySyntax(client);
+                return;
+            }
+
+        }
+        public void SwitchItem(GamePlayer player, eInventorySlot ToSlot, eInventorySlot FromSlot)
+        {
+            if (player.Inventory.GetItem(FromSlot) != null)
+            {
+                InventoryItem item = player.Inventory.GetItem(FromSlot);
+
+                if (!GlobalConstants.IsWeapon(item.Object_Type))
+                {
+                    DisplayMessage(player.Client, "That is not a weapon!");
+                    DisplaySyntax(player.Client);
+                    return;
+                }
+
+                if (!player.Inventory.MoveItem(FromSlot, ToSlot, 1))
+                {
+                    DisplayMessage(player.Client, "There seems to have been a problem. Please try again.");
+                    DisplaySyntax(player.Client);
+                    return;
+                }
+            }
+        }
+    }
+}
