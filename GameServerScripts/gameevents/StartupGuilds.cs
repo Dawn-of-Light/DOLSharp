2c96eedd207dfc8aba884899ba09762d37c2a3a2
darwin-eden <darwin-eden@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2012-04-09 07:32:31 +0000

    - Added: several Server Properties to choose what should be created in database or not, loaded at server start or not.

--- GameServerScripts/gameevents/StartupGuilds.cs
+++ GameServerScripts/gameevents/StartupGuilds.cs
@@ -45,7 +45,10 @@ namespace DOL.GS.GameEvents
 		[ScriptLoadedEvent]
 		public static void OnScriptCompiled(DOLEvent e, object sender, EventArgs args)
 		{
-			foreach (eRealm currentRealm in Enum.GetValues(typeof(eRealm)))
+            if (!ServerProperties.Properties.STARTING_GUILD)
+                return;
+            
+            foreach (eRealm currentRealm in Enum.GetValues(typeof(eRealm)))
 			{
 				if (currentRealm == eRealm.None ||currentRealm == eRealm.Door)
 					continue;
