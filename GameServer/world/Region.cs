33e76c8538105718f736a91cdc7ed22535fe2a95
darwin-eden <darwin-eden@users.sourceforge.net>
2012-04-13 04:31:54 +0000

    - Add: following istandalone revisions 3045 and 3046
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/eden@3048 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/world/Region.cs
+++ GameServer/world/Region.cs
@@ -851,11 +851,10 @@ namespace DOL.GS
 
 			if (myMobCount + myItemCount + myMerchantCount + myBindCount > 0)
 			{
-				if (ServerProperties.Properties.VERBOSE_LEVEL < 1 && log.IsInfoEnabled)
+				if (log.IsInfoEnabled)
 					log.Info(String.Format("Region: {0} ({1}) loaded {2} mobs, {3} merchants, {4} items {5} bindpoints, from DB ({6})", Description, ID, myMobCount, myMerchantCount, myItemCount, myBindCount, TimeManager.Name));
 
-				if (ServerProperties.Properties.VERBOSE_LEVEL < 1)
-					log.Debug("Used Memory: " + GC.GetTotalMemory(false) / 1024 + "KB");
+				log.Debug("Used Memory: " + GC.GetTotalMemory(false) / 1024 / 1024 + "MB");
 
 				if (allErrors != string.Empty)
 					log.Error("Error loading the following NPC ClassType(s), GameNPC used instead:" + allErrors.TrimEnd(','));
