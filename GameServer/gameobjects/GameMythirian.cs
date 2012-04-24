39de4822dab963e7e3e030a5cee42255c45efbeb
matt450 <matt450@users.sourceforge.net>
2012-04-24 13:01:06 +0000

    Changed: Falling damage calculations moved to GamePlayer.cs Added:   MythicalSafeFall bonus type, the value of this bonus represents the max damage percentage a player can take in a fall.  Safe Fall Ability benefit is applied prior to this bonus being applied. Added:   DOL.GS.GameMythirian class GameInventoryItem
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@3064 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/gameobjects/GameMythirian.cs
@@ -0,0 +1,68 @@
+﻿/* DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using DOL.Events;
+using DOL.Language;
+using DOL.GS.PacketHandler;
+using DOL.Database;
+using DOL.GS.Spells;
+using log4net;
+
+namespace DOL.GS
+{
+        public class GameMythirian : GameInventoryItem
+        {
+                private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+                private GameMythirian() { }
+
+                public GameMythirian(ItemTemplate template)
+                        : base(template)
+                {
+                }
+
+                public GameMythirian(ItemUnique template)
+                        : base(template)
+                {
+                }
+
+                public GameMythirian(InventoryItem item)
+                        : base(item)
+                {
+                }
+
+                public override bool CanEquip(GamePlayer player)
+                {
+                        if (base.CanEquip(player))
+                        {
+                                if (Type_Damage <= player.ChampionLevel)
+                                {
+                                        return true;
+                                }
+
+                                // maybe display some message to player here?
+                        }
+
+                        return false;
+                }
+
+      }
+}
+ 
