af60dddc78997fc30c0a822c4cb79cc7077364c5
terryammon <terryammon@users.sourceforge.net>
2012-05-03 15:22:25 +0000

    - Changed: Set BeltofMoon summoned staff to 2handed. - Bugfix: ABSDamageShield spellhandler will now hit the attacker not the caster. Added: CommonAstralSpells, new spellhandlers for ml10 items. - Updated: SummonIllusionBlade, Lowered pets level to match patch 1.109b and set npctemplate to Spell.LifeDrainReturn.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@3070 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/spells/SummonIllusionBlade.cs
+++ GameServer/spells/SummonIllusionBlade.cs
@@ -24,91 +24,82 @@ using DOL.GS.PacketHandler;
 
 namespace DOL.GS.Spells
 {
-	[SpellHandler("IllusionBladeSummon")]
-	public class IllusionBladeSummon : SummonSpellHandler
-	{
-		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    [SpellHandler("IllusionBladeSummon")]
+    public class IllusionBladeSummon : SummonSpellHandler
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-		public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
-		{
-			//Template of the Illusionblade NPC
-			INpcTemplate template = NpcTemplateMgr.GetTemplate(71980);
-			
-			
-			if (template == null)
-			{
-				if (log.IsWarnEnabled)
-					log.WarnFormat("NPC template {0} not found! Spell: {1}", Spell.LifeDrainReturn, Spell.ToString());
-				MessageToCaster("NPC template 71980 not found!", eChatType.CT_System);
-				return;
-			}
+        public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
+        {
+            //Template of the Illusionblade NPC
+            INpcTemplate template = NpcTemplateMgr.GetTemplate((ushort)Spell.LifeDrainReturn);
 
-			GameSpellEffect effect = CreateSpellEffect(target, effectiveness);
+            if (template == null)
+            {
+                if (log.IsWarnEnabled)
+                    log.WarnFormat("NPC template {0} not found! Spell: {1}", Spell.LifeDrainReturn, Spell.ToString());
+                MessageToCaster("NPC template " + (ushort)Spell.LifeDrainReturn + " not found!", eChatType.CT_System);
+                return;
+            }
 
-			IControlledBrain brain = GetPetBrain(Caster);
-			m_pet = GetGamePet(template);
-			//brain.WalkState = eWalkState.Stay;
-			m_pet.SetOwnBrain(brain as AI.ABrain);
+            GameSpellEffect effect = CreateSpellEffect(target, effectiveness);
+            IControlledBrain brain = GetPetBrain(Caster);
+            m_pet = GetGamePet(template);
+            //brain.WalkState = eWalkState.Stay;
+            m_pet.SetOwnBrain(brain as AI.ABrain);
+            int x, y, z;
+            ushort heading;
+            Region region;
 
-			int x, y, z;
-			ushort heading;
-			Region region;
+            GetPetLocation(out x, out y, out z, out heading, out region);
 
-			GetPetLocation(out x, out y, out z, out heading, out region);
+            m_pet.X = x;
+            m_pet.Y = y;
+            m_pet.Z = z;
+            m_pet.Heading = heading;
+            m_pet.CurrentRegion = region;
+            m_pet.CurrentSpeed = 0;
+            m_pet.Realm = Caster.Realm;
+            m_pet.Level = 44; // lowered in patch 1109b
+            m_pet.AddToWorld();
+            //Check for buffs
+            if (brain is ControlledNpcBrain)
+                (brain as ControlledNpcBrain).CheckSpells(StandardMobBrain.eCheckSpellType.Defensive);
 
-			m_pet.X = x;
-			m_pet.Y = y;
-			m_pet.Z = z;
-			m_pet.Heading = heading;
-			m_pet.CurrentRegion = region;
+            AddHandlers();
+            SetBrainToOwner(brain);
+            m_pet.AutoSetStats();
+            effect.Start(m_pet);
+            //Set pet infos & Brain
 
-			m_pet.CurrentSpeed = 0;
-			m_pet.Realm = Caster.Realm;
-			m_pet.Level = Caster.Level;
-			m_pet.AddToWorld();
-			//Check for buffs
-			if (brain is ControlledNpcBrain)
-				(brain as ControlledNpcBrain).CheckSpells(StandardMobBrain.eCheckSpellType.Defensive);
+        }
 
-			AddHandlers();
+        protected override GamePet GetGamePet(INpcTemplate template) { return new IllusionBladePet(template); }
+        protected override IControlledBrain GetPetBrain(GameLiving owner) { return new ProcPetBrain(owner); }
+        protected override void SetBrainToOwner(IControlledBrain brain) { }
+        protected override void AddHandlers() { GameEventMgr.AddHandler(m_pet, GameLivingEvent.AttackFinished, EventHandler); }
 
-			SetBrainToOwner(brain);
-			m_pet.AutoSetStats();
-
-			effect.Start(m_pet);
-
-			
-			
-			//Set pet infos & Brain
-			
-		}
-
-		protected override GamePet GetGamePet(INpcTemplate template) { return new IllusionBladePet(template); }
-		protected override IControlledBrain GetPetBrain(GameLiving owner) { return new ProcPetBrain(owner); }
-		protected override void SetBrainToOwner(IControlledBrain brain) { }
-		protected override void AddHandlers() { GameEventMgr.AddHandler(m_pet, GameLivingEvent.AttackFinished, EventHandler); }
-
-		protected void EventHandler(DOLEvent e, object sender, EventArgs arguments)
-		{
-			AttackFinishedEventArgs args = arguments as AttackFinishedEventArgs;
-			if(args == null || args.AttackData == null)
-				return;
-		}
-		public IllusionBladeSummon(GameLiving caster, Spell spell, SpellLine line)
-			: base(caster, spell, line) { }
-	}
+        protected void EventHandler(DOLEvent e, object sender, EventArgs arguments)
+        {
+            AttackFinishedEventArgs args = arguments as AttackFinishedEventArgs;
+            if (args == null || args.AttackData == null)
+                return;
+        }
+        public IllusionBladeSummon(GameLiving caster, Spell spell, SpellLine line)
+            : base(caster, spell, line) { }
+    }
 }
 
 namespace DOL.GS
 {
-	public class IllusionBladePet : GamePet
-	{
-		public override int MaxHealth
-		{
-			get { return Level * 10; }
-		}
-		public override void OnAttackedByEnemy(AttackData ad) { }
-		public IllusionBladePet (INpcTemplate npcTemplate) : base(npcTemplate) { }
-	}
+    public class IllusionBladePet : GamePet
+    {
+        public override int MaxHealth
+        {
+            get { return Level * 10; }
+        }
+        public override void OnAttackedByEnemy(AttackData ad) { }
+        public IllusionBladePet(INpcTemplate npcTemplate) : base(npcTemplate) { }
+    }
 }
 
