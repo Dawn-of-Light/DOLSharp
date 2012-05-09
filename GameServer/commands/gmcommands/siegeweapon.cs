bd52478926aacd73cbf326ac5ab2655594a3029b
sinswolf <sinswolf@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2012-05-09 15:40:37 +0000

    - Added: the other siegeweapons like ballista,catapult,cauldron,trebuchet - Changed: the message how to use siegeweapon

--- GameServer/commands/gmcommands/siegeweapon.cs
+++ GameServer/commands/gmcommands/siegeweapon.cs
@@ -22,7 +22,7 @@ namespace DOL.GS.Commands
 	"&siegeweapon",
 	ePrivLevel.GM,
 	"creates siege weapons",
-	"/siegeweapon create miniram/lightram/mediumram/heavyram")]
+	"/siegeweapon create miniram/lightram/mediumram/heavyram/catapult/ballista/cauldron/trebuchet")]
 	public class SiegeWeaponCommandHandler : AbstractCommandHandler, ICommandHandler
 	{
 		public void OnCommand(GameClient client, string[] args)
@@ -93,6 +93,62 @@ namespace DOL.GS.Commands
 									ram.Name = "heavy ram";
 									ram.Realm = client.Player.Realm;
 									ram.AddToWorld();
+									break;
+								}
+								case "catapult":
+								{
+									GameSiegeCatapult cat = new GameSiegeCatapult();
+									cat.X = client.Player.X;
+									cat.Y = client.Player.Y;
+									cat.Z = client.Player.Z;
+									cat.CurrentRegion = client.Player.CurrentRegion;
+									cat.Model = 0xA26;
+									cat.Level = 3;
+									cat.Name = "catapult";
+									cat.Realm = client.Player.Realm;
+									cat.AddToWorld();
+									break;
+								}
+								case "ballista":
+								{
+									GameSiegeBallista bal = new GameSiegeBallista();
+									bal.X = client.Player.X;
+									bal.Y = client.Player.Y;
+									bal.Z = client.Player.Z;
+									bal.CurrentRegion = client.Player.CurrentRegion;
+									bal.Model = 0x0A55;
+									bal.Level = 3;
+									bal.Name = "field ballista";
+									bal.Realm = client.Player.Realm;
+									bal.AddToWorld();
+									break;
+								}
+								case "cauldron":
+								{
+									GameSiegeRam ram = new GameSiegeRam();
+									ram.X = client.Player.X;
+									ram.Y = client.Player.Y;
+									ram.Z = client.Player.Z;
+									ram.CurrentRegion = client.Player.CurrentRegion;
+									ram.Model =  0xA2F;
+									ram.Level = 3;
+									ram.Name = "cauldron of boiling oil";
+									ram.Realm = client.Player.Realm;
+									ram.AddToWorld();
+									break;
+								}
+								case "trebuchet":
+								{
+									GameSiegeTrebuchet tre = new GameSiegeTrebuchet();
+									tre.X = client.Player.X;
+									tre.Y = client.Player.Y;
+									tre.Z = client.Player.Z;
+									tre.CurrentRegion = client.Player.CurrentRegion;
+									tre.Model = 0xA2E;
+									tre.Level = 3;
+									tre.Name = "trebuchet";
+									tre.Realm = client.Player.Realm;
+									tre.AddToWorld();
 									break;
 								}
 						}
