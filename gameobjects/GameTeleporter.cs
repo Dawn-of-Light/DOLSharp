aaa5c4ace89ec5a06b29592154b3139b4c253348
tolakram <tolakram@users.sourceforge.net>
2013-02-08 16:11:55 +0000

    - Added: OnPlayerTeleport to ServerRules and new server property to set immunity time when teleporting - added: New serverproperty for game entered timer - Bugfix: Moved ServerRule immunity event handlers into AbstractServerRules to enable correct handling for each rule type
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@3187 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- gameobjects/GameTeleporter.cs
+++ gameobjects/GameTeleporter.cs
@@ -331,7 +331,9 @@ namespace DOL.GS
 			if (player.InCombat == false && GameRelic.IsPlayerCarryingRelic(player) == false)
 			{
 				player.LeaveHouse();
+				GameLocation currentLocation = new GameLocation("TeleportStart", player.CurrentRegionID, player.X, player.Y, player.Z);
 				player.MoveTo((ushort)destination.RegionID, destination.X, destination.Y, destination.Z, (ushort)destination.Heading);
+				GameServer.ServerRules.OnPlayerTeleport(player, currentLocation, destination);
 			}
 		}
 	}
