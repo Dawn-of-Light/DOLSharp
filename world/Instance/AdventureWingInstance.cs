f4d8a71c9fb3df1718696b4d5da1e834c94e0dab
dol-leodagan <leodagan@durron.net>
2013-08-30 11:34:07 +0000

    Added : AdventureWingJumpPoint to handle jumping into an instance through dungeon colored doors (Catacombs) Added : RegionInstance to handle temporary instanced copies of existing Region (used to create private dungeons, should work even on open areas) Added : AdventureWingInstance to handle ownership of RegionInstance, as this private instanced area are meant for a player or a group only. Changed : GameServer.csproj to compile new files.
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk/GameServer@3233 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ world/Instance/AdventureWingInstance.cs
@@ -0,0 +1,168 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using DOL.Database;
+using DOL.AI.Brain;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DOL.GS
+{
+	/// <summary>
+	/// Description of AdventureWingInstance.
+	/// Used to create personal or group dungeon instance so player can play in there without being annoyed
+	/// will try to maintain owner ship of the instance so player or group can get back to it
+	/// The Instance will destroy 30 min after nobody is inside
+	/// Other manager are needed to prevent too much instance spawning (using JumpPoint for example)
+	/// </summary>
+	public class AdventureWingInstance : RegionInstance
+	{
+		
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		
+		public const ushort minute_until_close = 30;
+		
+		private Group m_group;
+		
+		private GamePlayer m_player;
+		
+		public Group Group
+        {
+            get { return m_group; }
+            set { m_group = value; }
+        }
+		        
+		public GamePlayer Player
+        {
+            get { return m_player; }
+            set { m_player = value; }
+        }
+		
+		public AdventureWingInstance(ushort ID, GameTimer.TimeManager time, RegionData dat)
+			: base(ID, time, dat)
+		{
+		}
+		
+		public void UpdateInstanceOwner() 
+		{
+			if(this.NumPlayers < 1) 
+			{
+				return;	
+			}
+			
+			//there is still player inside search for owner or any change.
+        	
+        	//search vars
+        	GamePlayer arbitraryplayer = null;
+            Group arbitrarygroup = null;
+            bool stillOwner = false;
+            
+            // group instance
+            if(m_group != null) 
+            {
+            	// check if group still inside
+				foreach(GamePlayer ininstance in this.PlayersInside) 
+				{
+					if(ininstance.Group != null && m_group == ininstance.Group) 
+					{
+						m_player = ininstance.Group.Leader;
+						stillOwner = true;
+						break;
+					}
+					else if(ininstance.Group != null) 
+					{
+						arbitrarygroup = ininstance.Group;
+						arbitraryplayer = ininstance.Group.Leader;
+					}
+				}	            	
+            }
+            
+            // check if player owner is still inside
+            if(!stillOwner && m_player != null)
+            {
+            	foreach(GamePlayer ininstance in this.PlayersInside) 
+				{
+					if(ininstance == m_player)
+					{
+						if(m_player.Group != null)
+						{
+							m_group = m_player.Group;
+							m_player = m_player.Group.Leader;
+						}
+						stillOwner = true;
+						break;
+					}
+            		else
+            		{
+            			arbitraryplayer = ininstance;
+            		}
+				}           	
+            }
+            
+            // if no owner found
+            if(!stillOwner) 
+            {
+            	//give ownership arbitrarly
+            	if(arbitrarygroup != null)
+            	{
+            		m_group = arbitrarygroup;
+            		m_player = arbitrarygroup.Leader;
+            	}
+            	else if(arbitraryplayer != null)
+            	{
+            		m_group = null;
+					m_player = arbitraryplayer;	            		
+            	}
+            	
+            }
+		}
+		
+		public override void OnPlayerLeaveInstance(GamePlayer player)
+        {
+			// last player going out
+			if(this.NumPlayers == 1) 
+			{
+				if(player.Group != null) 
+				{
+					this.m_group = player.Group;
+					this.m_player = player.Group.Leader;
+				}
+				else {
+					this.m_group = null;
+					this.m_player = player;
+				}
+			}
+			
+            //Decrease the amount of players
+            base.OnPlayerLeaveInstance(player);
+            	
+            if(this.NumPlayers > 0) 
+            {
+            	UpdateInstanceOwner();
+            }
+            else
+            {
+            	//If no more players remain, remove and clean up after a timer !
+                log.Warn("Instance now empty, will destroy instance " + Description + ", ID: " + ID + ", type=" + GetType().ToString() + ". In " + minute_until_close + " min.");
+                this.BeginAutoClosureCountdown(minute_until_close);
+            }
+        }
+	}
+}
---
+++ world/Instance/RegionInstance.cs
@@ -0,0 +1,266 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using DOL.Database;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+
+namespace DOL.GS
+{
+	/// <summary>
+	/// Description of RegionInstance.
+	/// Clone a RegionData to a New BaseInstance
+	/// Can duplicate any "Region" of the Game into a dedicated Instance
+	/// </summary>
+	public class RegionInstance : BaseInstance
+	{
+		
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		
+		private List<GamePlayer> m_players_in;
+		
+		protected GameLocation m_sourceentrance;
+		
+		protected List<GamePlayer> PlayersInside
+		{
+			get { return m_players_in; }
+		}
+
+		public GameLocation SourceEntrance
+		{
+			get { return m_sourceentrance; }
+			set { m_sourceentrance = value; }
+		}
+		
+		public override void OnPlayerEnterInstance(GamePlayer player)
+		{
+			//Add Player
+			this.m_players_in.Add(player);
+			//Stop the timer to prevent the region's removal.
+			base.OnPlayerEnterInstance(player);
+		}
+		
+		public override void OnPlayerLeaveInstance(GamePlayer player)
+        {
+            //Decrease the amount of players
+            base.OnPlayerLeaveInstance(player);
+            this.m_players_in.Remove(player);
+        }
+		
+		public RegionInstance(ushort ID, GameTimer.TimeManager time, RegionData dat)
+			: base(ID, time, dat)
+		{	
+			this.m_players_in = new List<GamePlayer>();
+			this.DestroyWhenEmpty = false;
+		}
+		
+		public override void LoadFromDatabase(Mob[] mobObjs, ref long mobCount, ref long merchantCount, ref long itemCount, ref long bindCount)
+        {
+            if (!LoadObjects)
+                return;
+
+            Assembly gasm = Assembly.GetAssembly(typeof(GameServer));
+            var staticObjs = GameServer.Database.SelectObjects<WorldObject>("Region = " + Skin);
+            
+            
+            int count = mobObjs.Length + staticObjs.Count;
+            if (count > 0) PreAllocateRegionSpace(count + 100);
+            
+            int myItemCount = staticObjs.Count;
+            int myMobCount = 0;
+            int myMerchantCount = 0;
+            
+            string allErrors = string.Empty;
+
+            if (mobObjs.Length > 0)
+            {
+                foreach (Mob mob in mobObjs)
+                {
+                    GameNPC myMob = null;
+                    string error = string.Empty;
+  
+                    // Default Classtype
+                    string classtype = ServerProperties.Properties.GAMENPC_DEFAULT_CLASSTYPE;
+                    
+                    // load template if any
+                    INpcTemplate template = null;
+                    if(mob.NPCTemplateID != -1)
+                    {
+                    	template = NpcTemplateMgr.GetTemplate(mob.NPCTemplateID);
+                    }
+                    
+
+                    if (mob.Guild.Length > 0 && mob.Realm >= 0 && mob.Realm <= (int)eRealm._Last)
+                    {
+                        Type type = ScriptMgr.FindNPCGuildScriptClass(mob.Guild, (eRealm)mob.Realm);
+                        if (type != null)
+                        {
+                            try
+                            {
+                                
+                                myMob = (GameNPC)type.Assembly.CreateInstance(type.FullName);
+                               	
+                            }
+                            catch (Exception e)
+                            {
+                                if (log.IsErrorEnabled)
+                                    log.Error("LoadFromDatabase", e);
+                            }
+                        }
+                    }
+
+  
+                    if (myMob == null)
+                    {
+                    	if(template != null && template.ClassType != null && template.ClassType.Length > 0 && template.ClassType != Mob.DEFAULT_NPC_CLASSTYPE && template.ReplaceMobValues)
+                    	{
+                			classtype = template.ClassType;
+                    	}
+                        else if (mob.ClassType != null && mob.ClassType.Length > 0 && mob.ClassType != Mob.DEFAULT_NPC_CLASSTYPE)
+                        {
+                            classtype = mob.ClassType;
+                        }
+
+                        try
+                        {
+                            myMob = (GameNPC)gasm.CreateInstance(classtype, false);
+                        }
+                        catch
+                        {
+                            error = classtype;
+                        }
+
+                        if (myMob == null)
+                        {
+                            foreach (Assembly asm in ScriptMgr.Scripts)
+                            {
+                                try
+                                {
+                                    myMob = (GameNPC)asm.CreateInstance(classtype, false);
+                                    error = string.Empty;
+                                }
+                                catch
+                                {
+                                    error = classtype;
+                                }
+
+                                if (myMob != null)
+                                    break;
+                            }
+
+                            if (myMob == null)
+                            {
+                                myMob = new GameNPC();
+                                error = classtype;
+                            }
+                        }
+                    }
+
+                    if (!allErrors.Contains(error))
+                        allErrors += " " + error + ",";
+
+                    if (myMob != null)
+                    {
+                        try
+                        {
+                        	Mob clone = (Mob)mob.Clone();
+                        	clone.AllowAdd = false;
+                        	clone.AllowDelete = false;
+                        	clone.Region = this.ID;
+                        	
+                        	myMob.LoadFromDatabase(clone);
+
+                            if (myMob is GameMerchant)
+                            {
+                                myMerchantCount++;
+                            }
+                            else
+                            {
+                                myMobCount++;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            if (log.IsErrorEnabled)
+                                log.Error("Failed: " + myMob.GetType().FullName + ":LoadFromDatabase(" + mob.GetType().FullName + ");", e);
+                            throw;
+                        }
+
+                        myMob.AddToWorld();
+                    }
+                }
+            }
+
+            if (staticObjs.Count > 0)
+            {
+                foreach (WorldObject item in staticObjs)
+                {
+                	WorldObject itemclone = (WorldObject)item.Clone();
+                	itemclone.AllowAdd = false;
+                	itemclone.AllowDelete = false;
+                	itemclone.Region = this.ID;
+                	
+                    GameStaticItem myItem;
+                    if (!string.IsNullOrEmpty(itemclone.ClassType))
+                    {
+                        myItem = gasm.CreateInstance(itemclone.ClassType, false) as GameStaticItem;
+                        if (myItem == null)
+                        {
+                            foreach (Assembly asm in ScriptMgr.Scripts)
+                            {
+                                try
+                                {
+                                    myItem = (GameStaticItem)asm.CreateInstance(itemclone.ClassType, false);
+                                }
+                                catch { }
+                                if (myItem != null)
+                                    break;
+                            }
+                            if (myItem == null)
+                                myItem = new GameStaticItem();
+                        }
+                    }
+                    else
+                        myItem = new GameStaticItem();
+
+                    myItem.AddToWorld();
+                }
+            }
+
+            if (myMobCount + myItemCount + myMerchantCount > 0)
+            {
+                if (log.IsInfoEnabled)
+                    log.Info(String.Format("AdventureWingInstance: {0} ({1}) loaded {2} mobs, {3} merchants, {4} items, from DB ({5})", Description, ID, myMobCount, myMerchantCount, myItemCount, TimeManager.Name));
+
+                log.Debug("Used Memory: " + GC.GetTotalMemory(false) / 1024 / 1024 + "MB");
+
+                if (allErrors != string.Empty)
+                    log.Error("Error loading the following NPC ClassType(s), GameNPC used instead:" + allErrors.TrimEnd(','));
+
+                Thread.Sleep(0);  // give up remaining thread time to other resources
+            }
+            mobCount += myMobCount;
+            merchantCount += myMerchantCount;
+            itemCount += myItemCount;
+        }
+
+	}
+}
