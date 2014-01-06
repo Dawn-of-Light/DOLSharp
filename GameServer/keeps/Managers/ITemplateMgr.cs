f9b376613d4a20975a8d8beb0f1ffc3108031e68
darwin-eden <darwin-eden@users.sourceforge.net>
2014-01-06 11:49:49 +0000

    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/branch/eden@3317 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/keeps/Managers/ITemplateMgr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DOL.GS.Keeps
+{
+	public interface ITemplateMgr
+	{
+		void RefreshTemplate(GameKeepGuard guard);
+		void SetGuardLevel(GameKeepGuard guard);
+		void SetGuardBrain(GameKeepGuard guard);
+		void SetGuardSpeed(GameKeepGuard guard);
+	}
+}
