2acfa2b7491752d0270d09f00b6eb86e8554327b
kypd <kypd@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2014-08-15 08:57:54 +0000

    Added : PacketLib for version 1.114 and 1.115. Updated : GameClient.cs to handle over 1.113 client versions. Needed : Darwin/Eden Client update for 1.112 "WriteItemData"
    No changes in 1.114 client packet code, one change for 1.115, Inventory Update (S=>C 0x0002) added a Short int in front of Item Data (Before Sending Byte Level) not documented for now, seems to have some effect on displaying Bonus Level in Tool Tips. 
    
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@3329 c9877f0d-d2ea-46ae-93cb-121acdf3e971

---
+++ GameServer/packets/Server/PacketLib1115.cs
@@ -0,0 +1,67 @@
+﻿/*
+* DAWN OF LIGHT - The first free open source DAoC server emulator
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
+#define NOENCRYPTION
+using System;
+using System.Reflection;
+using DOL.Database;
+using System.Collections;
+using System.Collections.Generic;
+using DOL.GS.PlayerTitles;
+using log4net;
+
+
+namespace DOL.GS.PacketHandler
+{
+    [PacketLib(1115, GameClient.eClientVersion.Version1115)]
+    public class PacketLib1115 : PacketLib1114
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Constructs a new PacketLib for Client Version 1.115
+        /// </summary>
+        /// <param name="client">the gameclient this lib is associated with</param>
+        public PacketLib1115(GameClient client)
+            : base(client)
+        {
+
+        }
+        
+        /// <summary>
+        /// New Iem Packet Data in v1.115
+        /// </summary>
+        /// <param name="pak"></param>
+        /// <param name="item"></param>
+        protected override void WriteItemData(GSTCPPacketOut pak, InventoryItem item) {
+        	
+        	if (item == null)
+			{
+				pak.Fill(0x00, 23); // 1.115 +1 short in front of item Data
+				return;
+			}
+        	
+        	// FIXME: Unknown short, seems to have some importance in displaying "Bonus Level" in Tool Tip.
+        	// More research needed, this doesn't break 1.115 Client...
+        	pak.WriteShort(0);
+        	
+        	// TODO : Override for PacketLib 1.112 needed for this to work (See Darwin/Eden 1.112 implementation)
+        	base.WriteItemData(pak, item);
+        }
+    }
+}
