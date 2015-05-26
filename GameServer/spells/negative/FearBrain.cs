c399d6d52c82c4bbb688a11abd23072431112bc1
kypd <kypd@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2015-05-26 06:17:37 +0000

    Update: Fear Spell Handler and Fear Brain reviewed to be more effective and use base Spellhandler Methods. Update: Spell Handler "on Resisted" Method improved to allow easier override Add: SpellMessagesHelper Static Extension to Help sending Messages from Spells and replace SpellHandler Base Methods.

--- GameServer/spells/negative/FearBrain.cs
+++ GameServer/spells/negative/FearBrain.cs
@@ -1,40 +1,62 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+
 using DOL.GS;
-using DOL.GS.Spells;
-using DOL.GS.Effects;
 
 namespace DOL.AI.Brain
 {
 	public class FearBrain : StandardMobBrain
 	{
-		public FearBrain()
-			: base()
-		{
-			ThinkInterval = 5000;
+		/// <summary>
+		/// Fixed thinking Interval for Fleeing
+		/// </summary>
+		public override int ThinkInterval {
+			get {
+				return 3000;
+			}
 		}
-
+		
+		/// <summary>
+		/// Flee from Players on Brain Think
+		/// </summary>
 		public override void Think()
 		{
-			foreach (GamePlayer player in Body.GetPlayersInRadius((ushort)AggroRange))
+			foreach (GamePlayer player in Body.GetPlayersInRadius((ushort)Math.Max(AggroRange, 750)))
 			{
 				CalculateFleeTarget(player);
 				break;
 			}
 		}
 
-		///<summary>Calculate flee target.</summary>
-		///<param>The target to flee.</param>
+		///<summary>
+		/// Calculate flee target.
+		/// </summary>
+		///<param name="target">The target to flee.</param>
 		protected virtual void CalculateFleeTarget(GameLiving target)
 		{
-			Point3D stalker = new Point3D(target.X, target.Y, target.Z);
-			ushort NotTarget = Body.GetHeading( stalker );
+			ushort TargetAngle = (ushort)((Body.GetHeading(target) + 2048) % 4096);
 
-			ushort TargetAngle = (ushort)((NotTarget + 2048) % 4096);
-			Body.Heading = TargetAngle;
-
-            Point2D fleePoint = Body.GetPointFromHeading( Body.Heading, 300 );
+            Point2D fleePoint = Body.GetPointFromHeading(TargetAngle, 300);
 			Body.StopFollowing();
 			Body.StopAttack();
-			Body.WalkTo( fleePoint.X, fleePoint.Y, Body.Z, Body.MaxSpeed );
+			Body.WalkTo(fleePoint.X, fleePoint.Y, Body.Z, Body.MaxSpeed);
 		}
 	}
 } 
