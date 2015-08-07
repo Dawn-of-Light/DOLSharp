8cf0f170217d38cb787f522850506a971641f466
dol-leodagan <leodagan@durron.net>
2015-08-07 13:51:31 +0000

    Updated: Moved Some GamePlayer Utils Methods to static Player Utils Extension Class. Fixed: Bugs around Bonedancer Pet Targeted Damage Add with Area of Effect Radius, it was checked as an Enemy Spell Fixed: Positive Effects shouldn't have Area/Range reduced Time/Effect. (Effectivenes mod)
    git-svn-id: https://svn.code.sf.net/p/dolserver/code/DOLSharp/trunk@3540 c9877f0d-d2ea-46ae-93cb-121acdf3e971

--- GameServer/spells/Bonedancer/SummonCommanderPet.cs
+++ GameServer/spells/Bonedancer/SummonCommanderPet.cs
@@ -89,9 +89,9 @@ namespace DOL.GS.Spells
                 delve.Add("");
                 delve.Add(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text2"));
                 delve.Add("");
-                delve.Add(String.Format(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text3", Spell.Target)));
-                delve.Add(String.Format(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text4", Math.Abs(Spell.Power))));
-                delve.Add(String.Format(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text5", (Spell.CastTime / 1000).ToString("0.0## " + (LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "Effects.DelveInfo.Seconds"))))));
+                delve.Add(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text3", Spell.Target));
+                delve.Add(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text4", Math.Abs(Spell.Power)));
+                delve.Add(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonCommanderPet.DelveInfo.Text5", (Spell.CastTime / 1000).ToString("0.0## " + (LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "Effects.DelveInfo.Seconds")))));
                 return delve;
             }
 		}
