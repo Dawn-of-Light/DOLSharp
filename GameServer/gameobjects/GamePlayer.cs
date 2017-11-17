ccd3bd80bda4f00888a6933c581a2064a085893a
OntheDOL <OntheDOL@users.noreply.github.com>
2017-11-17 16:06:12 -0700

    Camouflage change to live-like (#60)
    * Camouflage change to live-like
    
    Changes to camouflage ability to reflect live-like mechanics and timer.
    Some translations added.
    
    * update french camouflage translations
    
    update french camouflage translations
    
    * camouflage PR update
    
    Remove duplicate disableskill call
    
    * update camo
    
    diabled and timer reset on every PVP/RVR attack the player makes

--- GameServer/gameobjects/GamePlayer.cs
+++ GameServer/gameobjects/GamePlayer.cs
@@ -6313,15 +6313,17 @@ namespace DOL.GS
 					{
 						(weapon as GameInventoryItem).OnStrikeTarget(this, target);
 					}
-					//Camouflage
-					if (target is GamePlayer && HasAbility(Abilities.Camouflage))
+					//Camouflage - Camouflage will be disabled only when attacking a GamePlayer or ControlledNPC of a GamePlayer.
+					if (HasAbility(Abilities.Camouflage) && target is GamePlayer || (target is GameNPC && (target as GameNPC).Brain is IControlledBrain && ((target as GameNPC).Brain as IControlledBrain).GetPlayerOwner() != null))
 					{
 						CamouflageEffect camouflage = EffectList.GetOfType<CamouflageEffect>();
-						if (camouflage != null)
+												
+						if (camouflage != null)// Check if Camo is active, if true, cancel ability.
 						{
-							DisableSkill(SkillBase.GetAbility(Abilities.Camouflage), CamouflageSpecHandler.DISABLE_DURATION);
-							camouflage.Cancel(false);
+							camouflage.Cancel(false);						
 						}
+						Skill camo = SkillBase.GetAbility(Abilities.Camouflage); // now we find the ability
+						DisableSkill(camo, CamouflageSpecHandler.DISABLE_DURATION); // and here we disable it.
 					}
 
 					// Multiple Hit check
