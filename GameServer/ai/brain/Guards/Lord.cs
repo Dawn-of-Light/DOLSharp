e303e91f05b33ba22fb4c2f39f70b72a7905a977
tegstewart <tegstewart@gmail.com>
2017-12-20 17:35:08 -0700

    Lord calls for help
    This causes keep lords to call for help when attacked, causing all surviving keep guards to rush to reinforce them without aggroing a specific target so they'll attack the first hostile they see.
    
    Once we have pathing working, this will allow for things like bottlenecking reinforcements in stair and doorways, but for now they come in from all directions so clearing keep guards before tapping the lord is extremely important.

--- GameServer/ai/brain/Guards/Lord.cs
+++ GameServer/ai/brain/Guards/Lord.cs
@@ -8,6 +8,8 @@ namespace DOL.AI.Brain
 	/// </summary>
 	public class LordBrain : KeepGuardBrain
 	{
+		private long m_nextCallForHelpTime = 0; // Used to limit how often keep lords call for help
+	
 		public LordBrain() : base()
 		{
 		}
@@ -32,5 +34,37 @@ namespace DOL.AI.Brain
 			}
 			base.Think();
 		}
-	}
+
+		/// <summary>
+		/// Bring all alive keep guards to defend the lord
+		/// </summary>
+		/// <param name="attackData">The data associated with the puller's attack.</param>
+		protected override void BringFriends(AttackData ad)
+		{
+			long currenttime = DateTime.Now.Ticks;
+			if (Body is GuardLord lord && m_nextCallForHelpTime < currenttime)
+			{
+				// Don't call for help more than once every minute
+				m_nextCallForHelpTime = currenttime + TimeSpan.TicksPerMinute;
+
+				int iGuardsResponding = 0;
+				foreach (GameKeepGuard guard in lord.Component.Keep.Guards.Values)
+					if (guard != null && guard.IsAlive && guard.IsAvailable && !(guard is FrontierHastener))
+					{
+						iGuardsResponding++;
+						guard.Follow(ad.Target, GameNPC.STICKMINIMUMRANGE, int.MaxValue);
+					}
+
+				string sMessage = $"{lord.Name} bellows for assistance ";
+				if (iGuardsResponding == 0)
+					sMessage += "but no guards respond!";
+				else
+					sMessage += $"and {iGuardsResponding} guards respond!";
+
+				foreach (GamePlayer player in lord.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE, true))
+					if (player != null)
+						ChatUtil.SendErrorMessage(player, sMessage);
+			}
+		}
+	}// LordBrain
 }
