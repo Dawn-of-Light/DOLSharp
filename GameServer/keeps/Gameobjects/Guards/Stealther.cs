df5164cd8f1fac96c990a2c031b5662715d4ab07
tegstewart <tegstewart@gmail.com>
2018-07-01 16:09:08 -0600

    Set to be stealthy
    Stealth guards weren't stealthing.

--- GameServer/keeps/Gameobjects/Guards/Stealther.cs
+++ GameServer/keeps/Gameobjects/Guards/Stealther.cs
@@ -2,6 +2,9 @@ namespace DOL.GS.Keeps
 {
 	public class GuardStealther : GameKeepGuard
 	{
-
+        public GuardStealther() : base()
+        {
+            Flags = eFlags.STEALTH;
+        }
 	}
 }
