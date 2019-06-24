debc1e7c27fa5359d0d195859ef2d56ea0d802e7
tegstewart <tegstewart@gmail.com>
2019-06-24 17:26:02 -0600

    Properly incorporates MoPower/MoPain for necros, expanded wild minion
    Necro pets now apply the MoPower and MoPain for RAs for pet spells and melee attacks.
    Now applies Wild Minion RA to ranged attacks and spells when the the EXPAND_WILD_MINION server property is true.

--- GameServer/propertycalc/CriticalArcheryHitChanceCalculator.cs
+++ GameServer/propertycalc/CriticalArcheryHitChanceCalculator.cs
@@ -1,4 +1,5 @@
 using System;
+using DOL.AI.Brain;
 
 namespace DOL.GS.PropertyCalc
 {
@@ -18,8 +19,19 @@ namespace DOL.GS.PropertyCalc
 
 		public override int CalcValue(GameLiving living, eProperty property) 
 		{
-			// base 10% chance of critical for all with ranged weapons plus ra bonus
-			return 10 + living.BuffBonusCategory4[(int)property] + living.AbilityBonus[(int)property];
+			int chance = living.BuffBonusCategory4[(int)property] + living.AbilityBonus[(int)property];
+
+			if (living is GamePet gamePet)
+			{
+				if (ServerProperties.Properties.EXPAND_WILD_MINION && gamePet.Brain is IControlledBrain playerBrain
+					&& playerBrain.GetPlayerOwner() is GamePlayer player
+					&& player.GetAbility<RealmAbilities.WildMinionAbility>() is RealmAbilities.WildMinionAbility ab)
+					chance += ab.Amount;
+			}
+			else // not a pet
+				chance += 10;
+
+			return chance;
 		}
 	}
 }
