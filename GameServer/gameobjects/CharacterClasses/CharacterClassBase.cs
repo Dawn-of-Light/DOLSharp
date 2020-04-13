d7d6cb42977b24017662ef28516c575a51650458
Ted Stewart <tegstewart@gmail.com>
2020-04-12 22:02:03 -0600

    Changed log to private
    Protected leads to inheriting classes misreporting their calling object in the log.

--- GameServer/gameobjects/CharacterClasses/CharacterClassBase.cs
+++ GameServer/gameobjects/CharacterClasses/CharacterClassBase.cs
@@ -31,7 +31,7 @@ namespace DOL.GS
 	/// </summary>
 	public abstract class CharacterClassBase : ICharacterClass
 	{
-		protected static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
 		/// <summary>
 		/// id of class in Client
