9b82d652eb513f6da45b06eb1e15b252b0da3a21
Ted Stewart <tegstewart@gmail.com>
2020-04-28 14:29:27 -0600

    Applied Math.Abs() to debuff values
    Debuffs are applied inconsistently in code; they're added to properties in some places and subtracted in others.

--- GameServer/propertycalc/WeaponSkillCalculator.cs
+++ GameServer/propertycalc/WeaponSkillCalculator.cs
@@ -10,8 +10,8 @@ namespace DOL.GS.PropertyCalc
 			+ living.BaseBuffBonusCategory[(int)property] // enchance the weaponskill
 			+ living.SpecBuffBonusCategory[(int)property] // enchance the weaponskill
 				//hotfix for poisons where both debuff components have same value
-			- (int)(living.DebuffCategory[(int)property] / 5.4) // reduce
-		   + living.ItemBonus[(int)property];
+			- (int)Math.Abs(living.DebuffCategory[(int)property] / 5.4) // reduce
+			+ living.ItemBonus[(int)property];
 			return (int)Math.Max(1, percent);
 		}
 	}
