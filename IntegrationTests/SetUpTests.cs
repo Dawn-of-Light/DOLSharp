bd8abb945bb34112d2859528c79537df54deb737
NetDwarf <netdwarf@users.noreply.github.com>
2020-12-06 11:51:26 +0100

    Merge Integration Tests

--- IntegrationTests/SetUpTests.cs
+++ IntegrationTests/SetUpTests.cs
@@ -24,6 +24,9 @@ using DOL.GS;
 using DOL.Database.Connection;
 
 using NUnit.Framework;
+using DOL.Database;
+using System.Linq;
+using DOL.Database.Attributes;
 
 namespace DOL.Server.Tests
 {
@@ -36,7 +39,7 @@ namespace DOL.Server.Tests
 		public SetUpTests()
 		{
 		}
-		
+
 		/// <summary>
 		/// Create Game Server Instance for Tests
 		/// </summary>
@@ -47,21 +50,54 @@ namespace DOL.Server.Tests
 			Console.WriteLine("Code Base: " + CodeBase.FullName);
 			DirectoryInfo FakeRoot = CodeBase.Parent;
 			Console.WriteLine("Fake Root: " + FakeRoot.FullName);
-			
-			if(GameServer.Instance == null)
+
+			if (GameServer.Instance == null)
 			{
 				GameServerConfiguration config = new GameServerConfiguration();
 				config.RootDirectory = FakeRoot.FullName;
 				config.DBType = ConnectionType.DATABASE_SQLITE;
 				config.DBConnectionString = string.Format("Data Source={0};Version=3;Pooling=False;Cache Size=1073741824;Journal Mode=Off;Synchronous=Off;Foreign Keys=True;Default Timeout=60",
-			                                     Path.Combine(config.RootDirectory, "dol-tests-only.sqlite3.db"));
+												 Path.Combine(config.RootDirectory, "dol-tests-only.sqlite3.db"));
 				config.Port = 0; // Auto Choosing Listen Port
 				config.UDPPort = 0; // Auto Choosing Listen Port
 				config.IP = System.Net.IPAddress.Parse("127.0.0.1");
 				config.UDPIP = System.Net.IPAddress.Parse("127.0.0.1");
 				config.RegionIP = System.Net.IPAddress.Parse("127.0.0.1");
-				GameServer.CreateInstance(config);
+
+				GameServer.LoadTestDouble(new GameServerWithDefaultDB(config));
+
 				Console.WriteLine("Game Server Instance Created !");
+			}
+		}
+
+		private class GameServerWithDefaultDB : GameServer
+		{
+			public GameServerWithDefaultDB(GameServerConfiguration config) : base(config) { }
+
+			protected override void CheckAndInitDB()
+			{
+				if (m_database == null)
+				{
+					m_database = ObjectDatabase.GetObjectDatabase(Configuration.DBType, Configuration.DBConnectionString);
+
+					//Load only default assembly
+					var assembly = Assembly.Load("DOLDatabase");
+					// Walk through each type in the assembly
+					assembly.GetTypes().AsParallel().ForAll(type =>
+						{
+							if (!type.IsClass || type.IsAbstract)
+							{
+								return;
+							}
+
+							var attrib = type.GetCustomAttributes<DataTable>(false);
+							if (attrib.Any())
+							{
+
+								m_database.RegisterDataObject(type);
+							}
+						});
+				}
 			}
 		}
 		
