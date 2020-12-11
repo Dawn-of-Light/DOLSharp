58d12bc302c7b43a9fae038881a351592fff1fdc
tegstewart <tegstewart@gmail.com>
2020-12-10 21:38:22 -0700

    Server property based BP/RP rewards
    Now uses server properties to determine BP/RP rewards, and the code is a lot cleaner.

--- GameServer/gameobjects/CustomNPC/DreadedSealCollector.cs
+++ GameServer/gameobjects/CustomNPC/DreadedSealCollector.cs
@@ -18,6 +18,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using DOL.Database;
 using DOL.GS.PacketHandler;
 
@@ -29,6 +30,53 @@ namespace DOL.GS
     /// </summary>
     public class DreadedSealCollector : GameNPC
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected static List<Tuple<int, int>> m_levelMultipliers = ParseMultipliers(ServerProperties.Properties.DREADEDSEALS_LEVEL_MULTIPLIER);
+        protected static Dictionary<string, float> m_BPValues = ParseValues(ServerProperties.Properties.DREADEDSEALS_BP_VALUES, true);
+        protected static Dictionary<string, float> m_RPValues = ParseValues(ServerProperties.Properties.DREADEDSEALS_RP_VALUES, false);
+
+        /// <summary>
+        /// Parse a server property string level multiplier values
+        /// </summary>
+        /// <param name="serverProperty"></param>
+        /// <returns></returns>
+        protected static List<Tuple<int, int>> ParseMultipliers(string serverProperty)
+        {
+            List<Tuple<int, int>> list = new List<Tuple<int, int>>();
+
+            foreach (string entry in serverProperty.Split(';'))
+            {
+                string[] asVal = entry.Split('|');
+
+                if (asVal.Length > 1 && int.TryParse(asVal[0], out int level) && int.TryParse(asVal[1], out int multiplier))
+                    list.Add(Tuple.Create(level, multiplier));
+            } // foreach
+
+            list.Sort();
+            return list;
+        }
+
+        /// <summary>
+        /// Parse a server property string BP or RP values
+        /// </summary>
+        /// <param name="serverProperty"></param>
+        /// <returns></returns>
+        protected static Dictionary<string, float> ParseValues(string serverProperty, bool dbl)
+        {
+            Dictionary<string, float> dict = new Dictionary<string, float>();
+
+            foreach (string entry in serverProperty.Split(';'))
+            {
+                string[] asVal = entry.Split('|');
+
+                if (asVal.Length > 1 && float.TryParse(asVal[1], out float value))
+                    dict[asVal[0]] = value;
+            } // foreach
+
+            return dict;
+        }
+
         private void SendReply(GamePlayer target, string msg)
         {
             target.Out.SendMessage(msg, eChatType.CT_System, eChatLoc.CL_ChatWindow);
@@ -39,94 +87,60 @@ namespace DOL.GS
             if (!base.Interact(player))
                 return false;
 
-            player.Out.SendMessage("Hand me any Dreaded Seal and I'll give you realm points!",
+            player.Out.SendMessage("Hand me any Dreaded Seal and I'll give you realm and bounty points!",
                     eChatType.CT_Say, eChatLoc.CL_ChatWindow);
- 
+
             return true;
         }
 
-        protected static readonly string[] SealKeys =
-            {
-                "glowing_dreaded_seal",
-                "sanguine_dreaded_seal",
-                "lambent_dreaded_seal",
-                "lambent_dreaded_seal2",
-                "fulgent_dreaded_seal",
-                "effulgent_dreaded_seal"
-            };
-
-        protected static readonly long[] SealValues = { 1, 1, 10, 10, 50, 250 };
-
         public override bool ReceiveItem(GameLiving source, InventoryItem item)
         {
-            GamePlayer player = source as GamePlayer;
-            int Level = player.Level;
-            long currentrps = player.RealmPoints;
-            long maxrps = 66181501;
-
-            if (GetDistanceTo(player) > WorldMgr.INTERACT_DISTANCE)
+            if (source is GamePlayer player && item != null)
             {
-                ((GamePlayer)source).Out.SendMessage("You are too far away to give anything to me "
-                + player.Name + ". Come a little closer.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                return false;
-            }
-
-            if (player != null && item != null)
-            {
-                int index = Array.IndexOf(SealKeys, item.Id_nb);
-
-                if (index < 0)
-                    return base.ReceiveItem(source, item);
-
-                long amount = SealValues[index];
-
-                if (Level <= 20)
+                if (GetDistanceTo(player) > WorldMgr.INTERACT_DISTANCE)
                 {
-                    ((GamePlayer)source).Out.SendMessage("You are too young yet to make use of these items "
-                    + player.Name + ". Come back in " + (21 - Level) + " levels.", eChatType.CT_Say, eChatLoc.CL_ChatWindow);
+                    ((GamePlayer)source).Out.SendMessage("You are too far away to give anything to me "
+                    + player.Name + ". Come a little closer.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     return false;
                 }
-                else if (Level < 26)
+
+                if (m_BPValues.TryGetValue(item.Id_nb, out float bps)
+                    && m_RPValues.TryGetValue(item.Id_nb, out float rps))
                 {
-                    amount *= 20 * item.Count; // At level 21 to 25 you get 20 Realm Points per set of seals.
-                    player.GainBountyPoints(1);             // Force a +1 BP gain
+                    int multiplier = -1;
+                    int nextLevel = 0;
+
+                    foreach (Tuple<int, int> tup in m_levelMultipliers)
+                    {
+                        if (player.Level >= tup.Item1)
+                            multiplier = tup.Item2;
+                        else
+                        {
+                            nextLevel = tup.Item1;
+                            break;
+                        }
+                    }
+
+                    if (multiplier < 0)
+                        log.Error($"Dreaded Seal multiplier could not be found for player level {player.Level}, DREADEDSEALS_LEVEL_MULTIPLIER server property={ServerProperties.Properties.DREADEDSEALS_LEVEL_MULTIPLIER}");
+                    else if (multiplier == 0)
+                    {
+                        ((GamePlayer)source).Out.SendMessage("You are too young yet to make use of these items "
+                        + player.Name + ". Come back in " + (nextLevel - player.Level) + " levels.", eChatType.CT_Say, eChatLoc.CL_ChatWindow);
+                        return false;
+                    }
+
+                    if (bps > 0)
+                        player.GainBountyPoints((int)(item.Count * multiplier * bps));
+
+                    if (rps > 0)
+                        player.GainRealmPoints((int)(item.Count * multiplier * rps));
+
+                    player.Inventory.RemoveItem(item);
+                    player.Out.SendUpdatePoints();
+
+                    return true;
                 }
-                else if (Level < 31)
-                {
-                    amount *= 30 * item.Count; // At level 26 to 30 you get 30 Realm Points per set of seals.
-                    player.GainBountyPoints(2);             // Force a +2 BP gain
-                }
-                else if (Level < 36)
-                {
-                    amount *= 50 * item.Count; // At level 31 to 35 you get 50 Realm Points per set of seals.
-                    player.GainBountyPoints(3);            // Force a +3 BP gain
-                }
-                else if (Level < 41)
-                    amount *= 300 * item.Count; // At level 36 to 40 you get 300 Realm Points per set of seals.
-                else if (Level < 46)
-                    amount *= 700 * item.Count; // At level 41 to 45 you get 700 Realm Points per set of seals.
-                else if (Level < 50)
-                    amount *= 1500 * item.Count; // At level 46 to 49 you get 1500 Realm Points per set of seals.
-                else
-                    amount *= 3000 * item.Count; // At level 50 you get 3000 Realm Points per set of seals.
-
-                if (currentrps < maxrps)
-                {
-                    if (amount + currentrps > maxrps)
-                        amount = maxrps - currentrps; // only give enough realm points to reach max
-
-                    player.GainRealmPoints(amount);
-                }
-
-                if (Level > 35)
-                {
-                    player.GainBountyPoints(amount / 55);
-                } // Only BP+ those of 36+ to prevent double BP gains
-
-                player.Inventory.RemoveItem(item);
-                player.Out.SendUpdatePoints();
-
-                return true;
             }
 
             return base.ReceiveItem(source, item);
