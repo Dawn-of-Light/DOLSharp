3ce5e7b86df25e911aa14e7ee60e6033ccc651f1
NetDwarf <NetDwarf@users.noreply.github.com>
2020-12-13 21:59:02 +0000

    Move all tests into Tests project (#272)
    * Merge Integration Tests
    
    * DOL.Integration namespace for integration tests
    
    * Cleanup using statements and some comments
    
    * Merge Integration and Unit tests into same project
    
    * Adjust Travis and AppVeyor config
    
    Change output directory of Tests project
    
    * Fix AppVeyor
    
    Replace single quote with double quote for assembly location

---
+++ Tests/IntegrationTests/SetUpTests.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+
+using NUnit.Framework;
+using DOL.GS;
+using DOL.Database;
+using DOL.Database.Connection;
+using DOL.Database.Attributes;
+
+namespace DOL.Integration.Server
+{
+	/// <summary>
+	/// SetUpTests Start The Needed Environnement for Unit Tests
+	/// </summary>
+	[SetUpFixture]
+	public class SetUpTests
+	{
+		public SetUpTests()
+		{
+		}
+
+		/// <summary>
+		/// Create Game Server Instance for Tests
+		/// </summary>
+		public static void CreateGameServerInstance()
+		{
+			Console.WriteLine("Create Game Server Instance");
+			DirectoryInfo CodeBase = new FileInfo(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath).Directory;
+			Console.WriteLine("Code Base: " + CodeBase.FullName);
+			DirectoryInfo FakeRoot = CodeBase.Parent;
+			Console.WriteLine("Fake Root: " + FakeRoot.FullName);
+
+			if (GameServer.Instance == null)
+			{
+				GameServerConfiguration config = new GameServerConfiguration();
+				config.RootDirectory = FakeRoot.FullName;
+				config.DBType = ConnectionType.DATABASE_SQLITE;
+				config.DBConnectionString = string.Format("Data Source={0};Version=3;Pooling=False;Cache Size=1073741824;Journal Mode=Off;Synchronous=Off;Foreign Keys=True;Default Timeout=60",
+												 Path.Combine(config.RootDirectory, "dol-tests-only.sqlite3.db"));
+				config.Port = 0; // Auto Choosing Listen Port
+				config.UDPPort = 0; // Auto Choosing Listen Port
+				config.IP = System.Net.IPAddress.Parse("127.0.0.1");
+				config.UDPIP = System.Net.IPAddress.Parse("127.0.0.1");
+				config.RegionIP = System.Net.IPAddress.Parse("127.0.0.1");
+
+				GameServer.LoadTestDouble(new GameServerWithDefaultDB(config));
+
+				Console.WriteLine("Game Server Instance Created !");
+			}
+		}
+
+		private class GameServerWithDefaultDB : GameServer
+		{
+			public GameServerWithDefaultDB(GameServerConfiguration config) : base(config) { }
+
+			protected override void CheckAndInitDB()
+			{
+				if (m_database == null)
+				{
+					m_database = ObjectDatabase.GetObjectDatabase(Configuration.DBType, Configuration.DBConnectionString);
+
+					//Load only default assembly
+					var assembly = Assembly.Load("DOLDatabase");
+					// Walk through each type in the assembly
+					assembly.GetTypes().AsParallel().ForAll(type =>
+						{
+							if (!type.IsClass || type.IsAbstract)
+							{
+								return;
+							}
+
+							var attrib = type.GetCustomAttributes<DataTable>(false);
+							if (attrib.Any())
+							{
+
+								m_database.RegisterDataObject(type);
+							}
+						});
+				}
+			}
+		}
+		
+		[OneTimeSetUp]
+		public virtual void Init()
+		{
+			CreateGameServerInstance();
+			
+			if (!GameServer.Instance.IsRunning)
+			{
+				Console.WriteLine("Starting GameServer");
+				if (!GameServer.Instance.Start())
+				{
+					Console.WriteLine("Error init GameServer");
+				}
+			}
+			else
+			{
+				Console.WriteLine("GameServer already running, skip init of Gameserver...");
+			}
+		}
+
+		[OneTimeTearDown]
+		public void Dispose()
+		{
+			GameServer.Instance.Stop();
+		}
+	}
+}
---
+++ Tests/UnitTests/UT_DataQuest.cs
@@ -0,0 +1,375 @@
+﻿using DOL.Database;
+using DOL.GS;
+using DOL.GS.PlayerClass;
+using DOL.GS.Quests;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DOL.UnitTests.Gameserver
+{
+    [TestFixture]
+    class UT_DataQuest
+    {
+        FakeServer fakeServer;
+        [SetUp]
+        public void Init()
+        {
+            GS.ServerProperties.Properties.DEBUG_LOAD_REGIONS = "";
+            GS.ServerProperties.Properties.DISABLED_REGIONS = "";
+            GS.ServerProperties.Properties.DISABLED_EXPANSIONS = "";
+            fakeServer = FakeServer.LoadAndReturn();
+        }
+        #region Accessor
+        [Test]
+        public void MoneyReward_Init_Zero()
+        {
+            var dataQuest = NewDataQuest();
+
+            var actual = dataQuest.MoneyReward();
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExperiencePercent_Init_Zero()
+        {
+            var dataQuest = NewDataQuest();
+            var player = NewFakePlayer();
+            player.Level = 1;
+
+            var actual = dataQuest.ExperiencePercent(player);
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MoneyReward_RewardMoneyIs2InAString_2()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardMoney = "2";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            var actual = dataQuest.MoneyReward();
+            var expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ExperiencePercent_PlayerIsLevelOneAndRewardXPIs2InAString_4()
+        {
+            var player = NewFakePlayer();
+            player.Level = 1;
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardXP = "2";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            var actual = dataQuest.ExperiencePercent(player);
+            var expected = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RewardMoney_SetTo2Pipe3Pipe4OnStep2_3()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardMoney = "2|3|4";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            dataQuest.Step = 2;
+
+            var actual = dataQuest.SpyRewardMoney;
+            var expected = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RewardXP_SetTo2Pipe3Pipe4OnStepOne_3()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardXP = "2|3|4";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            dataQuest.Step = 2;
+
+            var actual = dataQuest.SpyRewardXP;
+            var expected = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RewardCLXP_SetTo4_4()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardCLXP = "4";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.SpyRewardCLXP;
+            var expected = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RewardRP_SetTo5_5()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardRP = "5";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.SpyRewardRP;
+            var expected = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RewardBP_SetTo6_6()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.RewardBP = "6";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.SpyRewardBP;
+            var expected = 6;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StepType_Init_StepTypeUnknown()
+        {
+            var dataQuest = NewDataQuest();
+
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.StepType;
+            var expected = DataQuest.eStepType.Unknown;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Step_Init_Zero()
+        {
+            var dataQuest = NewDataQuest();
+
+            var actual = dataQuest.Step;
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion Accessor
+
+        #region CheckQuestQualification
+        [Test]
+        public void CheckQuestQualification_Default_True()
+        {
+            var player = NewFakePlayer();
+            var dataQuest = NewDataQuest();
+
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsTrue(isQualified);
+        }
+
+        [Test]
+        public void CheckQuestQualification_PlayerIsLevelOneAndMinLevelIsTwo_False()
+        {
+            var player = NewFakePlayer();
+            player.Level = 1;
+            var dataQuest = NewDataQuest();
+            dataQuest.SpyDBDataQuest.MinLevel = 2;
+
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsFalse(isQualified);
+        }
+
+        [Test]
+        public void CheckQuestQualification_PlayerIsPaladinAndAllowedClassIsCleric_False()
+        {
+            var player = NewFakePlayer();
+            player.fakeCharacterClass = new ClassPaladin();
+            var dbDataQuest = new DBDataQuest();
+            var clericClassID = (int)eCharacterClass.Cleric;
+            dbDataQuest.AllowedClasses = clericClassID.ToString();
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsFalse(isQualified);
+        }
+
+        [Test]
+        public void CheckQuestQualification_PlayerDidQuestThreeTimesAndMaxCountIsThree_False()
+        {
+            var player = NewFakePlayer();
+            var dataQuest = NewDataQuest();
+            dataQuest.SpyDBDataQuest.MaxCount = 3;
+            dataQuest.SpyDBDataQuest.StartType = (byte)DataQuest.eStartType.Collection;
+            dataQuest.SpyCharQuest.Count = 3;
+            fakeServer.fakeDatabase.SelectObjectReturns = new List<CharacterXDataQuest>() { dataQuest.SpyCharQuest };
+
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsFalse(isQualified);
+        }
+
+        [Test]
+        public void CheckQuestQualification_PlayerStartedQuestAlready_False()
+        {
+            var player = NewFakePlayer();
+            var dataQuest = NewDataQuest();
+
+            player.fakeQuestList.Add(dataQuest);
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsFalse(isQualified);
+        }
+
+        [Test]
+        public void CheckQuestQualification_PlayerFinishedQuestAlreadyAndMaxCountReached_False()
+        {
+            var player = NewFakePlayer();
+            var dataQuest = NewDataQuest();
+            dataQuest.SpyDBDataQuest.MaxCount = 1;
+            dataQuest.SpyCharQuest.Count = 1;
+
+            player.fakeQuestListFinished.Add(dataQuest);
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsFalse(isQualified);
+        }
+
+        [Test]
+        public void CheckQuestQualification_DependentQuestNotDone_False()
+        {
+            var player = NewFakePlayer();
+            var dbDataQuest = new DBDataQuest();
+            dbDataQuest.QuestDependency = "SomeQuestName";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            bool isQualified = dataQuest.CheckQuestQualification(player);
+
+            Assert.IsFalse(isQualified);
+        }
+#endregion CheckQuestQualification
+
+        [Test]
+        public void IsDoingQuest_SameQuest_True()
+        {
+            var dataQuest = NewDataQuest();
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.IsDoingQuest(dataQuest);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsDoingQuest_SameQuestAndStepIsZero_False()
+        {
+            var dataQuest = NewDataQuest();
+            dataQuest.Step = 0;
+
+            var actual = dataQuest.IsDoingQuest(dataQuest);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsDoingQuest_QuestWithDifferentID_False()
+        {
+            var dataQuest = NewDataQuest();
+            dataQuest.SpyDBDataQuest.ID = 1;
+            var otherDataQuest = NewDataQuest();
+            otherDataQuest.SpyDBDataQuest.ID = 2;
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.IsDoingQuest(otherDataQuest);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void TargetName_Default_EmptyString()
+        {
+            var dataQuest = NewDataQuest();
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.TargetName;
+            var expected = "";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TargetName_SomeName_SomeName()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.TargetName = "SomeName";
+            var dataQuest = NewDataQuest(dbDataQuest);
+            dataQuest.Step = 1;
+
+            var actual = dataQuest.TargetName;
+            var expected = "SomeName";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TargetName_FooZeroAndBarZeroOnStep2_Bar()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.TargetName = "Foo;0|Bar;0";
+            var dataQuest = NewDataQuest(dbDataQuest);
+            dataQuest.Step = 2;
+
+            var actual = dataQuest.TargetName;
+            var expected = "Bar";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TargetText_BarPipeBazOnStep2_Baz()
+        {
+            var dbDataQuest = NewDBDataQuest();
+            dbDataQuest.TargetText = "Bar|Baz";
+            var dataQuest = NewDataQuest(dbDataQuest);
+
+            dataQuest.Step = 2;
+
+            var actual = dataQuest.SpyTargetText;
+            var expected = "Baz";
+            Assert.AreEqual(expected, actual);
+        }
+
+        private FakeQuestPlayer NewFakePlayer() => new FakeQuestPlayer();
+        private DataQuestSpy NewDataQuest() => NewDataQuest(NewDBDataQuest());
+        private DataQuestSpy NewDataQuest(DBDataQuest dbDataQuest) => new DataQuestSpy(null, dbDataQuest, new CharacterXDataQuest());
+        private DBDataQuest NewDBDataQuest() => new DBDataQuest();
+
+        private class FakeQuestPlayer : FakePlayer
+        {
+            public List<AbstractQuest> fakeQuestList = new List<AbstractQuest>();
+            public List<AbstractQuest> fakeQuestListFinished = new List<AbstractQuest>();
+
+            public override List<AbstractQuest> QuestList => fakeQuestList;
+            public override List<AbstractQuest> QuestListFinished => fakeQuestListFinished;
+        }
+
+        private class DataQuestSpy : DataQuest
+        {
+            public long SpyRewardMoney => RewardMoney;
+            public long SpyRewardXP => RewardXP;
+            public long SpyRewardCLXP => RewardCLXP;
+            public long SpyRewardRP => RewardRP;
+            public long SpyRewardBP => RewardBP;
+            public string SpyTargetText => TargetText;
+
+            public DBDataQuest SpyDBDataQuest => m_dataQuest;
+            public CharacterXDataQuest SpyCharQuest => m_charQuest;
+
+            public DataQuestSpy(GamePlayer player, DBDataQuest dbDataQuest, CharacterXDataQuest charXDataQuest) : base(player, dbDataQuest, charXDataQuest) { }
+        }
+    }
+}
