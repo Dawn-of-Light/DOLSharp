26595d218fb560111bf41fd38c841062e66fbed0
Ted Stewart <tegstewart@gmail.com>
2020-12-16 15:03:55 -0700

    Various improvements
    Removed reeding a superfluous leading zero level multiplier.
    
    Made function and variable names more descriptive.
    
    Made collectors communicate more accurate and useful information to players, standardized their responses to use the chat rather than system window.
    
    Added error log message and made collector responses more informative to help admins track down server property issues.
    
    Collectors can now give out just BPs or just RPs, rather that failing to award anything if only BP or RP rewards are defined.  This allows for things like glowing seals only giving BP rewards so you have to kill keep lords to get seals which grant RPs.

--- GameServer/gameobjects/CustomNPC/DreadedSealCollector.cs
+++ GameServer/gameobjects/CustomNPC/DreadedSealCollector.cs
@@ -32,20 +32,20 @@ namespace DOL.GS
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        protected static List<Tuple<int, int>> m_levelMultipliers = ParseMultipliers(ServerProperties.Properties.DREADEDSEALS_LEVEL_MULTIPLIER);
-        protected static Dictionary<string, float> m_BPValues = ParseValues(ServerProperties.Properties.DREADEDSEALS_BP_VALUES);
-        protected static Dictionary<string, float> m_RPValues = ParseValues(ServerProperties.Properties.DREADEDSEALS_RP_VALUES);
+        protected static List<Tuple<int, int>> m_levelMultipliers = ParseMultipliers();
+        protected static Dictionary<string, float> m_BPMultipliers = ParseValues(ServerProperties.Properties.DREADEDSEALS_BP_VALUES);
+        protected static Dictionary<string, float> m_RPMultipliers = ParseValues(ServerProperties.Properties.DREADEDSEALS_RP_VALUES);
 
         /// <summary>
         /// Parse a server property string level multiplier values
         /// </summary>
         /// <param name="serverProperty"></param>
         /// <returns></returns>
-        protected static List<Tuple<int, int>> ParseMultipliers(string serverProperty)
+        protected static List<Tuple<int, int>> ParseMultipliers()
         {
             List<Tuple<int, int>> list = new List<Tuple<int, int>>();
 
-            foreach (string entry in serverProperty.Split(';'))
+            foreach (string entry in ServerProperties.Properties.DREADEDSEALS_LEVEL_MULTIPLIER.Split(';'))
             {
                 string[] asVal = entry.Split('|');
 
@@ -53,7 +53,11 @@ namespace DOL.GS
                     list.Add(Tuple.Create(level, multiplier));
             } // foreach
 
-            list.Sort();
+            if (list.Count > 0)
+                list.Sort();
+            else
+                log.Error("ParseMultipliers: Could not parse any level multipliers; DreadedSealCollector disabled.");
+
             return list;
         }
 
@@ -87,8 +91,19 @@ namespace DOL.GS
             if (!base.Interact(player))
                 return false;
 
-            player.Out.SendMessage("Hand me any Dreaded Seal and I'll give you realm and bounty points!",
-                    eChatType.CT_Say, eChatLoc.CL_ChatWindow);
+            string response;
+
+            if (m_levelMultipliers.Count <= 0)
+                response = "Sorry, no level multipliers are defined so I cannot accept seals at this time.";
+            else if (m_BPMultipliers.Count > 0 && m_RPMultipliers.Count > 0)
+                response = "Hand me Dreaded Seals and I'll give you Bounty and Realm points!";
+            else if (m_BPMultipliers.Count > 0)
+                response = "Hand me Dreaded Seals and I'll give you Bounty points!";
+            else if (m_RPMultipliers.Count > 0)
+                response = "Hand me Dreaded Seals and I'll give you Realm points!";
+            else
+                response = "Sorry, no dreaded seal types are defined so I cannot accept seals at this time.";
+            player.Out.SendMessage(response, eChatType.CT_Say, eChatLoc.CL_ChatWindow);
 
             return true;
         }
@@ -100,20 +115,40 @@ namespace DOL.GS
                 if (GetDistanceTo(player) > WorldMgr.INTERACT_DISTANCE)
                 {
                     ((GamePlayer)source).Out.SendMessage("You are too far away to give anything to me "
-                    + player.Name + ". Come a little closer.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                    + player.Name + ". Come a little closer.", eChatType.CT_Say, eChatLoc.CL_ChatWindow);
                     return false;
                 }
 
-                if (m_BPValues.TryGetValue(item.Id_nb, out float bps)
-                    && m_RPValues.TryGetValue(item.Id_nb, out float rps))
+                if (m_levelMultipliers.Count < 1)
                 {
-                    int multiplier = -1;
+                    ((GamePlayer)source).Out.SendMessage("Sorry, no level multipliers are defined so I cannot accept seals at this time.",
+                        eChatType.CT_Say, eChatLoc.CL_ChatWindow);
+                    return false;
+                }
+
+                float bpMultiplier = 0;
+                if (m_BPMultipliers.ContainsKey(item.Id_nb))
+                    bpMultiplier = m_BPMultipliers[item.Id_nb];
+
+                float rpMultiplier = 0;
+                if (m_RPMultipliers.ContainsKey(item.Id_nb))
+                    rpMultiplier = m_RPMultipliers[item.Id_nb];
+
+                if (bpMultiplier < 1 && rpMultiplier < 1)
+                {
+                    ((GamePlayer)source).Out.SendMessage("Sorry, I cannot accept items of that type.",
+                        eChatType.CT_Say, eChatLoc.CL_ChatWindow);
+                    return false;
+                }
+                else
+                {
+                    int levelMultiplier = 0;
                     int nextLevel = 0;
 
                     foreach (Tuple<int, int> tup in m_levelMultipliers)
                     {
                         if (player.Level >= tup.Item1)
-                            multiplier = tup.Item2;
+                            levelMultiplier = tup.Item2;
                         else
                         {
                             nextLevel = tup.Item1;
@@ -121,20 +156,19 @@ namespace DOL.GS
                         }
                     }
 
-                    if (multiplier < 0)
-                        log.Error($"Dreaded Seal multiplier could not be found for player level {player.Level}, DREADEDSEALS_LEVEL_MULTIPLIER server property={ServerProperties.Properties.DREADEDSEALS_LEVEL_MULTIPLIER}");
-                    else if (multiplier == 0)
+                    if (levelMultiplier <= 0)
                     {
                         ((GamePlayer)source).Out.SendMessage("You are too young yet to make use of these items "
                         + player.Name + ". Come back in " + (nextLevel - player.Level) + " levels.", eChatType.CT_Say, eChatLoc.CL_ChatWindow);
+
                         return false;
                     }
 
-                    if (bps > 0)
-                        player.GainBountyPoints((int)(item.Count * multiplier * bps));
+                    if (bpMultiplier > 0)
+                        player.GainBountyPoints((int)(item.Count * levelMultiplier * bpMultiplier));
 
-                    if (rps > 0)
-                        player.GainRealmPoints((int)(item.Count * multiplier * rps));
+                    if (rpMultiplier > 0)
+                        player.GainRealmPoints((int)(item.Count * levelMultiplier * rpMultiplier));
 
                     player.Inventory.RemoveItem(item);
                     player.Out.SendUpdatePoints();
