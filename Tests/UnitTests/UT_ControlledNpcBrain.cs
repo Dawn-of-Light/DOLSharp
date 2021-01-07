0db10446caa5be8328593703f7374e878d8510a5
NetDwarf <netdwarf@users.noreply.github.com>
2021-01-07 12:10:18 +0100

    Revert ControlledNpcBrain.Get..Owner() methods

---
+++ Tests/UnitTests/UT_ControlledNpcBrain.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using DOL.AI.Brain;
+using NUnit.Framework;
+
+namespace DOL.UnitTests.Gameserver
+{  
+    [TestFixture]
+    class UT_ControlledNpcBrain
+    {
+        [SetUp]
+        public void Init()
+        {
+            GS.ServerProperties.Properties.DISABLED_REGIONS = "";
+            GS.ServerProperties.Properties.DISABLED_EXPANSIONS = "";
+        }
+
+        [Test]
+        public void GetPlayerOwner_InitWithPlayer_Player()
+        {
+            var player = new FakePlayer();
+            var brain = new ControlledNpcBrain(player);
+
+            var actual = brain.GetPlayerOwner();
+
+            var expected = player;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
