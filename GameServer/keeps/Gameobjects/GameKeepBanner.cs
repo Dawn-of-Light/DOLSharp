c55d9dbd2901c127dca9ae4d758fd35eb7bf6dc5
NetDwarf <netdwarf@users.noreply.github.com>
2021-03-09 17:04:54 +0100

    Fix GameKeepBanner.LoadFromPosition

--- GameServer/keeps/Gameobjects/GameKeepBanner.cs
+++ GameServer/keeps/Gameobjects/GameKeepBanner.cs
@@ -158,7 +158,7 @@ namespace DOL.GS.Keeps
 
 			PositionMgr.LoadKeepItemPosition(pos, this);
 			string sKey = this.TemplateID;
-			if (component.AbstractKeep.Banners.ContainsKey(sKey))
+			if (component.AbstractKeep.Banners.ContainsKey(sKey) == false)
 			{
 				component.AbstractKeep.Banners.Add(sKey, this);
 				if (BannerType == eBannerType.Guild)
