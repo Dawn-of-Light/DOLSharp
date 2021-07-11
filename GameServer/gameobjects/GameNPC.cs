f0e2189bd5c36834a0196428a70771c79df4c98d
Lord-Seithenin <86377388+Lord-Seithenin@users.noreply.github.com>
2021-07-11 15:29:03 +0700

    Fix Pets following player erratically (#320)
    Fix Pets following player erratically
    Change speed of pets to a similar speed of the player

--- GameServer/gameobjects/GameNPC.cs
+++ GameServer/gameobjects/GameNPC.cs
@@ -1743,8 +1743,14 @@ namespace DOL.GS
 			newX = (int)(followTarget.X - diffx);
 			newY = (int)(followTarget.Y - diffy);
 			newZ = (int)(followTarget.Z - diffz);
-			WalkTo(newX, newY, (ushort)newZ, MaxSpeed);
-			return ServerProperties.Properties.GAMENPC_FOLLOWCHECK_TIME;
+
+            if (InCombat || Brain is BomberBrain)
+                WalkTo(newX, newY, (ushort)newZ, MaxSpeed);
+            else if (MaxSpeed < GetDistance(new Point2D(newX, newY)))
+                WalkTo(newX, newY, (ushort)newZ, (short)Math.Min(MaxSpeed, followLiving.CurrentSpeed + 50));
+            else
+                WalkTo(newX, newY, (ushort)newZ, (short)GetDistance(new Point2D(newX, newY)));
+            return ServerProperties.Properties.GAMENPC_FOLLOWCHECK_TIME;
 		}
 
 		/// <summary>
