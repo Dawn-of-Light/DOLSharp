ad0e5952902f4548935f45d3f81ca86586403d99
NetDwarf <NetDwarf@users.noreply.github.com>
2021-11-25 17:55:14 +0100

    Fix WarMap Teleport (#369)

--- GameServer/packets/Client/168/warmapshowrequesthandler.cs
+++ GameServer/packets/Client/168/warmapshowrequesthandler.cs
@@ -38,7 +38,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 				return;
 
 			//hack fix new keep ids
-			else if ((int)client.Version >= (int)GameClient.eClientVersion.Version190 && (int)client.Version < (int)GameClient.eClientVersion.Version1115)
+			else if ((int)client.Version >= (int)GameClient.eClientVersion.Version190)
 			{
 				if (keepId >= 82)
 					keepId -= 7;
