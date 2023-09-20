ac0668fa4b5a09320a14cd4946162ed60b21f94e
NetDwarf <NetDwarf@users.noreply.github.com>
2023-09-20 16:10:36 +0200

    Only load mobs from enabled regions
    Respect DISABLED_REGIONS serverproperty
    Add DBColumn.IsNotIn operator

---
+++ Tests/UnitTests/UT_Util.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using DOL.GS;
+using NUnit.Framework;
+
+namespace DOL.UnitTests;
+
+[TestFixture]
+public class UT_Util
+{
+    [Test]
+    public void SplitCSV_EmptyString_EmptyList()
+    {
+        var actual = Util.SplitCSV("");
+        var expected = new List<string>();
+        Assert.AreEqual(expected, actual);
+    }
+}
