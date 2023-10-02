73ad9c9c6c33e949a8b5ca1237594cecf9228ae9
NetDwarf <NetDwarf@users.noreply.github.com>
2023-10-02 21:44:55 +0200

    Hotfix: Server not starting
    Statement with "IN ()" not valid in MySQL (but SQLite)

--- DOLDatabase/WhereClause.cs
+++ DOLDatabase/WhereClause.cs
@@ -141,7 +141,7 @@ namespace DOL.Database
                 if (val is IEnumerable<object> valueCollection)
                 {
                     var result = new List<TextAtom>() { new TextAtom(columnName), new TextAtom(op), new TextAtom("(") };
-                    if(!valueCollection.Any()) return result.Append(new TextAtom(")"));
+                    if(!valueCollection.Any()) return new[] { new TextAtom("false") };
                     result.Add(new ValueAtom(valueCollection.ElementAt(0)));
                     foreach(var element in valueCollection.Skip(1))
                     {
